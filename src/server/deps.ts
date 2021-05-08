bd6cb85903c80cb95b153e90ddb7b2a9a7ca452d
Luca Casonato <lucacasonato@yahoo.com>
2021-05-08 02:18:55 +0200

    more other esbuild

--- src/server/deps.ts
+++ src/server/deps.ts
@@ -8,6 +8,6 @@ export { extname } from "https://deno.land/std@0.95.0/path/mod.ts";
 export * as oak from "https://deno.land/x/oak@v7.4.0/mod.ts";
 
 // -- esbuild --
-import * as esbuild from "https://cdn.skypack.dev/esbuild-wasm?dts";
+import * as esbuild from "https://gist.githubusercontent.com/lucacasonato/358c6b7e8198bfb2cf3d220e49fdcf5f/raw/257dbbbc87fb5f82052b0fa455bd73f4ed07dd81/esbuild-wasm.js";
 export { esbuild };
 export { denoPlugin } from "https://raw.githubusercontent.com/lucacasonato/esbuild_deno_loader/c5ee642f4552078324badbf1b541c7222c07d5ff/mod.ts";
