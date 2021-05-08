207296c0233bd48a2efd17d2de6555b6f36fb167
Luca Casonato <lucacasonato@yahoo.com>
2021-05-08 02:22:53 +0200

    polyfill globalThis.performance

--- src/server/deps.ts
+++ src/server/deps.ts
@@ -8,6 +8,12 @@ export { extname } from "https://deno.land/std@0.95.0/path/mod.ts";
 export * as oak from "https://deno.land/x/oak@v7.4.0/mod.ts";
 
 // -- esbuild --
+const start = new Date().getTime();
+globalThis.performance = {
+  now() {
+    return new Date().getTime() - start;
+  },
+};
 import * as esbuild from "https://gist.githubusercontent.com/lucacasonato/358c6b7e8198bfb2cf3d220e49fdcf5f/raw/257dbbbc87fb5f82052b0fa455bd73f4ed07dd81/esbuild-wasm.js";
 export { esbuild };
 export { denoPlugin } from "https://raw.githubusercontent.com/lucacasonato/esbuild_deno_loader/c5ee642f4552078324badbf1b541c7222c07d5ff/mod.ts";
