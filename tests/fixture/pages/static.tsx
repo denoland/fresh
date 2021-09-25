00b005b804cfc97c5d32c0ab6579ca5e9ba75844
Luca Casonato <hello@lcas.dev>
2021-09-25 11:01:37 +0200

    BREAKING: disable runtime JS by default

--- tests/fixture/pages/static.tsx
+++ tests/fixture/pages/static.tsx
@@ -1,9 +1,7 @@
 /** @jsx h */
 
-import { h, PageConfig } from "../deps.ts";
+import { h } from "../deps.ts";
 
 export default function StaticPage() {
   return <p>This is a static page.</p>;
 }
-
-export const config: PageConfig = { runtimeJS: false };
