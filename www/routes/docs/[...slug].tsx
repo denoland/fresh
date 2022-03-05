2865203df31493d10a8049c13c170af58065e0ff
Luca Casonato <hello@lcas.dev>
2022-03-05 20:12:01 +0100

    improvements

--- www/routes/docs/[...slug].tsx
+++ www/routes/docs/[...slug].tsx
@@ -10,7 +10,12 @@ import WarningBanner from "../../components/WarningBanner.tsx";
 import { TOC } from "../../data/docs.ts";
 
 interface Data {
-  markdown: string | null;
+  page: Page | null;
+}
+
+interface Page {
+  title: string;
+  markdown: string;
 }
 
 export const handler: Handlers<Data> = {
@@ -26,16 +31,24 @@ export const handler: Handlers<Data> = {
       path = `${path}/index`;
     }
     const url = new URL(`../../../docs/${path}.md`, import.meta.url);
-    let markdown: string | null = null;
+    let page: Page | null = null;
     try {
-      markdown = await Deno.readTextFile(url);
+      const markdown = await Deno.readTextFile(url);
+      let title: string | null = null;
+      const parts = ctx.match.slug.split("/");
+      if (parts.length === 1) {
+        title = TOC[parts[0]].title;
+      } else {
+        title = TOC[parts[0]].pages!.find(([id]) => id === parts[1])![1];
+      }
+      page = { title, markdown };
     } catch (err) {
       if (!(err instanceof Deno.errors.NotFound)) {
         throw err;
       }
     }
-    const resp = ctx.render({ markdown });
-    if (markdown === null) {
+    const resp = ctx.render({ page });
+    if (page === null) {
       return new Response(resp.body, { ...resp, status: 404 });
     }
     return resp;
@@ -46,16 +59,12 @@ export default function DocsPage(props: PageProps<Data>) {
   return (
     <>
       <Head>
-        <title>fresh - The next-gen web framework.</title>
-        <meta
-          name="description"
-          content="The next-gen web framework. Just in time edge rendering, island based interactivity, and no configuration TypeScript support."
-        />
+        <title>{props.data.page?.title ?? "Not Found"} | fresh docs</title>
         <link rel="stylesheet" href="/gfm.css" />
       </Head>
       <Header />
       <NavigationBar active="/docs" />
-      <Main path={props.url.pathname} markdown={props.data.markdown} />
+      <Main path={props.url.pathname} page={props.data.page} />
       <Footer />
     </>
   );
@@ -82,37 +91,29 @@ function Header() {
   );
 }
 
-function Main(props: { path: string; markdown: string | null }) {
+function Main(props: { path: string; page: Page | null }) {
   const main = tw`mx-auto max-w-screen-lg px-4 grid grid-cols-8 gap-6`;
   return (
     <div class={main}>
       <DocsSidebar path={props.path} />
-      <Content path={props.path} markdown={props.markdown} />
+      <Content path={props.path} page={props.page} />
     </div>
   );
 }
 
-function Content(props: { path: string; markdown: string | null }) {
+function Content(props: { path: string; page: Page | null }) {
   const main = tw`col-span-6 py-8`;
   const title = tw`text(4xl gray-900) tracking-tight font-extrabold`;
   const body = tw`mt-6`;
-  let heading = "";
-  if (props.markdown !== null) {
-    const parts = props.path.slice("/docs/".length).split("/");
-    if (parts.length === 1) {
-      heading = TOC[parts[0]].title;
-    } else {
-      heading = TOC[parts[0]].pages!.find(([id]) => id === parts[1])![1];
-    }
-  } else {
-    heading = "Page not found";
-  }
-  props.markdown ??= "The article you were looking for was not found.";
-  const html = gfm.render(props.markdown);
+  props.page ??= {
+    title: "Not Found",
+    markdown: "The article you were looking for was not found.",
+  };
+  const html = gfm.render(props.page.markdown);
   return (
     <main class={main}>
       <WarningBanner />
-      <h1 class={title}>{heading}</h1>
+      <h1 class={title}>{props.page.title}</h1>
       <div
         class={`${body} markdown-body`}
         dangerouslySetInnerHTML={{ __html: html }}
