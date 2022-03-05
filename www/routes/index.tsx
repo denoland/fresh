bb7180328ecbff2e7bf9c6e884e782b7029851df
Luca Casonato <hello@lcas.dev>
2022-03-05 15:18:17 +0100

    tests: run lighthouse against preview deployments in CI (#107)

--- www/routes/index.tsx
+++ www/routes/index.tsx
@@ -26,7 +26,7 @@ export default function MainPage() {
 function Hero() {
   const container = tw`w-full h-64 flex justify-center items-center flex-col`;
   const title = tw
-    `text(4xl sm:5xl lg:6xl gray-900) sm:tracking-tight font-extrabold`;
+    `text(4xl sm:5xl lg:6xl gray-900 center) sm:tracking-tight font-extrabold`;
   const subtitle = tw`mt-4 text(2xl gray-600)`;
 
   return (
