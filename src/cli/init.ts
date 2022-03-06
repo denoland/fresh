20a662a3d69b1601a2f460c1302449f503496d2e
crowlkats <crowlkats@toaxl.com>
2022-03-06 18:23:07 +0100

    update init

--- src/cli/init.ts
+++ src/cli/init.ts
@@ -148,7 +148,7 @@ const JOKES = [
   "An SEO expert walked into a bar, pub, inn, tavern, hostelry, public house.",
 ];
 
-export const handler = (_ctx: HandlerContext): Response => {
+export const handler = (_req: Request, _ctx: HandlerContext): Response => {
   const randomIndex = Math.floor(Math.random() * 10);
   const body = JOKES[randomIndex];
   return new Response(body);
