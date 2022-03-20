49ac6af315d4029c970f5beca0adf5419ebcc991
Sylvain <sylvaincau31@gmail.com>
2022-03-20 20:32:15 +0800

    override types error

--- www/routes/_render.ts
+++ www/routes/_render.ts
@@ -5,13 +5,15 @@ import { RenderContext, RenderFn, virtualSheet } from "../server_deps.ts";
 
 const sheet = virtualSheet();
 sheet.reset();
+// @ts-expect-error `Type 'VirtualSheet' is missing the following properties from type 'Sheet<unknown>': target, insert`. A virtualSheet has these properties. ts bug ? 
 setup({ sheet });
 
 export function render(ctx: RenderContext, render: RenderFn) {
   const snapshot = ctx.state.get("twindSnapshot") as unknown[] | null;
   sheet.reset(snapshot || undefined);
   render();
-  ctx.styles.splice(0, ctx.styles.length, ...sheet.target);
+  // @ts-expect-error `Type 'VirtualSheet' is missing the following properties from type 'Sheet<unknown>': target, insert`.
+  ctx.styles.splice(0, ctx.styles.length, ...(sheet).target);
   const newSnapshot = sheet.reset();
   ctx.state.set("twindSnapshot", newSnapshot);
 }
