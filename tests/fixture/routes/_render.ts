bc0813359261697696d04ae9190db7349ba2e4a7
ʀᴀʏ <nn_201312@163.com>
2022-03-31 09:35:22 +0800

    Update _render.ts

--- tests/fixture/routes/_render.ts
+++ tests/fixture/routes/_render.ts
@@ -1,7 +1,7 @@
 import { RenderContext, RenderFn } from "../../../server.ts";
 
 export async function render(_ctx: RenderContext, render: RenderFn) {
-  await new Promise((r) => r());
+  await new Promise<void>((r) => r());
   const body = render();
   if (typeof body !== "string") {
     throw new Error("body is missing");
