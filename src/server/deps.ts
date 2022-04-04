70f5bb790f0a26de8ccef184d4a9a6b5e4b93911
Luca Casonato <hello@lcas.dev>
2022-04-04 13:51:17 +0200

    update to 0.0.11

--- src/server/deps.ts
+++ src/server/deps.ts
@@ -15,7 +15,7 @@ export type {
 } from "https://deno.land/std@0.128.0/http/server.ts";
 
 // -- router --
-export * as router from "https://crux.land/router@0.0.8";
+export * as router from "https://crux.land/router@0.0.11";
 
 // -- media types --
 export { lookup as mediaTypeLookup } from "https://deno.land/x/media_types@v2.10.2/mod.ts";
