f1a39e2e7a120f1f6c7be40c91c89b26163cfb65
hashrock <hashrock@users.noreply.github.com>
2022-04-23 22:09:35 +0900

    www: add hero animation & new logo (#153)

--- www/fresh.gen.ts
+++ www/fresh.gen.ts
@@ -7,6 +7,7 @@ import * as $1 from "./routes/docs/[...slug].tsx";
 import * as $2 from "./routes/gfm.css.ts";
 import * as $3 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
+import * as $$1 from "./islands/LemonDrop.tsx";
 
 const manifest = {
   routes: {
@@ -17,6 +18,7 @@ const manifest = {
   },
   islands: {
     "./islands/Counter.tsx": $$0,
+    "./islands/LemonDrop.tsx": $$1,
   },
   baseUrl: import.meta.url,
 };
