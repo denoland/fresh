001965d8203f5d0e53ef09d40f5aa1f059783645
Luca Casonato <hello@lcas.dev>
2022-06-02 14:05:10 +0200

    fix

--- examples/counter/fresh.gen.ts
+++ examples/counter/fresh.gen.ts
@@ -3,13 +3,11 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/index.tsx";
-import * as $1 from "./routes/index2.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/index.tsx": $0,
-    "./routes/index2.tsx": $1,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
