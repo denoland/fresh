12f8e2c2cea7b43190bb83d54b873a8123562a34
satotake <doublequotation@gmail.com>
2022-06-23 01:57:50 +0900

    init: add favicon.ico to static
    Browswers try to get `/favicon.ico` by default.
    This request enters `[name].tsx` and returns greet unexpectedly.
    To guard it, favicon is fetched from `https://fresh.deno.dev` and save into `static`.

--- init.ts
+++ init.ts
@@ -233,6 +233,13 @@ await Deno.writeTextFile(
   STATIC_LOGO,
 );
 
+const faviconArrayBuffer = await fetch("https://fresh.deno.dev/favicon.ico")
+  .then((d) => d.arrayBuffer());
+await Deno.writeFile(
+  join(directory, "static", "favicon.ico"),
+  new Uint8Array(faviconArrayBuffer),
+);
+
 let MAIN_TS = `/// <reference no-default-lib="true" />
 /// <reference lib="dom" />
 /// <reference lib="dom.asynciterable" />
