8a64c0050162a57127f1a2bb677b2e5ec85426fe
benjamingeets <benjamingeets@tutamail.com>
2022-06-25 21:36:04 +0200

    feat: test html lang

---
+++ src/server/render_test.ts
@@ -0,0 +1,14 @@
+import { template } from "./render.tsx";
+import { assertStringIncludes } from "../../tests/deps.ts";
+Deno.test("check lang", () => {
+  const lang = "fr";
+  const body = template({
+    bodyHtml: "",
+    headComponents: [],
+    imports: [],
+    preloads: [],
+    styles: [],
+    lang: lang,
+  });
+  assertStringIncludes(body, `<html lang="${lang}">`);
+});
