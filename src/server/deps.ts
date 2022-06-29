0471fdf57bf299a926808ee2ee523cc4a01df2ca
Lorenzo Signoretti <lorenzosigno@gmail.com>
2022-06-30 00:12:14 +0100

    Support port, help and hostname on `start` command

--- src/server/deps.ts
+++ src/server/deps.ts
@@ -4,6 +4,8 @@ export {
   fromFileUrl,
   toFileUrl,
 } from "https://deno.land/std@0.128.0/path/mod.ts";
+export { bold, red } from "https://deno.land/std@0.128.0/fmt/colors.ts";
+export { parse } from "https://deno.land/std@0.128.0/flags/mod.ts";
 export { walk } from "https://deno.land/std@0.128.0/fs/walk.ts";
 export { serve } from "https://deno.land/std@0.128.0/http/server.ts";
 export type {
--- src/server/mod.ts
+++ src/server/mod.ts
@@ -1,5 +1,6 @@
 import { ServerContext } from "./context.ts";
-import { serve } from "./deps.ts";
+import { serve, parse } from "./deps.ts";
+import { error } from "./error.ts";
 import {
   AppModule,
   ErrorPageModule,
@@ -45,15 +46,58 @@ export interface Manifest {
 
 export { ServerContext };
 
-export async function start(
-  routes: Manifest,
-  opts: StartOptions = {},
-) {
+type KeyOrValueOf<T> = keyof T | T[keyof T];
+
+export async function start(routes: Manifest, opts: StartOptions = {}) {
+  const aliases = {
+    p: "port",
+    H: "hostname",
+    h: "help",
+  } as const;
+
+  let args: {
+    [P in KeyOrValueOf<typeof aliases>]?: string;
+  } = {};
+
+  // Exit the process if Fresh is started with unknown options
+  try {
+    args = parse(Deno.args, {
+      alias: aliases,
+      // Treat all args as strings
+      string: Object.entries(aliases).flat(),
+      unknown: (arg) => {
+        throw new Error(`Unknown or unexpected option: ${arg}`);
+      },
+    }) as typeof args;
+  } catch (err) {
+    error(err.message);
+  }
+
+  if (args.help) {
+    console.log(`
+      DESCRIPTION:
+        Starts the application.
+
+      USAGE:
+        $ deno task start -p <port> -H <hostname>
+
+      OPTIONS:
+        --port, -p      A port number on which to start the application
+        --hostname, -H  Hostname on which to start the application (default: 0.0.0.0)
+        --help, -h      Displays this message
+    `);
+    Deno.exit(0);
+  }
+
+  const port: number =
+    (args.port && parseInt(args.port)) ||
+    (Deno.env.get("PORT") && parseInt(Deno.env.get("PORT")!)) ||
+    opts.port ||
+    8000;
+  const hostname: string =
+    args.hostname || Deno.env.get("HOSTNAME") || opts.hostname || "0.0.0.0";
+
   const ctx = await ServerContext.fromManifest(routes, opts);
-  console.log(
-    `Server listening on http://${opts?.hostname ?? "localhost"}:${
-      opts?.port ?? 8000
-    }`,
-  );
-  await serve(ctx.handler(), opts);
+  console.log(`Started server on http://${hostname || "localhost"}:${port}`);
+  await serve(ctx.handler(), { ...opts, port, hostname });
 }
