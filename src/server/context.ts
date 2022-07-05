3e4b92690a74a347a27dd630d0aeb8c0dcea33b1
un-versed <gu.bat98@gmail.com>
2022-07-05 00:05:34 -0300

    fix: add docs link to async error message

--- src/server/context.ts
+++ src/server/context.ts
@@ -402,7 +402,7 @@ export class ServerContext {
           }
 
           if (typeof route.component === 'function' && route.component.constructor.name === "AsyncFunction") {
-            throw new Error("Async components are not supported.");
+            throw new Error("Async components are not supported. Fetch data inside of the loader, as described in the docs: https://fresh.deno.dev/docs/getting-started/fetching-data");
           }
 
           const preloads: string[] = [];
