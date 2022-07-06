e6bd51c851ddf25485e6ba22453f8b96511a67ca
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2022-07-06 16:52:48 +0300

    chore: format file so it's the same as `_404.tsx`

--- www/routes/_500.tsx
+++ www/routes/_500.tsx
@@ -3,5 +3,9 @@ import { h } from "preact";
 import { ErrorPageProps } from "$fresh/server.ts";
 
 export default function Error500Page({ error }: ErrorPageProps) {
-  return <p>500 internal error: {(error as Error).message}</p>;
+  return (
+    <p>
+      500 internal error: {(error as Error).message}
+    </p>
+  );
 }
