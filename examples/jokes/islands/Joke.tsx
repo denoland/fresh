0540bd409bbcfab2c8e5023f1616e24abaaf0fc5
xyntechx <xyntechx@gmail.com>
2022-07-06 19:17:57 +0800

    Format files

--- examples/jokes/islands/Joke.tsx
+++ examples/jokes/islands/Joke.tsx
@@ -6,26 +6,25 @@ import { tw } from "@twind";
 import JOKES from "../utils/jokes.ts";
 
 interface Props {
-    index: number;
+  index: number;
 }
 
 export default function Joke(props: Props) {
-    const [index, setIndex] = useState(props.index);
-    const btn = tw`px-2 py-1 border(gray-100 1) hover:bg-yellow-100 w-max focus:outline-none mt-4`;
-    const text = tw`p-0 m-2 text-xl`;
+  const [index, setIndex] = useState(props.index);
+  const btn = tw
+    `px-2 py-1 border(gray-100 1) hover:bg-yellow-100 w-max focus:outline-none mt-4`;
+  const text = tw`p-0 m-2 text-xl`;
 
-    return (
-        <div class={tw`flex flex-col items-center justify-center`}>
-            <p class={text}>{JOKES[index]}</p>
-            <button
-                class={btn}
-                onClick={() =>
-                    setIndex(Math.floor(Math.random() * JOKES.length))
-                }
-                disabled={!IS_BROWSER}
-            >
-                Another One!
-            </button>
-        </div>
-    );
+  return (
+    <div class={tw`flex flex-col items-center justify-center`}>
+      <p class={text}>{JOKES[index]}</p>
+      <button
+        class={btn}
+        onClick={() => setIndex(Math.floor(Math.random() * JOKES.length))}
+        disabled={!IS_BROWSER}
+      >
+        Another One!
+      </button>
+    </div>
+  );
 }
