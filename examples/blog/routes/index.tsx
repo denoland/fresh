8af08db20f54db11b0d5f71a68e36e3d817f2d53
harshmangalam <harshdev8218@gmail.com>
2022-07-23 19:04:01 +0530

    added error handling

--- examples/blog/routes/index.tsx
+++ examples/blog/routes/index.tsx
@@ -8,29 +8,35 @@ import { Marked } from "https://deno.land/x/markdown@v2.0.0/mod.ts";
 import { Post } from "../types/blog.ts";
 export const handler: Handlers = {
   async GET(req, ctx) {
-    const BLOG_DIR = "blog";
-    const posts = [];
-    // async iterator to get all md files in blog dir
-    for await (const dir of Deno.readDir(BLOG_DIR)) {
-      if (dir.isFile) {
-        // utf-8 text decoder
-        const decoder = new TextDecoder("utf-8");
-        // decode md file
-        const markdown = decoder.decode(
-          await Deno.readFile(`${BLOG_DIR}/${dir.name}`)
-        );
-        // parse md file into metadata and content
-        const markup = Marked.parse(markdown);
-        posts.push(markup.meta);
+    try {
+      const BLOG_DIR = "blog";
+      const posts = [];
+      // async iterator to get all md files in blog dir
+      for await (const dir of Deno.readDir(BLOG_DIR)) {
+        if (dir.isFile) {
+          // utf-8 text decoder
+          const decoder = new TextDecoder("utf-8");
+          // decode md file
+          const markdown = decoder.decode(
+            await Deno.readFile(`${BLOG_DIR}/${dir.name}`)
+          );
+          // parse md file into metadata and content
+          const markup = Marked.parse(markdown);
+          posts.push(markup.meta);
+        }
       }
+      return ctx.render({ posts });
+    } catch (error) {
+      return ctx.render({ error: error.message });
     }
-    return ctx.render({ posts });
   },
 };
 export default function Blog({ data }: PageProps) {
   const posts: Post[] = data.posts;
+  const error = data?.error;
   return (
     <Layout title="Blog">
+      {error && <p>{error}</p>}
       <div>
         <section className={tw`grid grid-cols-1 gap-4 md:grid-cols-2`}>
           {posts.map((post) => (
