b39608890e354f9899b237f909ac25c3289d9e11
Luca Casonato <hello@lcas.dev>
2022-07-30 16:59:35 +0200

    chore: run `deno fmt` with new deno version (#562)

--- www/components/Button.tsx
+++ www/components/Button.tsx
@@ -9,8 +9,7 @@ export function RoundedButton(props: h.JSX.HTMLAttributes<HTMLButtonElement>) {
         touchAction: "manipulation",
       }}
       {...props}
-      class={tw
-        `p-3 border border-transparent rounded-full text-white bg-green(500 hover:600) focus:(outline-none ring(2 offset-2 green-500)) disabled:(bg-green-200 cursor-default)`}
+      class={tw`p-3 border border-transparent rounded-full text-white bg-green(500 hover:600) focus:(outline-none ring(2 offset-2 green-500)) disabled:(bg-green-200 cursor-default)`}
     />
   );
 }
