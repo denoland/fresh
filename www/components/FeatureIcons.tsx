005619793691d63b95bf5e76652acfc9a381068d
hashrock <hashrock@users.noreply.github.com>
2022-08-02 17:13:14 +0900

    www: fix icon size

--- www/components/FeatureIcons.tsx
+++ www/components/FeatureIcons.tsx
@@ -145,7 +145,7 @@ export function Island() {
 export function Globe() {
   return (
     <svg
-      class={tw`md:bg-green-200 rounded-full w-19 h-20`}
+      class={tw`md:bg-green-200 rounded-full w-20 h-20`}
       viewBox="0 0 75 75"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
