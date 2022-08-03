94b49ead35ce377faa52a18e02b40d3672e45d8c
moonstripe <kojinglick@gmail.com>
2022-08-03 10:27:48 -0700

    just typo fixes

--- examples/seo/utils/twind.ts
+++
@@ -1,8 +0,0 @@
-import { IS_BROWSER } from "$fresh/runtime.ts";
-import { Configuration, setup } from "twind";
-export * from "twind";
-export const config: Configuration = {
-  darkMode: "class",
-  mode: "silent",
-};
-if (IS_BROWSER) setup(config);
