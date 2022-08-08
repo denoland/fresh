5b9ce720d0aab789026409f775b1305258e0d68a
Kid <44045911+kidonng@users.noreply.github.com>
2022-08-09 05:19:26 +0800

    chore: remove `deno.unstable` references (#600)

--- init.ts
+++ init.ts
@@ -276,7 +276,6 @@ let MAIN_TS = `/// <reference no-default-lib="true" />
 /// <reference lib="dom" />
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
-/// <reference lib="deno.unstable" />
 
 import { ${
   useTwind ? "InnerRenderFunction, RenderContext, " : ""
