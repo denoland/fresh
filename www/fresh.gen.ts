3805ef0d4e244ba7227f24b28e8fad12a07d8e10
stracciatella27 <schoko.bombe44@gmail.com>
2022-08-15 18:28:39 +0200

    Add 404 and 500 page

--- www/fresh.gen.ts
+++ www/fresh.gen.ts
@@ -2,20 +2,24 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/docs/[...slug].tsx";
-import * as $1 from "./routes/gfm.css.ts";
-import * as $2 from "./routes/index.tsx";
-import * as $3 from "./routes/raw.ts";
+import * as $0 from "./routes/_404.tsx";
+import * as $1 from "./routes/_500.tsx";
+import * as $2 from "./routes/docs/[...slug].tsx";
+import * as $3 from "./routes/gfm.css.ts";
+import * as $4 from "./routes/index.tsx";
+import * as $5 from "./routes/raw.ts";
 import * as $$0 from "./islands/CopyArea.tsx";
 import * as $$1 from "./islands/Counter.tsx";
 import * as $$2 from "./islands/LemonDrop.tsx";
 
 const manifest = {
   routes: {
-    "./routes/docs/[...slug].tsx": $0,
-    "./routes/gfm.css.ts": $1,
-    "./routes/index.tsx": $2,
-    "./routes/raw.ts": $3,
+    "./routes/_404.tsx": $0,
+    "./routes/_500.tsx": $1,
+    "./routes/docs/[...slug].tsx": $2,
+    "./routes/gfm.css.ts": $3,
+    "./routes/index.tsx": $4,
+    "./routes/raw.ts": $5,
   },
   islands: {
     "./islands/CopyArea.tsx": $$0,
