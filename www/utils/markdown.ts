31cb83829a40a3348d26ee5249b3c94ddf7a7745
Benny Yen <benny123tw@gmail.com>
2022-08-15 21:07:50 +0800

    www: update deps
    frontmatter -> std/encoding/front_matter

--- www/utils/markdown.ts
+++ www/utils/markdown.ts
@@ -3,4 +3,4 @@ import "https://esm.sh/prismjs@1.27.0/components/prism-jsx.js?no-check";
 import "https://esm.sh/prismjs@1.27.0/components/prism-typescript.js?no-check";
 import "https://esm.sh/prismjs@1.27.0/components/prism-tsx.js?no-check";
 
-export { parse as frontMatter } from "https://deno.land/x/frontmatter@v0.1.4/mod.ts";
+export { extract as frontMatter } from "$std/encoding/front_matter.ts";
