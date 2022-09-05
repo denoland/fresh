5a63e8fa11f7b575cc771887b296d26f9cf58212
Luca Casonato <hello@lcas.dev>
2022-09-06 01:09:58 +0200

    x

--- www/islands/Counter.tsx
+++ www/islands/Counter.tsx
@@ -1,4 +1,4 @@
-import { useState } from "preact/hooks";
+import { useSignal } from "@preact/signals";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { RoundedButton } from "../components/Button.tsx";
 import { IconMinus, IconPlus } from "../components/Icons.tsx";
@@ -8,20 +8,20 @@ interface CounterProps {
 }
 
 export default function Counter(props: CounterProps) {
-  const [count, setCount] = useState(props.start);
+  const count = useSignal(props.start);
   return (
     <div class="bg-gray-100 p-4 border border-gray-200 flex items-center justify-around">
       <RoundedButton
         title="Subtract 1"
-        onClick={() => setCount(count - 1)}
-        disabled={!IS_BROWSER || count <= 0}
+        onClick={() => count.value--}
+        disabled={!IS_BROWSER || count.value <= 0}
       >
         <IconMinus />
       </RoundedButton>
       <div class="text-3xl tabular-nums">{count}</div>
       <RoundedButton
         title="Add 1"
-        onClick={() => setCount(count + 1)}
+        onClick={() => count.value++}
         disabled={!IS_BROWSER}
       >
         <IconPlus />
