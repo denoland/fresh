982822948de26f78132f6eaa632bb8e572978fd5
MrSaeedNasiri <mr.nasiri.saeed@gmail.com>
2022-09-08 19:01:01 +0430

    format todo example

--- examples/todo/components/Task.tsx
+++ examples/todo/components/Task.tsx
@@ -4,10 +4,15 @@ import { IS_BROWSER } from "$fresh/runtime.ts";
 export function Task(props) {
   return (
     <div class="w-full bg-gray-50 h-16 text-black rounded shadow flex justify-between items-center content-between">
-        <p class="p-2 w-5/6">
-          {props.task}
-        </p>
-        <button onClick={()=>props.removeTask(props.task)} class="w-1/6 bg-red-100 h-full">Done</button>
+      <p class="p-2 w-5/6">
+        {props.task}
+      </p>
+      <button
+        onClick={() => props.removeTask(props.task)}
+        class="w-1/6 bg-red-100 h-full"
+      >
+        Done
+      </button>
     </div>
   );
 }
--- examples/todo/islands/Todo.tsx
+++ examples/todo/islands/Todo.tsx
@@ -7,22 +7,35 @@ interface TodoProps {
 
 export default function Todo() {
   const [tasks, setTasks] = useState([]);
-  const [task, setTask] = useState('');
-  function removeTask(s){
-    setTasks((p) => p.filter((e) => e !=s))
+  const [task, setTask] = useState("");
+  function removeTask(s) {
+    setTasks((p) => p.filter((e) => e != s));
   }
   return (
     <div class="flex flex-col w-full">
-        <form class="flex gap-2 w-full" onSubmit={(e)=>{
-            e.preventDefault();
-            setTasks((p)=> [...p, task]);
-            setTask("");
-            }}>
-            <input class="w-5/6 border-1 border-gray-500 h-16 rounded p-2" placeholder="Write your task here..." type="text" value={task} onInput={(e)=>setTask(e.target.value)}/>
-            <input type="submit" value="Add" class="w-1/6 bg-blue-600 text-gray-50 rounded"></input>
-        </form>
-        <Tasks tasks={tasks} removeTask={removeTask} />
+      <form
+        class="flex gap-2 w-full"
+        onSubmit={(e) => {
+          e.preventDefault();
+          setTasks((p) => [...p, task]);
+          setTask("");
+        }}
+      >
+        <input
+          class="w-5/6 border-1 border-gray-500 h-16 rounded p-2"
+          placeholder="Write your task here..."
+          type="text"
+          value={task}
+          onInput={(e) => setTask(e.target.value)}
+        />
+        <input
+          type="submit"
+          value="Add"
+          class="w-1/6 bg-blue-600 text-gray-50 rounded"
+        >
+        </input>
+      </form>
+      <Tasks tasks={tasks} removeTask={removeTask} />
     </div>
-
   );
 }
