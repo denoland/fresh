37b3195677c8daf56ca13f4fa7df9e0a8bf00895
hashrock <hashrock@users.noreply.github.com>
2022-09-09 03:50:18 +0900

    convert screenshots to jpeg

--- www/utils/screenshot.ts
+++ www/utils/screenshot.ts
@@ -1,4 +1,5 @@
 import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
+import { Image } from "https://deno.land/x/imagescript@v1.2.14/mod.ts";
 
 const url = Deno.args[0];
 const id = Deno.args[1];
@@ -13,6 +14,18 @@ const browser = await puppeteer.launch({
 });
 const page = await browser.newPage();
 await page.goto(url, { waitUntil: "networkidle2" });
-await page.screenshot({ path: `./www/static/showcase/${id}.png` });
+const pngPath = `./www/static/showcase/${id}.png`;
+await page.screenshot({ path: pngPath });
 
 await browser.close();
+
+// convert to jpeg
+const image2x = await Image.decode(await Deno.readFile(pngPath));
+const jpeg2x = `./www/static/showcase/${id}2x.jpg`;
+await Deno.writeFile(jpeg2x, await image2x.encodeJPEG(80));
+
+const jpeg1x = `./www/static/showcase/${id}1x.jpg`;
+const image1x = image2x.resize(image2x.width / 2, Image.RESIZE_AUTO);
+await Deno.writeFile(jpeg1x, await image1x.encodeJPEG(80));
+
+await Deno.removeSync(pngPath);
