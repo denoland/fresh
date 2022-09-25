93d3f8b36eddd5b2b4ce789315a9d258e2100fcd
Maciej Ładoś <maciej@lados.dev>
2022-09-25 20:09:03 +0200

    chore: prepare feature fixture

---
+++ tests/fixture_throw_response/routes/index.tsx
@@ -0,0 +1,14 @@
+import { Handler } from "../../../server.ts"
+
+export const handler: Handler = () => {
+  throw new Response("<html><body>Intercepted</body></html>", { 
+    status: 200,
+    headers: {
+      "Content-Type": "text/html"
+    }
+  })
+}
+
+export default function Page() {
+  return <p>Not Intercepted</p>
+}
---
+++ tests/fixture_throw_response/routes/nested/_middleware.ts
@@ -0,0 +1,10 @@
+import { MiddlewareHandler } from "../../../../server.ts";
+
+export const handler: MiddlewareHandler = () => {
+  throw new Response("<html><body>Intercepted</body></html>", { 
+    status: 200,
+    headers: {
+      "Content-Type": "text/html"
+    }
+  })
+}
