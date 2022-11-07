90a06c621dc9be8f47bce0fdaa525015889d8ca9
hashrock <hashrock@users.noreply.github.com>
2022-11-08 01:49:18 +0900

    fix(gallery): copy botton position with Safari and FF

--- www/components/gallery/CodeBox.tsx
+++ www/components/gallery/CodeBox.tsx
@@ -25,18 +25,19 @@ export default function CodeBox(props: CodeBoxProps) {
         <summary class="text-center cursor-pointer bg-gray-50 select-none text-gray-600 hover:text-gray-800 hover:bg-gray-100 py-2 rounded-b">
           Code
         </summary>
-        <code class="relative">
-          <pre
-            class="bg-gray-800 text-blue-300 p-2 text-sm rounded whitespace-pre-wrap"
-            dangerouslySetInnerHTML={{ __html: highlighted }}
-          />
+        <div class="relative">
+          <pre class="bg-gray-800 text-blue-300 p-2 text-sm rounded whitespace-pre-wrap">
+            <code
+              dangerouslySetInnerHTML={{ __html: highlighted }}
+            />
+          </pre>
           <button
             onClick={onCopy}
-            class="absolute top-2 right-2 px-3 py-2 border border-gray-400 rounded text-white"
+            class="absolute top-2 right-2 px-3 py-2 border border-gray-400 rounded text-white bg-gray-800"
           >
             {copied ? "Copied!" : "Copy"}
           </button>
-        </code>
+        </div>
       </details>
     </div>
   );
