81d73ba0a64b8bc1295548df81e2e1a92c542f89
Paul <dev@psimk.dev>
2022-12-01 18:52:00 +0200

    www: add social links to navigation (#821)
    resolves https://github.com/denoland/fresh/issues/818
    Co-authored-by: hashrock <hashrock@users.noreply.github.com>

--- www/components/NavigationBar.tsx
+++ www/components/NavigationBar.tsx
@@ -1,3 +1,5 @@
+import * as Icons from "./Icons.tsx";
+
 export default function NavigationBar(
   props: { active: string; class?: string },
 ) {
@@ -18,7 +20,7 @@ export default function NavigationBar(
   const isHome = props.active == "/";
   return (
     <nav class={"flex " + props.class ?? ""}>
-      <ul class="flex justify-center items-center gap-4 mx-4 my-6">
+      <ul class="flex justify-center items-center gap-4 mx-4 my-6 flex-wrap">
         {items.map((item) => (
           <li>
             <a
@@ -33,6 +35,23 @@ export default function NavigationBar(
             </a>
           </li>
         ))}
+
+        <li class="flex items-center">
+          <a
+            href="https://github.com/denoland/fresh"
+            class="hover:text-green-600 inline-block"
+          >
+            <Icons.GitHub />
+          </a>
+        </li>
+        <li class="flex items-center">
+          <a
+            href="https://discord.com/channels/684898665143206084/991511118524715139"
+            class="hover:text-green-600 inline-block"
+          >
+            <Icons.Discord />
+          </a>
+        </li>
       </ul>
     </nav>
   );
