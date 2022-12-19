4da878e7062b95051e95da92713392aba6162c04
crowlkats <crowlkats@toaxl.com>
2022-12-19 16:32:32 +0100

    update more

--- tests/deps.ts
+++ tests/deps.ts
@@ -8,9 +8,9 @@ export {
   assert,
   assertEquals,
   assertStringIncludes,
-} from "https://deno.land/std@0.150.0/testing/asserts.ts";
+} from "https://deno.land/std@0.170.0/testing/asserts.ts";
 export {
   TextLineStream,
-} from "https://deno.land/std@0.150.0/streams/delimiter.ts";
-export { delay } from "https://deno.land/std@0.150.0/async/delay.ts";
+} from "https://deno.land/std@0.170.0/streams/delimiter.ts";
+export { delay } from "https://deno.land/std@0.170.0/async/delay.ts";
 export { default as puppeteer } from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
