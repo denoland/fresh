409cde750d4893d14f287bb8e5258000f39a0268
iccee0 <git@roeh.ch>
2023-02-01 09:25:44 +0000

    Removed test  plugins

--- www/main.ts
+++ www/main.ts
@@ -6,12 +6,10 @@
 
 import { start } from "$fresh/server.ts";
 import twindPlugin from "$fresh/plugins/twind.ts";
-import ga4Plugin from "$fresh/plugins/ga4.ts";
 
 import manifest from "./fresh.gen.ts";
 import twindConfig from "./twind.config.ts";
 
-const GA4_MEASUREMENT_ID = Deno.env.get("GA4_MEASUREMENT_ID")  || "";
 
-await start(manifest, { plugins: [ twindPlugin(twindConfig), ga4Plugin(GA4_MEASUREMENT_ID) ] });
+await start(manifest, { plugins: [ twindPlugin(twindConfig) ] });
 
