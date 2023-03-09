efb1059324aebd9986977231a4737f94c925a8a1
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-03-09 12:08:52 -0800

    fix broken dep

--- tests/deps.ts
+++ tests/deps.ts
@@ -11,7 +11,7 @@ export {
 } from "https://deno.land/std@0.178.0/testing/asserts.ts";
 export {
   TextLineStream,
-} from "https://deno.land/std@0.178.0/streams/delimiter.ts";
+} from "https://deno.land/std@0.178.0/streams/text_line_stream.ts";
 export { delay } from "https://deno.land/std@0.178.0/async/delay.ts";
 export { retry } from "https://deno.land/std@0.178.0/async/retry.ts";
 export { default as puppeteer } from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
