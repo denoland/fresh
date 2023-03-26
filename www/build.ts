4576d99417333fc5224cb7331f16a83200e55521
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-03-26 17:46:03 -0500

    fmt

--- www/build.ts
+++ www/build.ts
@@ -5,23 +5,24 @@ import twindPlugin from "$fresh/plugins/twind.ts";
 import manifest from "./fresh.gen.ts";
 import twindConfig from "./twind.config.ts";
 
-const ctx = await ServerContext.fromManifest(manifest, { plugins: [twindPlugin(twindConfig)] });
+const ctx = await ServerContext.fromManifest(manifest, {
+  plugins: [twindPlugin(twindConfig)],
+});
 
-const bundle = await ctx.bundler.cache()
+const bundle = await ctx.bundler.cache();
 
 try {
-  await Deno.remove("./_frsh", {recursive: true})
-}
-catch(err) {
-  if(!(err instanceof Deno.errors.NotFound)) {
-    throw err
+  await Deno.remove("./_frsh", { recursive: true });
+} catch (err) {
+  if (!(err instanceof Deno.errors.NotFound)) {
+    throw err;
   }
 }
 
 await Deno.mkdir("./_frsh", {
-  recursive: true
-})
+  recursive: true,
+});
 
-for(const [filename, contents] of bundle) {
-  await Deno.writeFile("./_frsh"+filename, contents);
+for (const [filename, contents] of bundle) {
+  await Deno.writeFile("./_frsh" + filename, contents);
 }
