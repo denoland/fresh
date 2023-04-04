889510546bf1bf340a1f1e73e80a77c06a7bb582
Yoshiya Hinosawa <stibium121@gmail.com>
2023-04-04 12:47:59 +0900

    chore: add TODO label

--- tests/main_test.ts
+++ tests/main_test.ts
@@ -555,7 +555,7 @@ Deno.test("experimental Deno.serve", {
     assert(body.startsWith("bar"));
     const etag = resp.headers.get("etag");
     assert(etag);
-    // Enable this assertion when new Deno.serve is released.
+    // TODO(kt3k): Enable this assertion when new Deno.serve is released.
     // https://github.com/denoland/deno/pull/18568
     // assert(etag.startsWith("W/"), "etag should be weak");
     assertEquals(resp.headers.get("content-type"), "text/plain");
