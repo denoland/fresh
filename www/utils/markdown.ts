e0cb693c0fb2cb6b7a031b14445a53e6f15993ee
Asher Gomez <ashersaupingomez@gmail.com>
2023-05-27 17:36:15 +1000

    chore(deps): update to std@0.189.0 (#1201)

--- www/utils/markdown.ts
+++ www/utils/markdown.ts
@@ -4,4 +4,4 @@ import "https://esm.sh/prismjs@1.29.0/components/prism-typescript.js?no-check";
 import "https://esm.sh/prismjs@1.29.0/components/prism-tsx.js?no-check";
 import "https://esm.sh/prismjs@1.29.0/components/prism-diff.js?no-check";
 
-export { extract as frontMatter } from "$std/encoding/front_matter/yaml.ts";
+export { extract as frontMatter } from "$std/front_matter/yaml.ts";
