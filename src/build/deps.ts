3857a49f1cbfa4e696394c41b975d42af5d938e0
Luca Casonato <hello@lcas.dev>
2023-06-03 10:24:15 +0200

    chore: clean up esbuild builder logic (#1231)
    This will allow us to add various caches and similar around the esbuild
    builder easily in follow up PRs.
    
    It also makes doing an AOT build-step + load assets from disk much
    simpler.

---
+++ src/build/deps.ts
@@ -0,0 +1,20 @@
+// -- $std --
+export {
+  fromFileUrl,
+  toFileUrl,
+} from "https://deno.land/std@0.189.0/path/mod.ts";
+export { escape as regexpEscape } from "https://deno.land/std@0.189.0/regexp/escape.ts";
+
+// -- esbuild --
+// @deno-types="https://deno.land/x/esbuild@v0.17.11/mod.d.ts"
+import * as esbuildWasm from "https://deno.land/x/esbuild@v0.17.11/wasm.js";
+import * as esbuildNative from "https://deno.land/x/esbuild@v0.17.11/mod.js";
+// @ts-ignore trust me
+// deno-lint-ignore no-deprecated-deno-api
+const esbuild: typeof esbuildWasm = Deno.run === undefined
+  ? esbuildWasm
+  : esbuildNative;
+const esbuildWasmURL = new URL("./esbuild_v0.17.11.wasm", import.meta.url).href;
+export { esbuild, esbuildWasm as esbuildTypes, esbuildWasmURL };
+
+export { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.7.0/mod.ts";
