83eb2a824729315d26435926f4dc7b4a55e2dde1
Luca Casonato <hello@lcas.dev>
2023-06-15 21:16:56 +0200

    www: actually link to 1.2

--- www/routes/index.tsx
+++ www/routes/index.tsx
@@ -68,7 +68,7 @@ function HelloBar() {
   return (
     <a
       class="bg-green-400 text-black border(b green-500) p-3 text-center group"
-      href="https://deno.com/blog/fresh-1.1"
+      href="https://deno.com/blog/fresh-1.2"
     >
       <b>Fresh v1.2</b> has been released with <b>improved islands</b>,{" "}
       <b>preview npm: support</b>, and more!{"  "}
