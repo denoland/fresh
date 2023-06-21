9b98e13e29562f4866b95cff151b98ee14e2a0b7
EBIBO <stu995106@hotmail.com.tw>
2023-06-21 03:27:18 -0400

    fix: raise minimum deno version to 1.31.0 (#1333)
    Fresh uses `Deno.Command` which is not available in older versions of Deno.

--- src/dev/mod.ts
+++ src/dev/mod.ts
@@ -1,7 +1,7 @@
 import { dirname, fromFileUrl, gte, join, toFileUrl, walk } from "./deps.ts";
 import { error } from "./error.ts";
 
-const MIN_DENO_VERSION = "1.25.0";
+const MIN_DENO_VERSION = "1.31.0";
 
 export function ensureMinDenoVersion() {
   // Check that the minimum supported Deno version is being used.
