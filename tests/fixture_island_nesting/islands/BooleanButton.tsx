78ef4b0f6e3454db1471f9ec55d7f134abeccde7
Marvin Hagemeister <hello@marvinh.dev>
2023-06-21 19:58:26 +0200

    fix: conditional sibling islands DOM order
    This happened when both share the same parent and one or both render
    nothing into the DOM.

---
+++ tests/fixture_island_nesting/islands/BooleanButton.tsx
@@ -0,0 +1,13 @@
+import { Signal } from "@preact/signals";
+
+export default function BooleanButton({ signal }: { signal: Signal }) {
+  return (
+    <button
+      onClick={() => {
+        signal.value = !signal.value;
+      }}
+    >
+      Toggle
+    </button>
+  );
+}
