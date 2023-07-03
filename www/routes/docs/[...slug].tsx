50e3a253c9fed0951c72269bb13dd249ccea9a0e
ayame113 <40050810+ayame113@users.noreply.github.com>
2023-07-03 18:00:59 +0900

    use view transition in docs

--- www/routes/docs/[...slug].tsx
+++ www/routes/docs/[...slug].tsx
@@ -70,6 +70,7 @@ export default function DocsPage(props: PageProps<Data>) {
         <meta property="og:type" content="website" />
         <meta property="og:url" content={props.url.href} />
         <meta property="og:image" content={ogImageUrl} />
+        <meta name="view-transition" content="same-origin" />
       </Head>
       <div class="flex flex-col min-h-screen">
         <Header title="docs" active="/docs" />
