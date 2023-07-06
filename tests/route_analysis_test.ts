531d54b16189cf164b9f264848948c9dd6a48c31
Reed von Redwitz <reed+github@vonredwitz.com>
2023-07-06 12:28:38 +0200

    refactor test to actually fail when fix isn't applied

--- tests/route_analysis_test.ts
+++ tests/route_analysis_test.ts
@@ -1,23 +1,17 @@
+import { startFreshServerExpectErrors } from "./test_utils.ts";
+import { dirname, join } from "$std/path/mod.ts";
 import { assertStringIncludes } from "./deps.ts";
+
+const dir = dirname(import.meta.url);
 
 Deno.test({
   name: "route-conflicts",
   async fn() {
-    const cliProcess = new Deno.Command(Deno.execPath(), {
-      args: [
-        "run",
-        "-A",
-        `tests/fixture_route_analysis/dev.ts`,
-      ],
-      stdin: "null",
-      stdout: "piped",
-      stderr: "piped",
+    const errorMessage = await startFreshServerExpectErrors({
+      args: ["run", "-A", join(dir, "./fixture_route_analysis/dev.ts")],
     });
-
-    const { stderr } = await cliProcess.output();
-    const stderrDecoded = new TextDecoder().decode(stderr);
     assertStringIncludes(
-      stderrDecoded,
+      errorMessage,
       "Error: Route conflict detected. Multiple files have the same name",
     );
   },
