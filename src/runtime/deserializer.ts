82b0f2c95a715c43a4a0e4b41b4648c3c0747f29
Igor Brasileiro <brasileiro456@gmail.com>
2023-07-19 14:52:55 -0300

    fix: Prevent deserialize break on safari < 15.4 (#1483)

--- src/runtime/deserializer.ts
+++ src/runtime/deserializer.ts
@@ -56,7 +56,7 @@ export function deserialize(
         (o, k) => k === null ? o : o[k],
         v,
       );
-      parent[refPath.at(-1)!] = target;
+      parent[refPath[refPath.length - 1]!] = target;
     }
   }
   return v;
