a5822a875473683a042ae631454797bb17a0d0a9
Marvin Hagemeister <marvin@deno.com>
2023-07-25 11:17:40 +0200

    fix: parse multiple route params in same part (#1518)

--- tests/route_analysis_test.ts
+++ tests/route_analysis_test.ts
@@ -1,6 +1,9 @@
 import { startFreshServerExpectErrors } from "./test_utils.ts";
 import { dirname, join } from "$std/path/mod.ts";
 import { assertStringIncludes } from "./deps.ts";
+import { ServerContext } from "$fresh/server.ts";
+import manifest from "./fixture/fresh.gen.ts";
+import { assertEquals } from "$std/testing/asserts.ts";
 
 const dir = dirname(import.meta.url);
 
@@ -15,4 +18,26 @@ Deno.test({
       "Error: Route conflict detected. Multiple files have the same name",
     );
   },
+});
+
+Deno.test("match route parameter and static", async () => {
+  const handler = (await ServerContext.fromManifest(manifest, {})).handler();
+
+  const res = await handler(
+    new Request("https://fresh.deno.dev/movies/foo.json"),
+  );
+
+  assertEquals(await res.text(), "it works");
+  assertEquals(res.status, 200);
+});
+
+Deno.test("match multiple route parameters", async () => {
+  const handler = (await ServerContext.fromManifest(manifest, {})).handler();
+
+  const res = await handler(
+    new Request("https://fresh.deno.dev/movies/foo@bar"),
+  );
+
+  assertEquals(await res.text(), "it works");
+  assertEquals(res.status, 200);
 });
