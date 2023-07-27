5a816e4b680f8addfce90d85d4e9dbe476ff11a0
Reed von Redwitz <reed+github@vonredwitz.com>
2023-07-27 11:22:12 +0200

    uptake typing for AppProps in tests

--- tests/fixture/routes/state-in-props/_middleware.ts
+++ tests/fixture/routes/state-in-props/_middleware.ts
@@ -1,8 +1,9 @@
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
+import { TestState } from "../_app.tsx";
 
 export async function handler(
   _req: Request,
-  ctx: MiddlewareHandlerContext,
+  ctx: MiddlewareHandlerContext<TestState>,
 ) {
   ctx.state.stateInProps = "look, i am set from middleware";
   const resp = await ctx.next();
