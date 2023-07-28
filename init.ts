42730bafe36dae1aa48b29344e63fe04043684a1
Reed von Redwitz <reed+github@vonredwitz.com>
2023-07-28 09:12:37 +0200

    rework to add separate safe command

--- init.ts
+++ init.ts
@@ -537,7 +537,8 @@ try {
 const config = {
   lock: false,
   tasks: {
-    start: "deno check main.ts && deno run -A --watch=static/,routes/ dev.ts",
+    safe: "deno fmt --check && deno lint && deno check **/*.ts",
+    start: "deno run -A --watch=static/,routes/ dev.ts",
     update: "deno run -A -r https://fresh.deno.dev/update .",
   },
   lint: {
