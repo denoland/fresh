07d19c69e41338baf0abc6a9f1118846b2445741
Michael Herzner <michael.b.herzner@gmail.com>
2023-08-01 20:45:39 +0200

    chore: add @ts-expect-error to fixture

--- tests/fixture_dev_checks/main.ts
+++ tests/fixture_dev_checks/main.ts
@@ -9,6 +9,8 @@ import manifest from "./fresh.gen.ts";
 import notCallingCtxRender from "./plugins/not_calling_ctx_render.ts";
 import notHavingModules from "./plugins/not_having_modules.ts";
 
+// @ts-expect-error this project is meant to be configured incorrectly
+// both routes/_app.tsx and routes/test.tsx are invalid here
 await start(manifest, {
   plugins: [notHavingModules(), notCallingCtxRender()],
   staticDir: "./custom_static",
