7d4c7511055b1285e4907ec2afab10f21d7f31a3
Christian Svensson <csvn.dev@gmail.com>
2023-08-08 14:15:53 +0200

    fix: add missing state property to interface (#1579)

--- src/server/types.ts
+++ src/server/types.ts
@@ -195,7 +195,7 @@ export interface LayoutRoute {
 // --- UNKNOWN PAGE ---
 
 // deno-lint-ignore no-explicit-any
-export interface UnknownPageProps<T = any> {
+export interface UnknownPageProps<T = any, S = Record<string, unknown>> {
   /** The URL of the request that resulted in this page being rendered. */
   url: URL;
 
@@ -208,6 +208,7 @@ export interface UnknownPageProps<T = any> {
    * `undefined`.
    */
   data: T;
+  state: S;
 }
 
 export interface UnknownHandlerContext<State = Record<string, unknown>>
--- tests/fixture/routes/_404.tsx
+++ tests/fixture/routes/_404.tsx
@@ -1,10 +1,19 @@
 import { UnknownPageProps } from "$fresh/server.ts";
 
-export default function NotFoundPage({ data, url }: UnknownPageProps) {
+type Data = { hello: string };
+type State = { root: string };
+
+export default function NotFoundPage(
+  { data, state, url }: UnknownPageProps<Data | undefined, State>,
+) {
+  // Checks that we have the correct type for state
+  state.root satisfies string;
+
   return (
     <>
       <p>404 not found: {url.pathname}</p>
       {data?.hello && <p>Hello {data.hello}</p>}
+      <p>State root: {state.root}</p>
     </>
   );
 }
