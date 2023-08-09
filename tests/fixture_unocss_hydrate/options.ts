f6f204b94a5c89947c7c9f69e7b064dfbf31aa0c
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-08-09 23:57:13 +0200

    chore(plugins): order imports

--- tests/fixture_unocss_hydrate/options.ts
+++ tests/fixture_unocss_hydrate/options.ts
@@ -1,7 +1,7 @@
+import type { UserConfig } from "https://esm.sh/@unocss/core@0.53.1";
 import { FreshOptions } from "$fresh/server.ts";
 import unocssPlugin from "$fresh/plugins/unocss.ts";
 import unocssConfig from "./uno.config.ts";
-import type { UserConfig } from "https://esm.sh/@unocss/core@0.53.1";
 
 export default {
   plugins: [unocssPlugin(unocssConfig as UserConfig)],
