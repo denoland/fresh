c4c901bbca5b4fe725e608a289dbe990596cd66f
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-08-09 23:56:51 +0200

    chore(ci): try fixing broken types causing CI to fail

--- tests/fixture_unocss_hydrate/options.ts
+++ tests/fixture_unocss_hydrate/options.ts
@@ -1,5 +1,8 @@
 import { FreshOptions } from "$fresh/server.ts";
 import unocssPlugin from "$fresh/plugins/unocss.ts";
 import unocssConfig from "./uno.config.ts";
+import type { UserConfig } from "https://esm.sh/@unocss/core@0.53.1";
 
-export default { plugins: [unocssPlugin(unocssConfig)] } as FreshOptions;
+export default {
+  plugins: [unocssPlugin(unocssConfig as UserConfig)],
+} as FreshOptions;
