a7243b15e1bf98b4e6bf5ade503adda5dc9209ce
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-08-10 08:04:06 +0200

    feat(plugins): add client runtime script to unocss plugin by default

---
+++ plugins/unocss/main.ts
@@ -0,0 +1,5 @@
+export default function main() {
+  const script = document.createElement("script");
+  script.src = "https://cdn.jsdelivr.net/npm/@unocss/runtime";
+  document.head.appendChild(script);
+}
