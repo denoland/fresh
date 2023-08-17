ab5243c39a47fe484baf8276c21a0808332a1039
Asher Gomez <ashersaupingomez@gmail.com>
2023-08-17 10:42:29 +1000

    docs(www): update hello bar for Fresh 1.4

--- www/routes/index.tsx
+++ www/routes/index.tsx
@@ -77,11 +77,10 @@ function HelloBar() {
   return (
     <a
       class="bg-green-400 text-black border(b green-500) p-3 text-center group"
-      href="https://deno.com/blog/fresh-1.3"
+      href="https://deno.com/blog/fresh-1.4"
     >
-      <b>Fresh v1.3</b> has been released with <b>async route components</b>,
-      {" "}
-      <b>plugin enhancements</b>, and more!{"  "}
+      <b>Fresh v1.4</b> has been released with <b>faster page loads</b>,{" "}
+      <b>layouts</b>, and more!{"  "}
       <span class="group-hover:underline">→</span>
     </a>
   );
