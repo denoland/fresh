5f50efef6be1d1d34797ca17937dbe41e17652aa
Adam Gregory <adam@aguilaengineering.com>
2023-08-17 12:25:32 +0100

    Import @unocss/preset-uno from full URL in test

--- tests/fixture_unocss_hydrate/uno.config.ts
+++ tests/fixture_unocss_hydrate/uno.config.ts
@@ -1,5 +1,5 @@
 import type { UserConfig } from "$fresh/plugins/unocss.ts";
-import presetUno from "@unocss/preset-uno";
+import presetUno from "https://esm.sh/@unocss/preset-uno@0.54.2";
 
 export default {
   presets: [presetUno()],
