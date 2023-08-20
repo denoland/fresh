5adfdcddd1618448f1d372dc284437a42f9f196f
Miguel Romero Karam <miguelromerokaram@gmail.com>
2023-08-20 22:55:41 +0200

    chore(plugins): format via `deno fmt`

--- tests/fixture_unocss_hydrate/uno.config.ts
+++ tests/fixture_unocss_hydrate/uno.config.ts
@@ -3,5 +3,5 @@ import presetUno from "https://esm.sh/@unocss/preset-uno@0.55.1";
 
 export default defineConfig({
   presets: [presetUno()],
-  selfURL: import.meta.url
+  selfURL: import.meta.url,
 });
