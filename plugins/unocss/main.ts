954180b178da5e32f88bf5407948e14f9c3c0ce2
Adam Gregory <adam@aguilaengineering.com>
2023-08-20 15:02:04 +0100

    In the UnoCSS plugin pass the config to runtime
    -  Define a Config interface for the plugin, which extends UnoCSS UserConfig with a selfURL property
    - Add a `defineConfig()` function for help with types, and make it easier to adapt existing UnoCSS config modules for use with the plugin
    - Move the runtime script inline

--- plugins/unocss/main.ts
+++
@@ -1,5 +0,0 @@
-export default function main() {
-  const script = document.createElement("script");
-  script.src = "https://cdn.jsdelivr.net/npm/@unocss/runtime";
-  document.head.appendChild(script);
-}
