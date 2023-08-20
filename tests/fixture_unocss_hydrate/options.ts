af7175dbb689f9ee83e4a276f42fba5ceb15b9ac
Adam Gregory <adam@aguilaengineering.com>
2023-08-20 15:02:04 +0100

    In the UnoCSS plugin pass the config to runtime
    -  Define a Config interface for the plugin, which extends UnoCSS UserConfig with a selfURL property
    - Add a `defineConfig()` function for help with types, and make it easier to adapt existing UnoCSS config modules for use with the plugin
    - Move the runtime script inline

--- tests/fixture_unocss_hydrate/options.ts
+++ tests/fixture_unocss_hydrate/options.ts
@@ -1,7 +1,7 @@
 import { FreshOptions } from "$fresh/server.ts";
-import unocssPlugin, { type UserConfig } from "$fresh/plugins/unocss.ts";
+import unocssPlugin, { type Config } from "$fresh/plugins/unocss.ts";
 import unocssConfig from "./uno.config.ts";
 
 export default {
-  plugins: [unocssPlugin(unocssConfig as UserConfig)],
+  plugins: [unocssPlugin(unocssConfig as Config)],
 } as FreshOptions;
--- tests/fixture_unocss_hydrate/uno.config.ts
+++ tests/fixture_unocss_hydrate/uno.config.ts
@@ -1,6 +1,7 @@
-import type { UserConfig } from "$fresh/plugins/unocss.ts";
+import { defineConfig } from "$fresh/plugins/unocss.ts";
 import presetUno from "https://esm.sh/@unocss/preset-uno@0.55.1";
 
-export default {
+export default defineConfig({
   presets: [presetUno()],
-} satisfies UserConfig;
+  selfURL: import.meta.url
+});
