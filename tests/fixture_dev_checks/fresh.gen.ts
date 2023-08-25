062ca65b04413ea1e49c7670de403e5bcafa7400
Michael Herzner <michael.b.herzner@gmail.com>
2023-08-26 00:24:02 +0200

    chore: fix windows tests

--- tests/fixture_dev_checks/fresh.gen.ts
+++ tests/fixture_dev_checks/fresh.gen.ts
@@ -3,7 +3,7 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/_app.tsx";
-import * as $1 from "./routes/conflicts/:id.tsx";
+import * as $1 from "./routes/conflicts/[id].tsx";
 import * as $2 from "./routes/custom.txt.ts";
 import * as $3 from "./routes/index.tsx";
 import * as $4 from "./routes/nested/_app.tsx";
@@ -13,7 +13,7 @@ import * as $6 from "./routes/test/index.tsx";
 const manifest = {
   routes: {
     "./routes/_app.tsx": $0,
-    "./routes/conflicts/:id.tsx": $1,
+    "./routes/conflicts/[id].tsx": $1,
     "./routes/custom.txt.ts": $2,
     "./routes/index.tsx": $3,
     "./routes/nested/_app.tsx": $4,
