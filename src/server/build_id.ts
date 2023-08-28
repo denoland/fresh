efcaa2dee2ae5b43f53ae2c943780a15c3cd4e24
Marvin Hagemeister <marvin@deno.com>
2023-08-28 12:49:49 +0200

    fix: build id mismatch between CI + deploy (#1681)

--- src/server/build_id.ts
+++ src/server/build_id.ts
@@ -1,10 +1,16 @@
 import { toHashString } from "./deps.ts";
 
 const deploymentId = Deno.env.get("DENO_DEPLOYMENT_ID") ||
+  // For CI
+  Deno.env.get("GITHUB_SHA") ||
   crypto.randomUUID();
 const buildIdHash = await crypto.subtle.digest(
   "SHA-1",
   new TextEncoder().encode(deploymentId),
 );
 
-export const BUILD_ID = toHashString(buildIdHash, "hex");
+export let BUILD_ID = toHashString(buildIdHash, "hex");
+
+export function setBuildId(buildId: string) {
+  BUILD_ID = buildId;
+}
