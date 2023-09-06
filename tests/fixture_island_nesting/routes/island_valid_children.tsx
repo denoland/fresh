d6ef01a8e2b666548f1c57b5538abe89ceab3865
Marvin Hagemeister <marvin@deno.com>
2023-09-06 10:30:16 +0200

    chore: add more test cases

--- tests/fixture_island_nesting/routes/island_valid_children.tsx
+++ tests/fixture_island_nesting/routes/island_valid_children.tsx
@@ -1,5 +1,9 @@
 import { PassThrough } from "../islands/PassThrough.tsx";
 
+function Foo() {
+  return <h1>foo</h1>;
+}
+
 export default function Page() {
   return (
     <div id="page">
@@ -8,6 +12,12 @@ export default function Page() {
       <PassThrough>{null}</PassThrough>
       <PassThrough>{true}</PassThrough>
       <PassThrough>{false}</PassThrough>
+      <PassThrough>
+        <h1>hello</h1>
+      </PassThrough>
+      <PassThrough>
+        <Foo />
+      </PassThrough>
     </div>
   );
 }
