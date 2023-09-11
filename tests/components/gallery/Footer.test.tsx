3fb78eddb9c4e81e017875cd9959b524b4444784
Craig Doremus <cdoremus@gmail.com>
2023-09-11 15:32:17 -0400

    Footer component test using fresh-testing-library

---
+++ tests/components/gallery/Footer.test.tsx
@@ -0,0 +1,44 @@
+import {
+  assert,
+  assertEquals,
+  assertExists,
+  assertFalse,
+  assertStringIncludes,
+} from "$std/testing/asserts.ts";
+import { cleanup, fireEvent, render, setup } from "$fresh-testing-library";
+import { afterEach, beforeAll, describe, it } from "$std/testing/bdd.ts";
+import Footer from "../../../www/components/gallery/Footer.tsx";
+
+describe("Footer.tsx", () => {
+  beforeAll(setup);
+  afterEach(cleanup);
+
+  it("should display text", () => {
+    //@ts-ignore asdfase
+    const screen = render(<Footer></Footer>);
+    const fresh = screen.getByText("Fresh");
+    assertEquals(fresh.textContent, "Fresh");
+    const frameworkText = screen.getAllByText("Full Stack Framework");
+    assertExists(frameworkText);
+  });
+
+  it("should display menu", () => {
+    //@ts-ignore asdfase
+    const screen = render(<Footer></Footer>);
+    const menu1 = screen.getByText("Getting Started");
+    assertExists(menu1);
+    const menu2 = screen.getByText("Community");
+    assertExists(menu2);
+    const menu3 = screen.getByText("Discord");
+    assertExists(menu3);
+  });
+
+  // it("should display child elements", () => {
+  //   function GetChild() {
+  //     return (<div>child element</div>)
+  //   }
+  //   const screen = render(<Footer><GetChild/></Footer>);
+  //   const child = screen.getByText("child element");
+  //   assertExists(child);
+  // });
+});
