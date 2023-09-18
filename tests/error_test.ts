d84cc52d25effb3ec34913ecd1e38ef6a41dd373
Marvin Hagemeister <marvin@deno.com>
2023-09-18 13:55:38 +0200

    chore: remove unused import

--- tests/error_test.ts
+++ tests/error_test.ts
@@ -1,5 +1,4 @@
 import { ServerContext, Status } from "../server.ts";
-import { REFRESH_JS_URL } from "../src/server/constants.ts";
 import { assert, assertEquals, assertStringIncludes } from "./deps.ts";
 import manifest from "./fixture_error/fresh.gen.ts";
 
