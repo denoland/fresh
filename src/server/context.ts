c749e14e78300123e565eafe740d9211e36fd30b
Marvin Hagemeister <marvin@deno.com>
2023-09-18 16:18:58 +0200

    chore: remove unrelated change

--- src/server/context.ts
+++ src/server/context.ts
@@ -366,15 +366,7 @@ export class ServerContext {
           pattern: pathToPattern(baseRoute),
           url,
           name,
-          // deno-lint-ignore no-explicit-any
-          component: (props: any) => {
-            // Show our nice error page during dev mode.
-            if (dev && props.error) {
-              return h(DefaultErrorHandler, props);
-            }
-            // deno-lint-ignore no-explicit-any
-            return h((component ?? DefaultErrorHandler) as any, props);
-          },
+          component,
           handler: handler ??
             ((req, ctx) => router.defaultErrorHandler(req, ctx, ctx.error)),
           csp: Boolean(config?.csp ?? false),
