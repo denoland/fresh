fd235e877c4e2a071bd6bb8784719d0d7afc417e
Marvin Hagemeister <marvin@deno.com>
2023-10-05 10:47:37 +0200

    feat: only enable partials when attribute is set (#1861)

--- tests/partials_test.ts
+++ tests/partials_test.ts
@@ -630,7 +630,10 @@ Deno.test("partial navigation", async () => {
         document.querySelectorAll(".island").length === 1
       );
       assertEquals(page.url(), initialUrl);
-      await waitForText(page, "pre", "mount Counter A\n");
+      await waitFor(async () => {
+        const doc = parseHtml(await page.content());
+        return /mount Counter A/.test(doc.querySelector("pre")!.textContent!);
+      });
       await assertNoPageComments(page);
     },
   );
@@ -1024,4 +1027,3 @@ Deno.test("throws an error when response contains no partials", async () => {
 });
 
 // TODO Head merging
-// TODO Children Keys
