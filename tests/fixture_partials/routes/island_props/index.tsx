3fd8e586eaefee5559efd3445069a8086ce30a50
gimenes <tlgimenes@gmail.com>
2023-10-06 15:27:26 -0300

    fix tests

--- tests/fixture_partials/routes/island_props/index.tsx
+++ tests/fixture_partials/routes/island_props/index.tsx
@@ -1,21 +1,19 @@
 import { Partial } from "$fresh/runtime.ts";
-import { Fader } from "../../islands/Fader.tsx";
+// import { Fader } from "../../islands/Fader.tsx";
 import PropIsland from "../../islands/PropIsland.tsx";
 
 export default function PropsDemo() {
   return (
     <div>
       <Partial name="slot-1">
-        <Fader>
-          <p class="status-initial">initial</p>
-          <PropIsland
-            boolean={true}
-            number={1}
-            obj={{ foo: 123 }}
-            strArr={["foo"]}
-            string="foo"
-          />
-        </Fader>
+        <p class="status-initial">initial</p>
+        <PropIsland
+          boolean={true}
+          number={1}
+          obj={{ foo: 123 }}
+          strArr={["foo"]}
+          string="foo"
+        />
       </Partial>
       <p>
         <a
