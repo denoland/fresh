bb8921ac859d65f1a11facf3e819b2f19e37adf8
Marvin Hagemeister <marvin@deno.com>
2023-10-12 23:30:18 +0200

    fix: ctx.render headers not working (#1927)

--- tests/render_test.ts
+++ tests/render_test.ts
@@ -1,4 +1,8 @@
-import { assertSelector, parseHtml } from "$fresh/tests/test_utils.ts";
+import {
+  assertSelector,
+  parseHtml,
+  withFakeServe,
+} from "$fresh/tests/test_utils.ts";
 import { assertEquals } from "$std/testing/asserts.ts";
 import { createHandler } from "$fresh/server.ts";
 import manifest from "./fixture/fresh.gen.ts";
@@ -25,4 +29,20 @@ Deno.test("doesn't leak data across renderers", async () => {
     promises.push(load("bar"));
   }
   await Promise.all(promises);
+});
+
+Deno.test("render headers passed to ctx.render()", async () => {
+  await withFakeServe("./tests/fixture_render/main.ts", async (server) => {
+    let res = await server.get("/header_arr");
+    assertEquals(res.headers.get("x-foo"), "Hello world!");
+    await res.body?.cancel();
+
+    res = await server.get("/header_obj");
+    assertEquals(res.headers.get("x-foo"), "Hello world!");
+    await res.body?.cancel();
+
+    res = await server.get("/header_instance");
+    assertEquals(res.headers.get("x-foo"), "Hello world!");
+    await res.body?.cancel();
+  });
 });
