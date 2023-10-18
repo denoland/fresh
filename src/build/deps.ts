9e72e3cef3a937cab51954e3a3242d1c14e75980
Marvin Hagemeister <marvin@deno.com>
2023-10-18 18:27:56 +0200

    fix: cut off file names when project is at root / (#1957)
    (cherry picked from commit 4c59f9df65adaa8aaff9e9db8f4ededae2bcbefe)

--- src/build/deps.ts
+++ src/build/deps.ts
@@ -1,6 +1,8 @@
 // -- $std --
 export {
   fromFileUrl,
+  join,
+  relative,
   toFileUrl,
 } from "https://deno.land/std@0.193.0/path/mod.ts";
 export { escape as regexpEscape } from "https://deno.land/std@0.193.0/regexp/escape.ts";
