5016aa5238f898063c39b86391f86408d7033efa
Marvin Hagemeister <marvin@deno.com>
2023-10-19 11:59:09 +0200

    chore: import esbuild lazily (#1959)
    (cherry picked from commit c41cfc92271dcaa3d6c8befda57a07bce711a4a2)

--- src/build/deps.ts
+++ src/build/deps.ts
@@ -7,17 +7,4 @@ export {
 } from "https://deno.land/std@0.193.0/path/mod.ts";
 export { escape as regexpEscape } from "https://deno.land/std@0.193.0/regexp/escape.ts";
 
-// -- esbuild --
-// @deno-types="https://deno.land/x/esbuild@v0.19.4/mod.d.ts"
-import * as esbuildWasm from "https://deno.land/x/esbuild@v0.19.4/wasm.js";
-import * as esbuildNative from "https://deno.land/x/esbuild@v0.19.4/mod.js";
-// @ts-ignore trust me
-const esbuild: typeof esbuildWasm =
-  // deno-lint-ignore no-deprecated-deno-api
-  Deno.run === undefined || Deno.env.get("FRESH_ESBUILD_LOADER") === "portable"
-    ? esbuildWasm
-    : esbuildNative;
-const esbuildWasmURL = new URL("./esbuild_v0.19.4.wasm", import.meta.url).href;
-export { esbuild, esbuildWasm as esbuildTypes, esbuildWasmURL };
-
 export { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.8.2/mod.ts";
