436370fab6da6f26dc29dda246574b7c796c0033
Marvin Hagemeister <marvin@deno.com>
2023-10-27 20:55:57 +0200

    fix: twind with islands

--- plugins/twind/shared.ts
+++ plugins/twind/shared.ts
@@ -1,4 +1,8 @@
-import { Configuration, setup as twSetup, Sheet } from "twind";
+import { JSX, options as preactOptions, VNode } from "preact";
+import { Configuration, setup as twSetup, Sheet, tw } from "twind";
+import { IS_BROWSER } from "$fresh/runtime.ts";
+
+type PreactOptions = typeof preactOptions & { __b?: (vnode: VNode) => void };
 
 export const STYLE_ELEMENT_ID = "__FRSH_TWIND";
 
@@ -7,10 +11,47 @@ export interface Options extends Omit<Configuration, "mode" | "sheet"> {
   selfURL: string;
 }
 
+declare module "preact" {
+  namespace JSX {
+    interface DOMAttributes<Target extends EventTarget> {
+      class?: string;
+      className?: string;
+    }
+  }
+}
+
 export function setup(options: Options, sheet: Sheet) {
   twSetup({
     ...options,
     mode: "silent",
     sheet,
   });
+
+  if (IS_BROWSER) {
+    // Hook into options._diff which is called whenever a new comparison
+    // starts in Preact.
+    const originalHook = (preactOptions as PreactOptions).__b;
+    (preactOptions as PreactOptions).__b = (
+      // deno-lint-ignore no-explicit-any
+      vnode: VNode<JSX.DOMAttributes<any>>,
+    ) => {
+      if (typeof vnode.type === "string" && typeof vnode.props === "object") {
+        const { props } = vnode;
+        const classes: string[] = [];
+        if (props.class) {
+          classes.push(tw(props.class));
+          props.class = undefined;
+        }
+        if (props.className) {
+          classes.push(tw(props.className));
+          props.className = undefined;
+        }
+        if (classes.length) {
+          props.class = classes.join(" ");
+        }
+      }
+
+      originalHook?.(vnode);
+    };
+  }
 }
