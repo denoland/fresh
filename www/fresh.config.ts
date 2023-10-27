40e6c4516a8ab6c21f56a5170e94c26a036ceb5e
Marvin Hagemeister <marvin@deno.com>
2023-10-27 10:09:32 +0200

    WIP

--- www/fresh.config.ts
+++ www/fresh.config.ts
@@ -1,10 +1,10 @@
-import twindPlugin from "$fresh/plugins/twind.ts";
-import twindConfig from "./twind.config.ts";
+// import twindPlugin from "$fresh/plugins/twind.ts";
+// import twindConfig from "./twind.config.ts";
 import { defineConfig } from "$fresh/server.ts";
 
 export default defineConfig({
   build: {
     target: "safari12",
   },
-  plugins: [twindPlugin(twindConfig)],
+  // plugins: [twindPlugin(twindConfig)],
 });
