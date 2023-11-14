f831e313afddc4908a68831554197c2ac57e0eb2
Marvin Hagemeister <marvin@deno.com>
2023-11-14 13:41:58 +0100

    fix: wrong character

--- www/components/Button.tsx
+++ www/components/Button.tsx
@@ -7,7 +7,7 @@ export function RoundedButton(props: JSX.HTMLAttributes<HTMLButtonElement>) {
         touchAction: "manipulation",
       }}
       {...props}
-      class="p-3 border border-transparent rounded-full text-white bg-green(500 hover:600) focus:(outline-none ring(2 offset-2 green-500)) disabled:(bg-green-200 cursor-default)}"
+      class="p-3 border border-transparent rounded-full text-white bg-green(500 hover:600) focus:(outline-none ring(2 offset-2 green-500)) disabled:(bg-green-200 cursor-default)"
     />
   );
 }
