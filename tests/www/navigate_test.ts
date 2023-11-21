a6050bbfbca36f181ac88fdc3e5941eedc47d1a1
Marvin Hagemeister <marvin@deno.com>
2023-11-21 21:30:10 +0100

    chore: add more test failure debug info

--- tests/www/navigate_test.ts
+++ tests/www/navigate_test.ts
@@ -2,11 +2,19 @@ import { waitForText, withPageName } from "$fresh/tests/test_utils.ts";
 
 Deno.test("www - docs navigation", async () => {
   await withPageName("./www/main.ts", async (page, address) => {
+    const logs: string[] = [];
+    page.on("console", (msg) => logs.push(msg.text()));
     await page.goto(`${address}`, { waitUntil: "networkidle2" });
-    await page.click(`a[href="/docs"]`);
-    await waitForText(page, "h1", "Introduction");
 
-    await page.click(`a[href="/docs/getting-started/create-a-project"]`);
-    await waitForText(page, "h1", "Create a project");
+    try {
+      await page.click(`a[href="/docs"]`);
+      await waitForText(page, "h1", "Introduction");
+
+      await page.click(`a[href="/docs/getting-started/create-a-project"]`);
+      await waitForText(page, "h1", "Create a project");
+    } catch (err) {
+      console.log(logs);
+      throw err;
+    }
   });
 });
