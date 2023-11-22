850130d8988298ad2ceeb622c7f31e4c42c98d73
Marvin Hagemeister <hello@marvinh.dev>
2023-11-22 01:57:07 +0100

    chore: use dev in main test

--- tests/www/navigate_test.ts
+++ tests/www/navigate_test.ts
@@ -1,27 +1,13 @@
 import { waitForText, withPageName } from "$fresh/tests/test_utils.ts";
 
 Deno.test("www - docs navigation", async () => {
-  await withPageName("./www/main.ts", async (page, address) => {
-    const logs: string[] = [];
-    page.on("console", (msg) => logs.push(msg.text()));
-    page.on("requestfailed", (request) => {
-      console.log("FAILED", request.url());
-      request.continue();
-    });
-    page.on("pageerror", (er) => {
-      console.log(er);
-    });
-    await page.goto(`${address}`, { waitUntil: "networkidle2" });
+  await withPageName("./www/dev.ts", async (page, address) => {
+    await page.goto(`${address}`);
 
-    try {
-      await page.click(`a[href="/docs"]`);
-      await waitForText(page, "h1", "Introduction");
+    await page.click(`a[href="/docs"]`);
+    await waitForText(page, "h1", "Introduction");
 
-      await page.click(`a[href="/docs/getting-started/create-a-project"]`);
-      await waitForText(page, "h1", "Create a project");
-    } catch (err) {
-      console.log(logs);
-      throw err;
-    }
+    await page.click(`a[href="/docs/getting-started/create-a-project"]`);
+    await waitForText(page, "h1", "Create a project");
   });
 });
