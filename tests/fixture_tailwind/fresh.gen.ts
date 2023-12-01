2acf72d512a2e7b11798051fb663feaa040e515b
Marvin Hagemeister <marvin@deno.com>
2023-12-01 23:38:22 +0100

    fix: tailwind failing on non-existant files (#2118)

--- tests/fixture_tailwind/fresh.gen.ts
+++ tests/fixture_tailwind/fresh.gen.ts
@@ -3,6 +3,7 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $_app from "./routes/_app.tsx";
+import * as $_middleware from "./routes/_middleware.ts";
 import * as $index from "./routes/index.tsx";
 
 import { type Manifest } from "$fresh/server.ts";
@@ -10,6 +11,7 @@ import { type Manifest } from "$fresh/server.ts";
 const manifest = {
   routes: {
     "./routes/_app.tsx": $_app,
+    "./routes/_middleware.ts": $_middleware,
     "./routes/index.tsx": $index,
   },
   islands: {},
