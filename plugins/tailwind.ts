c43ce9ed20cdd18892937e1bc3583104f83758c6
Marvin Hagemeister <hello@marvinh.dev>
2023-12-02 10:17:08 +0100

    fix: ensure tailwind is loaded lazily

--- plugins/tailwind.ts
+++ plugins/tailwind.ts
@@ -1,77 +1,7 @@
-import { Plugin, PluginMiddleware, ResolvedFreshConfig } from "../server.ts";
-import tailwindCss, { Config } from "tailwindcss";
-import postcss from "npm:postcss@8.4.31";
-import cssnano from "npm:cssnano@6.0.1";
+import { Plugin, PluginMiddleware } from "../server.ts";
+import type postcss from "npm:postcss@8.4.31";
 import * as path from "https://deno.land/std@0.207.0/path/mod.ts";
 import { walk } from "https://deno.land/std@0.207.0/fs/walk.ts";
-
-const CONFIG_EXTENSIONS = ["ts", "js", "mjs"];
-
-async function findTailwindConfigFile(directory: string): Promise<string> {
-  let dir = directory;
-  while (true) {
-    for (let i = 0; i < CONFIG_EXTENSIONS.length; i++) {
-      const ext = CONFIG_EXTENSIONS[i];
-      const filePath = path.join(dir, `tailwind.config.${ext}`);
-      try {
-        const stat = await Deno.stat(filePath);
-        if (stat.isFile) {
-          return filePath;
-        }
-      } catch (err) {
-        if (!(err instanceof Deno.errors.NotFound)) {
-          throw err;
-        }
-      }
-    }
-
-    const parent = path.dirname(dir);
-    if (parent === dir) {
-      throw new Error(
-        `Could not find a tailwind config file in the current directory or any parent directory.`,
-      );
-    }
-
-    dir = parent;
-  }
-}
-
-async function initTailwind(
-  config: ResolvedFreshConfig,
-): Promise<postcss.Processor> {
-  const root = path.dirname(config.staticDir);
-
-  const configPath = await findTailwindConfigFile(root);
-  const url = path.toFileUrl(configPath).href;
-  const tailwindConfig = (await import(url)).default as Config;
-
-  if (!Array.isArray(tailwindConfig.content)) {
-    throw new Error(`Expected tailwind "content" option to be an array`);
-  }
-
-  tailwindConfig.content = tailwindConfig.content.map((pattern) => {
-    if (typeof pattern === "string") {
-      const relative = path.relative(Deno.cwd(), path.dirname(configPath));
-
-      if (!relative.startsWith("..")) {
-        return path.join(relative, pattern);
-      }
-    }
-    return pattern;
-  });
-
-  // PostCSS types cause deep recursion
-  // deno-lint-ignore no-explicit-any
-  const plugins: any[] = [
-    tailwindCss(tailwindConfig),
-  ];
-
-  if (!config.dev) {
-    plugins.push(cssnano());
-  }
-
-  return postcss(plugins);
-}
 
 export default function tailwind(): Plugin {
   let staticDir = path.join(Deno.cwd(), "static");
@@ -142,7 +72,9 @@ export default function tailwind(): Plugin {
     async configResolved(config) {
       if (config.dev) {
         staticDir = config.staticDir;
-        processor = await initTailwind(config);
+        processor = await (await import("./tailwind/compiler.ts")).initTailwind(
+          config,
+        );
         middlewares.push(tailwindMiddleware);
       }
     },
@@ -150,7 +82,12 @@ export default function tailwind(): Plugin {
     async buildStart(config) {
       staticDir = config.staticDir;
       const outDir = path.join(config.build.outDir, "static");
-      processor = await initTailwind(config);
+
+      if (processor === null) {
+        throw new Error(
+          `Failed to initialize tailwindcss. This is a bug in the Fresh Tailwind plugin.`,
+        );
+      }
 
       const files = walk(config.staticDir, {
         exts: ["css"],
