83154e6f6f686c8aeb135c925278fe878117f9eb
Reed von Redwitz <reed+github@vonredwitz.com>
2023-12-04 12:36:12 +0100

    fix: tailwind now respects basepath

--- tests/fixture_base_path/routes/index.tsx
+++ tests/fixture_base_path/routes/index.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <h1>it works</h1>;
-}
+import { defineRoute } from "$fresh/server.ts";
+
+export default defineRoute<{ data: string }>((req, ctx) => {
+  return (
+    <>
+      <h1 class="text-red-600 block">it works</h1>
+      {ctx.state.data}
+    </>
+  );
+});
