5d845c4a8ba2adb07a8b199cfa11208049019d2c
Adam Gregory <adam@aguilaengineering.com>
2023-12-20 15:43:54 +0000

    UnoCSS plugin updates
    - Use middleware to maintain "AOT" behaviour in dev mode
    - Use new configResolved() plugin hook to resolve UnoCSS config file
    - Reduce effort during renders in SSR mode

--- plugins/unocss.ts
+++ plugins/unocss.ts
@@ -6,7 +6,7 @@ import {
 } from "https://esm.sh/@unocss/core@0.56.5";
 import type { Theme } from "https://esm.sh/@unocss/preset-uno@0.56.5";
 
-import { Plugin, type PluginRenderStyleTag } from "$fresh/server.ts";
+import { Plugin } from "$fresh/server.ts";
 import {
   dirname,
   exists,
@@ -21,7 +21,7 @@ type PreactOptions = typeof preactOptions & { __b?: (vnode: VNode) => void };
 const SKIP_START_COMMENT = "@unocss-skip-start";
 const SKIP_END_COMMENT = "@unocss-skip-end";
 const SKIP_COMMENT_RE = new RegExp(
-  `(\/\/\\s*?${SKIP_START_COMMENT}\\s*?|\\/\\*\\s*?${SKIP_START_COMMENT}\\s*?\\*\\/|<!--\\s*?${SKIP_START_COMMENT}\\s*?-->)[\\s\\S]*?(\/\/\\s*?${SKIP_END_COMMENT}\\s*?|\\/\\*\\s*?${SKIP_END_COMMENT}\\s*?\\*\\/|<!--\\s*?${SKIP_END_COMMENT}\\s*?-->)`,
+  `(//\\s*?${SKIP_START_COMMENT}\\s*?|\\/\\*\\s*?${SKIP_START_COMMENT}\\s*?\\*\\/|<!--\\s*?${SKIP_START_COMMENT}\\s*?-->)[\\s\\S]*?(//\\s*?${SKIP_END_COMMENT}\\s*?|\\/\\*\\s*?${SKIP_END_COMMENT}\\s*?\\*\\/|<!--\\s*?${SKIP_END_COMMENT}\\s*?-->)`,
   "g",
 );
 
@@ -87,6 +87,36 @@ export function installPreactHook(classes: Set<string>) {
   };
 }
 
+/**
+ * Runs UnoCSS over the source code of the project
+ * @param uno The UnoCSS generator object
+ * @returns The generated CSS content
+ */
+async function runOverSource(uno: UnoGenerator): Promise<string> {
+  // Find source files
+  const projectDir = fromFileUrl(dirname(Deno.mainModule));
+  const sourceFiles = [];
+  for await (
+    const entry of walk(projectDir, {
+      includeDirs: false,
+      exts: [".tsx", ".ts", ".jsx", ".html"],
+    })
+  ) {
+    sourceFiles.push(entry.path);
+  }
+
+  // Read the source code into a single string
+  const sourceCode = await Promise.all(
+    sourceFiles.map((filename) => Deno.readTextFile(filename)),
+  ).then((x) => x.join("\n").replace(SKIP_COMMENT_RE, ""));
+
+  // Extract UnoCSS classes from the source code and generate CSS
+  const { css } = await uno.generate(sourceCode);
+
+  // Include the inline reset CSS
+  return unoResetCSS + css;
+}
+
 /** UnoCSS plugin - automatically generates CSS utility classes */
 export default function unocss(
   { config, aot = true, ssr = false, csr = false }: UnoCssPluginOptions = {},
@@ -95,23 +125,27 @@ export default function unocss(
   // or to use the browser runtime
   const configURL = new URL("./uno.config.ts", Deno.mainModule);
 
-  // Load config from file if required
-  if (config === undefined) {
-    import(configURL.toString()).then((mod) => {
-      config = mod.default;
-    }).catch((error) => {
-      exists(configURL, { isFile: true, isReadable: true }).then(
-        (configFileExists) => {
-          throw configFileExists ? error : new Error(
-            "uno.config.ts not found in the project directory! Please create it or pass a config object to the UnoCSS plugin",
-          );
-        },
-      );
-    });
-  }
+  // Link to CSS file, if AOT mode is enabled
+  const links = aot ? [{ rel: "stylesheet", href: "/uno.css" }] : [];
+
+  // Add entrypoint, if CSR mode is enabled
+  const scripts = csr ? [{ entrypoint: "main", state: {} }] : [];
+
+  // In CSR-only mode, include the style resets using an inline style tag
+  const styles = csr && !aot && !ssr ? [{ cssText: unoResetCSS }] : [];
+
+  const middlewares: Plugin["middlewares"] = [];
+
+  //  Created in configResolved()
+  let uno: UnoGenerator;
+
+  // Create a set that may be used to hold class names encountered during SSR
+  const ssrClasses = new Set<string>();
 
   return {
     name: "unocss",
+    middlewares,
+
     // Optional client runtime
     entrypoints: csr
       ? {
@@ -126,63 +160,85 @@ export default function unocss(
       }
       : {},
 
-    async renderAsync(ctx) {
-      // Link to CSS file, if AOT mode is enabled
-      const links = aot ? [{ rel: "stylesheet", href: "/uno.css" }] : [];
+    async configResolved(freshConfig) {
+      // Load config from file if required
+      if (config === undefined) {
+        try {
+          config = (await import(configURL.toString())).default;
+        } catch (error) {
+          if (await exists(configURL, { isFile: true, isReadable: true })) {
+            throw error;
+          } else {
+            throw new Error(
+              "uno.config.ts not found in the project directory! Please create it or pass a config object to the UnoCSS plugin",
+            );
+          }
+        }
+      }
 
-      // Add entrypoint, if CSR mode is enabled
-      const scripts = csr ? [{ entrypoint: "main", state: {} }] : [];
+      // Create the generator object
+      uno = new UnoGenerator(config);
 
-      // Generate inline styles, if SSR mode is enabled
-      const styles: PluginRenderStyleTag[] = [];
       if (ssr) {
-        // Create a set that will be used to hold class names encountered during SSR
-        const classes = new Set<string>();
-
         // Hook into Preact to add to the set of classes during the render
-        installPreactHook(classes);
+        installPreactHook(ssrClasses);
+      } else if (aot && freshConfig.dev) {
+        // Extract classes from source code now, and generate CSS
+        console.log(
+          "%cGenerating UnoCSS stylesheet...",
+          "color: blue; font-weight: bold",
+        );
+        const css = await runOverSource(uno);
+
+        // Craft a response for requests for the generated CSS file
+        const resp = new Response(css, {
+          headers: {
+            "Content-Type": "text/css",
+            "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate",
+          },
+        });
+
+        // Add a middleware to handle requests for the generated CSS file
+        middlewares.push({
+          path: "/",
+          middleware: {
+            handler: (_req, ctx) =>
+              ctx.url.pathname === "/uno.css" ? resp : ctx.next(),
+          },
+        });
+      }
+    },
+
+    async renderAsync(ctx) {
+      // Generate inline styles, if SSR mode is enabled
+      if (ssr) {
+        // Clear any classes extracted during previous renders
+        ssrClasses.clear();
 
         // Render. Preact will populate the list of classes.
         await ctx.renderAsync();
 
         // Run UnoCSS over the classes to generate CSS
-        const uno = new UnoGenerator(config);
-        const { css } = await uno.generate(classes);
-        styles.push({ cssText: `${unoResetCSS}\n${css}` });
-      } else {
-        await ctx.renderAsync();
-      }
+        const { css } = await uno.generate(ssrClasses);
 
-      return { scripts, styles, links };
+        // Include SSR CSS, and possibly CSR script
+        return { scripts, styles: [{ cssText: `${unoResetCSS}\n${css}` }] };
+      } else {
+        // Include link to AOT-generated CSS file and/or CSR script
+        await ctx.renderAsync();
+        return { scripts, links, styles };
+      }
     },
 
     async buildStart({ build: { outDir } }) {
-      // Find source files
-      const projectDir = fromFileUrl(dirname(Deno.mainModule));
-      const sourceFiles = [];
-      for await (
-        const entry of walk(projectDir, {
-          includeDirs: false,
-          exts: [".tsx", ".jsx", ".html"],
-        })
-      ) {
-        sourceFiles.push(entry.path);
+      // Generate a static CSS file, if AOT mode is enabled
+      if (aot) {
+        // Extract classes from source code and generate CSS
+        const css = await runOverSource(uno);
+
+        // Write the generated CSS to a static file
+        await Deno.writeTextFile(join(outDir, "static", "uno.css"), css);
       }
-
-      // Read the source code into a single string
-      const sourceCode = await Promise.all(
-        sourceFiles.map((filename) => Deno.readTextFile(filename)),
-      ).then((x) => x.join("\n").replace(SKIP_COMMENT_RE, ""));
-
-      // Extract UnoCSS classes from the source code and generate CSS
-      const uno = new UnoGenerator(config);
-      const { css } = await uno.generate(sourceCode);
-
-      // Write the generated CSS to a static file
-      await Deno.writeTextFile(
-        join(outDir, "static", "uno.css"),
-        unoResetCSS + css,
-      );
     },
   };
 }
