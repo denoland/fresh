c4c205eb4c3732ab8cd314b5a1f0f9c73612f579
Roj <ez@roj.im>
2024-03-08 21:59:47 +0300

    fmt

--- init.ts
+++ init.ts
@@ -629,7 +629,7 @@ const config = {
   lint: {
     rules: {
       tags: ["fresh", "recommended"],
-      exclude: ["no-window"]
+      exclude: ["no-window"],
     },
   },
   exclude: ["**/_fresh/*"],
