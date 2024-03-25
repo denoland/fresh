90dd3caf61f4026438951159490aacc4e6187c89
Marvin Hagemeister <hello@marvinh.dev>
2024-03-26 00:00:57 +0100

    chore: delete 1.x folder

--- src/__OLD/runtime/build_id.ts
+++
@@ -1,2 +0,0 @@
-// Note: in the client build this file is replaced with a file exporting a static string
-export { BUILD_ID } from "../server/build_id.ts";
--- src/__OLD/runtime/entrypoints/signals.ts
+++
@@ -1,1 +0,0 @@
-export { signal } from "@preact/signals";
