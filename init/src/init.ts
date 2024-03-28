0f11eba203d3de7406cd36604c218ac5faf8365c
Marvin Hagemeister <marvin@deno.com>
2024-03-28 14:23:48 +0100

    fix: fresh plugin import

--- init/src/init.ts
+++ init/src/init.ts
@@ -406,7 +406,7 @@ if (Deno.args.includes("build")) {
     imports: {
       // FIXME: Update once relased
       "@fresh/core": "jsr:@marvinh-test/fresh@^2.0.0-prealpha.8",
-      "@fresh/plugin-tailwind": "jsr:@marvinh-test/fresh@^2.0.0-prealpha.8",
+      "@fresh/plugin-tailwind": "jsr:@marvinh-test/fresh-tailwind@^0.0.1",
       "preact": "npm:preact@^10.20.1",
       "preact/signals": "npm:preact@^1.2.3",
     } as Record<string, string>,
