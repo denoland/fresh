cd8110441c8ff76b76e1adbe2964c5d3a2a912f7
Marvin Hagemeister <marvin@deno.com>
2024-04-17 11:43:38 +0200

    chore: add back preact jsx types

--- src/types.ts
+++ src/types.ts
@@ -27,3 +27,25 @@ export interface RouteConfig {
    */
   skipAppWrapper?: boolean;
 }
+
+export interface SignalLike<T> {
+  value: T;
+  peek(): T;
+  subscribe(fn: (value: T) => void): () => void;
+}
+
+declare global {
+  namespace preact.createElement.JSX {
+    interface HTMLAttributes {
+      /**
+       * Alternative url to fetch partials from on `<a>` or `<form>` tags
+       */
+      "f-partial"?: string | SignalLike<string>;
+      /**
+       * Enable or disable client side navigation and partials for this
+       * particular node and its children.
+       */
+      "f-client-nav"?: boolean | SignalLike<boolean>;
+    }
+  }
+}
