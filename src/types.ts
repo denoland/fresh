6668603763663a81a23b095cd56fa12844883ebe
Marvin Hagemeister <marvin@deno.com>
2024-04-19 16:44:21 +0200

    fix: workaround for JSR publishing

--- src/types.ts
+++ src/types.ts
@@ -34,18 +34,19 @@ export interface SignalLike<T> {
   subscribe(fn: (value: T) => void): () => void;
 }
 
-declare global {
-  namespace preact.createElement.JSX {
-    interface HTMLAttributes {
-      /**
-       * Alternative url to fetch partials from on `<a>` or `<form>` tags
-       */
-      "f-partial"?: string | SignalLike<string>;
-      /**
-       * Enable or disable client side navigation and partials for this
-       * particular node and its children.
-       */
-      "f-client-nav"?: boolean | SignalLike<boolean>;
-    }
-  }
-}
+// TODO: Uncomment once JSR supports global types
+// declare global {
+//   namespace preact.createElement.JSX {
+//     interface HTMLAttributes {
+//       /**
+//        * Alternative url to fetch partials from on `<a>` or `<form>` tags
+//        */
+//       "f-partial"?: string | SignalLike<string>;
+//       /**
+//        * Enable or disable client side navigation and partials for this
+//        * particular node and its children.
+//        */
+//       "f-client-nav"?: boolean | SignalLike<boolean>;
+//     }
+//   }
+// }
