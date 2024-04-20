c9efcd2daf66f1c66c81ab355df2f22dd71d9dbe
Marvin Hagemeister <marvin@deno.com>
2024-04-20 09:51:29 +0200

    chore: update script code cleanup

--- update/src/update.ts
+++ update/src/update.ts
@@ -1,6 +1,5 @@
 import * as path from "@std/path";
 import * as JSONC from "@std/jsonc";
-import { walk } from "@std/fs/walk";
 import * as tsmorph from "ts-morph";
 
 export const FRESH_VERSION = "2.0.0-alpha.1";
@@ -72,117 +71,74 @@ export async function update(dir: string) {
   try {
     await Deno.stat(routesDir);
 
-    for await (
-      const entry of walk(path.join(dir, "routes"), {
-        includeDirs: false,
-        includeFiles: true,
-        match: [/\.[tj]sx?$/],
-      })
-    ) {
-      if (!entry.isFile) continue;
-
-      const project = new tsmorph.Project();
-      const sfs = project.addSourceFilesAtPaths(
-        path.join(dir, "**", "*.{js,jsx,ts,tsx}"),
-      );
-      await Promise.all(sfs.map(async (sourceFile) => {
-        for (const d of sourceFile.getImportDeclarations()) {
-          const specifier = d.getModuleSpecifierValue();
-          if (specifier === "preact") {
-            for (const n of d.getNamedImports()) {
-              const name = n.getName();
-              if (name === "h" || name === "Fragment") n.remove();
-            }
-
-            removeEmptyImport(d);
-          } else if (specifier === "$fresh/server.ts") {
-            d.setModuleSpecifier("@fresh/core");
-          } else if (specifier === "$fresh/runtime.ts") {
-            d.setModuleSpecifier("@fresh/core/runtime");
-
-            for (const n of d.getNamedImports()) {
-              const name = n.getName();
-              if (name === "Head") n.remove();
-            }
-
-            removeEmptyImport(d);
-          }
-        }
-
-        let text = sourceFile.getFullText();
-        text = text.replaceAll("/** @jsx h */\n", "");
-        text = text.replaceAll("/** @jsxFrag Fragment */\n", "");
-        sourceFile.replaceWithText(text);
-
-        for (const [name, decl] of sourceFile.getExportedDeclarations()) {
-          if (name === "handler") {
-            const maybeObjs = decl[0].getChildrenOfKind(
-              tsmorph.ts.SyntaxKind.ObjectLiteralExpression,
-            );
-            if (maybeObjs.length > 0) {
-              const obj = maybeObjs[0];
-              for (const property of obj.getProperties()) {
-                if (property.isKind(tsmorph.ts.SyntaxKind.MethodDeclaration)) {
-                  const name = property.getName();
-                  if (
-                    name === "GET" || name === "POST" || name === "PATCH" ||
-                    name === "PUT" || name === "DELETE"
-                  ) {
-                    let hasRequestVar = false;
-                    const params = property.getParameters();
-                    if (params.length > 0) {
-                      const paramName = params[0].getName();
-
-                      hasRequestVar = paramName === "req";
-                      if (hasRequestVar || paramName === "_req") {
-                        params[0].remove();
-                      }
-                    }
-
-                    if (hasRequestVar) {
-                      property.insertVariableStatement(0, {
-                        declarationKind: tsmorph.VariableDeclarationKind.Const,
-                        declarations: [{
-                          name: "req",
-                          initializer: "ctx.req",
-                        }],
-                      });
-                    }
-                  }
-                }
-              }
-            } else if (tsmorph.Node.isFunctionDeclaration(decl[0])) {
-              const d = decl[0];
-              const params = d.getParameters();
-              let hasRequestParam = false;
-              if (params.length > 0) {
-                const paramName = params[0].getName();
-                hasRequestParam = paramName === "req";
-                if (hasRequestParam || paramName === "_req") {
-                  params[0].remove();
-                }
-
-                if (hasRequestParam) {
-                  d.insertVariableStatement(0, {
-                    declarationKind: tsmorph.VariableDeclarationKind.Const,
-                    declarations: [{
-                      name: "req",
-                      initializer: "ctx.req",
-                    }],
-                  });
-                }
-              }
-            }
-          }
-        }
-
-        await sourceFile.save();
-        await format(sourceFile.getFilePath());
-      }));
-    }
+    const project = new tsmorph.Project();
+    const sfs = project.addSourceFilesAtPaths(
+      path.join(routesDir, "**", "*.{js,jsx,ts,tsx}"),
+    );
+    await Promise.all(sfs.map((sourceFile) => {
+      return updateFile(sourceFile);
+    }));
   } catch (_) {
     // TODO
   }
+}
+
+async function updateFile(sourceFile: tsmorph.SourceFile): Promise<void> {
+  for (const d of sourceFile.getImportDeclarations()) {
+    const specifier = d.getModuleSpecifierValue();
+    if (specifier === "preact") {
+      for (const n of d.getNamedImports()) {
+        const name = n.getName();
+        if (name === "h" || name === "Fragment") n.remove();
+      }
+
+      removeEmptyImport(d);
+    } else if (specifier === "$fresh/server.ts") {
+      d.setModuleSpecifier("@fresh/core");
+    } else if (specifier === "$fresh/runtime.ts") {
+      d.setModuleSpecifier("@fresh/core/runtime");
+
+      for (const n of d.getNamedImports()) {
+        const name = n.getName();
+        if (name === "Head") n.remove();
+      }
+
+      removeEmptyImport(d);
+    }
+  }
+
+  let text = sourceFile.getFullText();
+  text = text.replaceAll("/** @jsx h */\n", "");
+  text = text.replaceAll("/** @jsxFrag Fragment */\n", "");
+  sourceFile.replaceWithText(text);
+
+  for (const [name, decl] of sourceFile.getExportedDeclarations()) {
+    if (name === "handler") {
+      const maybeObjs = decl[0].getChildrenOfKind(
+        tsmorph.ts.SyntaxKind.ObjectLiteralExpression,
+      );
+      if (maybeObjs.length > 0) {
+        const obj = maybeObjs[0];
+        for (const property of obj.getProperties()) {
+          if (property.isKind(tsmorph.ts.SyntaxKind.MethodDeclaration)) {
+            const name = property.getName();
+            if (
+              name === "GET" || name === "POST" || name === "PATCH" ||
+              name === "PUT" || name === "DELETE"
+            ) {
+              maybePrependReqVar(property);
+            }
+          }
+        }
+      } else if (tsmorph.Node.isFunctionDeclaration(decl[0])) {
+        const d = decl[0];
+        maybePrependReqVar(d);
+      }
+    }
+  }
+
+  await sourceFile.save();
+  await format(sourceFile.getFilePath());
 }
 
 function removeEmptyImport(d: tsmorph.ImportDeclaration) {
@@ -192,5 +148,30 @@ function removeEmptyImport(d: tsmorph.ImportDeclaration) {
     d.getDefaultImport() === undefined
   ) {
     d.remove();
+  }
+}
+
+function maybePrependReqVar(
+  method: tsmorph.MethodDeclaration | tsmorph.FunctionDeclaration,
+) {
+  let hasRequestVar = false;
+  const params = method.getParameters();
+  if (params.length > 0) {
+    const paramName = params[0].getName();
+
+    hasRequestVar = paramName === "req";
+    if (hasRequestVar || paramName === "_req") {
+      params[0].remove();
+    }
+  }
+
+  if (hasRequestVar) {
+    method.insertVariableStatement(0, {
+      declarationKind: tsmorph.VariableDeclarationKind.Const,
+      declarations: [{
+        name: "req",
+        initializer: "ctx.req",
+      }],
+    });
   }
 }
