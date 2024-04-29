76ee1e2ced606d4e8ceca314a8cf1d4a5db81895
Marvin Hagemeister <marvin@deno.com>
2024-04-30 00:02:46 +0200

    fix: PageProps typde

--- src/compat/server.ts
+++ src/compat/server.ts
@@ -3,4 +3,4 @@ import type { FreshContext } from "../context.ts";
 /**
  * @deprecated Use {@link FreshContext} instead
  */
-export type PageProps<Data = unknown, T = unknown> = FreshContext<T, Data>;
+export type PageProps<Data = unknown, T = unknown> = FreshContext<Data, T>;
