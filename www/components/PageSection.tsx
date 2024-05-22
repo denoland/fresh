55c8791cfff235d033146087910fbeaf2e9d7223
Josh Collinsworth <joshuajcollinsworth@gmail.com>
2024-05-22 12:00:03 -0500

    Third time's the charm?

--- www/components/PageSection.tsx
+++ www/components/PageSection.tsx
@@ -3,7 +3,7 @@ import { JSX } from "preact";
 export function PageSection(props: JSX.HTMLAttributes<HTMLDivElement>) {
   return (
     <section
-      id={props.id ?? null}
+      id={props.id ?? ""}
       class={`w-full max-w-screen-xl mx-auto my-16 md:my-24 lg:my-32 px-4 sm:px-8 lg:px-16 2xl:px-0 flex flex-col gap-8 md:gap-16 ${
         props.class ?? ""
       }`}
