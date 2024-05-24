968869f303271c80d873e58a8f81de62eb3ab43e
Marvin Hagemeister <marvin@deno.com>
2024-05-24 17:04:13 +0200

    www: port error pages to Fresh 2

--- www/routes/_500.tsx
+++
@@ -1,8 +0,0 @@
-import { ServerCodePage } from "./_404.tsx";
-
-export default function InternalServerError() {
-  return ServerCodePage({
-    serverCode: 500,
-    codeDescription: "Oops! Something went wrong.",
-  });
-}
