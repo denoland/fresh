bd60444f7c19d1838c9fedf212bf01f6d8846889
Marvin Hagemeister <marvin@deno.com>
2024-05-27 14:34:36 +0200

    fix: only generate stack on HttpError if status >= 500 (#2470)

--- src/error_test.ts
+++ src/error_test.ts
@@ -1,27 +1,12 @@
 import { expect } from "@std/expect";
-import { MODE, setMode } from "./runtime/server/mod.tsx";
 import { HttpError } from "./error.ts";
 
-Deno.test("HttpError - contains stack in development", () => {
-  const tmp = MODE;
-  setMode("development");
-  try {
-    const err = new HttpError(404);
-    expect(err.status).toEqual(404);
-    expect(typeof err.stack).toEqual("string");
-  } finally {
-    setMode(tmp);
-  }
-});
+Deno.test("HttpError - contains stack if error >=500", () => {
+  const err = new HttpError(404);
+  expect(err.status).toEqual(404);
+  expect(typeof err.stack).toEqual("undefined");
 
-Deno.test("HttpError - contains no stack in production", () => {
-  const tmp = MODE;
-  setMode("production");
-  try {
-    const err = new HttpError(404);
-    expect(err.status).toEqual(404);
-    expect(err.stack).toEqual(undefined);
-  } finally {
-    setMode(tmp);
-  }
+  const err2 = new HttpError(500);
+  expect(err2.status).toEqual(500);
+  expect(typeof err2.stack).toEqual("string");
 });
