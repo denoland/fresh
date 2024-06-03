6516c478e1829828cdcbaee30d142a5fbf4dec27
Marvin Hagemeister <marvin@deno.com>
2024-06-03 15:21:25 +0200

    feat: remove `MODE` global

--- src/runtime/server/mod.ts
+++ src/runtime/server/mod.ts
@@ -1,9 +1,4 @@
 /**
  * The mode Fresh can run in.
  */
-export type Mode = "development" | "build" | "production";
-export let MODE: Mode = "production";
-
-export function setMode(mode: Mode) {
-  MODE = mode;
-}
+export type Mode = "development" | "production";
