98e2155b7c523d9519ce682a0ec4891e58ddd92c
Marvin Hagemeister <marvin@deno.com>
2024-06-05 10:09:51 +0200

    fix(init): update init complete text

--- init/src/init.ts
+++ init/src/init.ts
@@ -532,10 +532,10 @@ Started" guide here: https://fresh.deno.dev/docs/getting-started
 
 Make sure to install Deno: https://deno.land/manual/getting_started/installation
 
-Then start the project:
+Then start the project in development mode:
 
 \`\`\`
-deno task start
+deno task dev
 \`\`\`
 
 This will watch the project directory and restart as necessary.
