f23b46c84ede72eb8ddd300ec5b39b69602bd7cd
Marvin Hagemeister <marvin@deno.com>
2024-07-03 11:26:15 +0200

    chore: release alpha 19

--- init/src/init.ts
+++ init/src/init.ts
@@ -2,9 +2,9 @@ import * as colors from "@std/fmt/colors";
 import * as path from "@std/path";
 
 // Keep these as is, as we replace these version in our release script
-const FRESH_VERSION = "2.0.0-alpha.18";
+const FRESH_VERSION = "2.0.0-alpha.19";
 const FRESH_TAILWIND_VERSION = "0.0.1-alpha.7";
-const PREACT_VERSION = "10.22.0";
+const PREACT_VERSION = "10.22.1";
 const PREACT_SIGNALS_VERSION = "1.2.3";
 
 export const enum InitStep {
--- update/src/update.ts
+++ update/src/update.ts
@@ -4,8 +4,8 @@ import * as tsmorph from "ts-morph";
 
 export const SyntaxKind = tsmorph.ts.SyntaxKind;
 
-export const FRESH_VERSION = "2.0.0-alpha.18";
-export const PREACT_VERSION = "10.22.0";
+export const FRESH_VERSION = "2.0.0-alpha.19";
+export const PREACT_VERSION = "10.22.1";
 export const PREACT_SIGNALS_VERSION = "1.2.3";
 
 export interface DenoJson {
