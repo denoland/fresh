39dbee5497d576efe1653c07ad61b28d2faeeb8e
Marvin Hagemeister <marvin@deno.com>
2024-09-30 18:38:41 +0200

    chore: release 2.0.0-alpha.21 (#2665)

--- init/src/init.ts
+++ init/src/init.ts
@@ -2,9 +2,9 @@ import * as colors from "@std/fmt/colors";
 import * as path from "@std/path";
 
 // Keep these as is, as we replace these version in our release script
-const FRESH_VERSION = "2.0.0-alpha.20";
+const FRESH_VERSION = "2.0.0-alpha.21";
 const FRESH_TAILWIND_VERSION = "0.0.1-alpha.7";
-const PREACT_VERSION = "10.23.2";
+const PREACT_VERSION = "10.24.1";
 const PREACT_SIGNALS_VERSION = "1.3.0";
 
 export const enum InitStep {
