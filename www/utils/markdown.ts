denoland/fresh

commit de8f1b0c5910389206604ff8256ef8f19a94b22b

feat: update for Deno 2 (#2655)
Co-authored-by: Luca Casonato <hello@lcas.dev>

www/utils/markdown.ts
@@ -1,17 +1,20 @@
-import Prism from "https://esm.sh/prismjs@1.29.0";
-import "https://esm.sh/prismjs@1.29.0/components/prism-jsx.js?no-check";
-import "https://esm.sh/prismjs@1.29.0/components/prism-typescript.js?no-check";
-import "https://esm.sh/prismjs@1.29.0/components/prism-tsx.js?no-check";
-import "https://esm.sh/prismjs@1.29.0/components/prism-diff.js?no-check";
-import "https://esm.sh/prismjs@1.29.0/components/prism-json.js?no-check";
-import "https://esm.sh/prismjs@1.29.0/components/prism-bash.js?no-check";
-import "https://esm.sh/prismjs@1.29.0/components/prism-yaml.js?no-check";
+import Prism from "prismjs";
+import "prismjs/components/prism-jsx.js";
+import "prismjs/components/prism-typescript.js";
+import "prismjs/components/prism-tsx.js";
+import "prismjs/components/prism-diff.js";
+import "prismjs/components/prism-json.js";
+import "prismjs/components/prism-bash.js";
+import "prismjs/components/prism-yaml.js";
 
 export { extractYaml as frontMatter } from "@std/front-matter";
 
-import * as Marked from "https://esm.sh/marked@7.0.2";
+import * as Marked from "marked";
 import { escape as escapeHtml } from "@std/html";
-import { mangle } from "$marked-mangle";
+import { mangle } from "marked-mangle";
+import GitHubSlugger from "github-slugger";
+
+const slugger = new GitHubSlugger();
 
 Marked.marked.use(mangle());
 
@@ -25,7 +28,16 @@ export interface MarkdownHeading {
 class DefaultRenderer extends Marked.Renderer {
   headings: MarkdownHeading[] = [];
 
-  text(text: string): string {
+  override text(
+    token: Marked.Tokens.Text | Marked.Tokens.Escape | Marked.Tokens.Tag,
+  ): string {
+    let text = token.text;
+    if (
+      token.type === "text" && "tokens" in token && token.tokens !== undefined
+    ) {
+      text = this.parser.parseInline(token.tokens);
+    }
+
     // Smartypants typography enhancement
     return text
       .replaceAll("...", "&#8230;")
@@ -39,37 +51,32 @@ class DefaultRenderer extends Marked.Renderer {
       .replaceAll(/['](.*?)[']/g, "&#8216;$1&#8217;");
   }
 
-  heading(
-    text: string,
-    level: 1 | 2 | 3 | 4 | 5 | 6,
-    raw: string,
-    slugger: Marked.Slugger,
-  ): string {
+  override heading({
+    tokens,
+    depth,
+    raw,
+  }: Marked.Tokens.Heading): string {
     const slug = slugger.slug(raw);
+    const text = this.parser.parseInline(tokens);
     this.headings.push({ id: slug, html: text });
-    return `<h${level} id="${slug}"><a class="md-anchor" tabindex="-1" href="#${slug}">${text}<span aria-hidden="true">#</span></a></h${level}>`;
+    return `<h${depth} id="${slug}"><a class="md-anchor" tabindex="-1" href="#${slug}">${text}<span aria-hidden="true">#</span></a></h${depth}>`;
   }
 
-  link(href: string, title: string | null, text: string) {
+  override link({ href, title, tokens }: Marked.Tokens.Link) {
+    const text = this.parser.parseInline(tokens);
     const titleAttr = title ? ` title="${title}"` : "";
     if (href.startsWith("#")) {
       return `<a href="${href}"${titleAttr}>${text}</a>`;
     }
-    if (this.options.baseUrl) {
-      try {
-        href = new URL(href, this.options.baseUrl).href;
-      } catch (_) {
-        //
-      }
-    }
+
     return `<a href="${href}"${titleAttr} rel="noopener noreferrer">${text}</a>`;
   }
 
-  image(src: string, title: string | null, alt: string | null) {
-    return `<img src="${src}" alt="${alt ?? ""}" title="${title ?? ""}" />`;
+  override image({ href, text, title }: Marked.Tokens.Image) {
+    return `<img src="${href}" alt="${text ?? ""}" title="${title ?? ""}" />`;
   }
 
-  code(code: string, info: string | undefined): string {
+  override code({ lang: info, text }: Marked.Tokens.Code): string {
     // format: tsx
     // format: tsx my/file.ts
     // format: tsx "This is my title"
@@ -96,9 +103,9 @@ class DefaultRenderer extends Marked.Renderer {
       : undefined;
 
     if (grammar === undefined) {
-      out += `<pre><code class="notranslate">${escapeHtml(code)}</code></pre>`;
+      out += `<pre><code class="notranslate">${escapeHtml(text)}</code></pre>`;
     } else {
-      const html = Prism.highlight(code, grammar, lang);
+      const html = Prism.highlight(text, grammar, lang);
       out +=
         `<pre class="highlight highlight-source-${lang} notranslate lang-${lang}"><code>${html}</code></pre>`;
     }
@@ -107,8 +114,8 @@ class DefaultRenderer extends Marked.Renderer {
     return out;
   }
 
-  blockquote(quote: string): string {
-    const match = quote.match(ADMISSION_REG);
+  override blockquote({ text }: Marked.Tokens.Blockquote): string {
+    const match = text.match(ADMISSION_REG);
     if (match) {
       const label: Record<string, string> = {
         tip: "Tip",
@@ -116,13 +123,13 @@ class DefaultRenderer extends Marked.Renderer {
         info: "Info",
       };
       const type = match[1];
-      quote = quote.slice(match[0].length);
+      text = text.slice(match[0].length);
       const icon = `<svg class="icon"><use href="/icons.svg#${type}" /></svg>`;
       return `<blockquote class="admonition ${type}">\n<span class="admonition-header">${icon}${
         label[type]
-      }</span>${quote}</blockquote>\n`;
+      }</span>${text}</blockquote>\n`;
     }
-    return `<blockquote>\n${quote}</blockquote>\n`;
+    return `<blockquote>\n${text}</blockquote>\n`;
   }
 }
 

.github/dependabot.yml
@@ -1,6 +1,5 @@
 version: 2
 updates:
-
   - package-ecosystem: "github-actions"
     directory: "/"
     schedule:

.github/workflows/ci.yml
@@ -14,15 +14,15 @@ jobs:
     strategy:
       fail-fast: false
       matrix:
-        deno: ["canary", "v1.x"]
+        deno: ["canary", "rc"]
         os: [macOS-latest, windows-latest, ubuntu-latest]
         include:
-        - os: ubuntu-latest
-          cache_path: ~/.cache/deno/
-        - os: macos-latest
-          cache_path: ~/Library/Caches/deno/
-        - os: windows-latest
-          cache_path: ~\AppData\Local\deno\
+          - os: ubuntu-latest
+            cache_path: ~/.cache/deno/
+          - os: macos-latest
+            cache_path: ~/Library/Caches/deno/
+          - os: windows-latest
+            cache_path: ~\AppData\Local\deno\
 
     steps:
       - name: Checkout repo
@@ -44,7 +44,7 @@ jobs:
       - name: Spell-check
         if: startsWith(matrix.os, 'ubuntu') && matrix.deno == 'v1.x'
         uses: crate-ci/typos@master
-        
+
       - name: Type check project
         run: deno task check:types
 

.github/workflows/deploy.yml
@@ -21,7 +21,7 @@ jobs:
       - name: Install Deno
         uses: denoland/setup-deno@v1
         with:
-          deno-version: v1.x
+          deno-version: rc
 
       - name: Build step
         working-directory: ./www

.github/workflows/publish.yml
@@ -18,8 +18,8 @@ jobs:
       - name: Install Deno
         uses: denoland/setup-deno@v1
         with:
-          deno-version: v1.x
-        
+          deno-version: rc
+
       - name: Publish Fresh
         run: deno publish
 

init/src/init.ts
@@ -15,6 +15,18 @@ export const enum InitStep {
   Docker = "Docker",
 }
 
+function css(strs: TemplateStringsArray, ...exprs: string[]): string {
+  let out = "";
+
+  for (let i = 0; i < exprs.length; i++) {
+    out += strs[i];
+    out += String(exprs[i]);
+  }
+  out += strs.at(-1) ?? "";
+
+  return out;
+}
+
 export class InitError extends Error {}
 
 function error(tty: MockTTY, message: string): never {
@@ -203,15 +215,17 @@ export default {
     await writeFile("tailwind.config.ts", TAILWIND_CONFIG_TS);
   }
 
-  const GRADIENT_CSS = `
-  .fresh-gradient {
-    background-color: rgb(134, 239, 172);
-    background-image: linear-gradient(to right bottom, rgb(219, 234, 254), rgb(187, 247, 208), rgb(254, 249, 195));
-  }
-  `;
+  const GRADIENT_CSS = css`.fresh-gradient {
+  background-color: rgb(134, 239, 172);
+  background-image: linear-gradient(
+    to right bottom,
+    rgb(219, 234, 254),
+    rgb(187, 247, 208),
+    rgb(254, 249, 195)
+  );
+}`;
 
-  const NO_TAILWIND_STYLES = `
-*,
+  const NO_TAILWIND_STYLES = css`*,
 *::before,
 *::after {
   box-sizing: border-box;
@@ -226,8 +240,15 @@ button, [role="button"] {
   cursor: pointer;
 }
 code {
-  font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas,
-    "Liberation Mono", "Courier New", monospace;
+  font-family:
+    ui-monospace,
+    SFMono-Regular,
+    Menlo,
+    Monaco,
+    Consolas,
+    "Liberation Mono",
+    "Courier New",
+    monospace;
   font-size: 1em;
 }
 img,
@@ -243,9 +264,21 @@ video {
 html {
   line-height: 1.5;
   -webkit-text-size-adjust: 100%;
-  font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif,
-    "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  font-family:
+    ui-sans-serif,
+    system-ui,
+    -apple-system,
+    BlinkMacSystemFont,
+    "Segoe UI",
+    Roboto,
+    "Helvetica Neue",
+    Arial,
+    "Noto Sans",
+    sans-serif,
+    "Apple Color Emoji",
+    "Segoe UI Emoji",
+    "Segoe UI Symbol",
+    "Noto Color Emoji";
 }
 .transition-colors {
   transition-property: background-color, border-color, color, fill, stroke;
@@ -340,14 +373,12 @@ html {
   font-variant-numeric: tabular-nums;
 }
 
-${GRADIENT_CSS}
-`;
+${GRADIENT_CSS}`;
 
-  const TAILWIND_CSS = `@tailwind base;
+  const TAILWIND_CSS = css`@tailwind base;
 @tailwind components;
 @tailwind utilities;
-${GRADIENT_CSS}
-`;
+${GRADIENT_CSS}`;
 
   const cssStyles = useTailwind ? TAILWIND_CSS : NO_TAILWIND_STYLES;
   await writeFile("static/styles.css", cssStyles);

src/app.ts
@@ -35,9 +35,15 @@ const DEFAULT_NOT_ALLOWED_METHOD = () => {
   throw new HttpError(405);
 };
 
-export type ListenOptions = Partial<Deno.ServeTlsOptions> & {
-  remoteAddress?: string;
-};
+export type ListenOptions =
+  & Partial<
+    Deno.ServeTcpOptions & Deno.TlsCertifiedKeyPem
+  >
+  & {
+    remoteAddress?: string;
+  };
+
+Deno.serve;
 
 export let getRouter: <State>(app: App<State>) => Router<MiddlewareFn<State>>;
 // deno-lint-ignore no-explicit-any
@@ -186,8 +192,7 @@ export class App<State> {
 
     return async (
       req: Request,
-      conn: Deno.ServeHandlerInfo | Deno.ServeUnixHandlerInfo =
-        DEFAULT_CONN_INFO,
+      conn: Deno.ServeHandlerInfo = DEFAULT_CONN_INFO,
     ) => {
       const url = new URL(req.url);
       // Prevent open redirect attacks
@@ -237,11 +242,20 @@ export class App<State> {
     if (!options.onListen) {
       options.onListen = (params) => {
         const pathname = (this.config.basePath) + "/";
-        const protocol = options.key && options.cert ? "https:" : "http:";
+        const protocol = "key" in options && options.key && options.cert
+          ? "https:"
+          : "http:";
+
+        let hostname = params.hostname;
+        // Windows being windows...
+        if (
+          Deno.build.os === "windows" &&
+          (hostname === "0.0.0.0" || hostname === "::")
+        ) {
+          hostname = "localhost";
+        }
         // Work around https://github.com/denoland/deno/issues/23650
-        const hostname = params.hostname.startsWith("::")
-          ? `[${params.hostname}]`
-          : params.hostname;
+        hostname = hostname.startsWith("::") ? `[${hostname}]` : hostname;
         const address = colors.cyan(
           `${protocol}//${hostname}:${params.port}${pathname}`,
         );

src/context.ts
@@ -42,7 +42,7 @@ export interface FreshContext<State = unknown> {
   readonly url: URL;
   readonly params: Record<string, string>;
   readonly error: unknown;
-  readonly info: Deno.ServeHandlerInfo | Deno.ServeUnixHandlerInfo;
+  readonly info: Deno.ServeHandlerInfo;
   /**
    * Return a redirect response to the specified path. This is the
    * preferred way to do redirects in Fresh.
@@ -95,7 +95,7 @@ export class FreshReqContext<State>
   state: State = {} as State;
   data: unknown = undefined;
   error: unknown | null = null;
-  info: Deno.ServeHandlerInfo | Deno.ServeUnixHandlerInfo;
+  info: Deno.ServeHandlerInfo | Deno.ServeHandlerInfo;
 
   next: FreshContext<State>["next"];
 
@@ -112,7 +112,7 @@ export class FreshReqContext<State>
   constructor(
     req: Request,
     url: URL,
-    info: Deno.ServeHandlerInfo | Deno.ServeUnixHandlerInfo,
+    info: Deno.ServeHandlerInfo,
     params: Record<string, string>,
     config: ResolvedFreshConfig,
     next: FreshContext<State>["next"],

commit 1081aa2eb93a2d4dbe8be04bc737babd73330761

fix: docs markdown smartypants (#2671)
This was a regression introduced in
https://github.com/denoland/fresh/pull/2655

Fixes https://github.com/denoland/fresh/issues/2670

www/utils/markdown.ts
@@ -31,15 +31,14 @@ class DefaultRenderer extends Marked.Renderer {
   override text(
     token: Marked.Tokens.Text | Marked.Tokens.Escape | Marked.Tokens.Tag,
   ): string {
-    let text = token.text;
     if (
       token.type === "text" && "tokens" in token && token.tokens !== undefined
     ) {
-      text = this.parser.parseInline(token.tokens);
+      return this.parser.parseInline(token.tokens);
     }
 
     // Smartypants typography enhancement
-    return text
+    return token.text
       .replaceAll("...", "&#8230;")
       .replaceAll("--", "&#8212;")
       .replaceAll("---", "&#8211;")