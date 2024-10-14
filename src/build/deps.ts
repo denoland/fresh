c0e8b43d45fa1714f5b9b31757bc2e9aaad75104
Marvin Hagemeister <marvin@deno.com>
2024-10-14 14:47:10 +0200

    fix: make `esbuild-deno-loader` work with Deno 2

--- src/build/deps.ts
+++ src/build/deps.ts
@@ -6,5 +6,5 @@ export {
   toFileUrl,
 } from "https://deno.land/std@0.216.0/path/mod.ts";
 export { escape as regexpEscape } from "https://deno.land/std@0.216.0/regexp/escape.ts";
-export { denoPlugins } from "jsr:@luca/esbuild-deno-loader@0.10.3";
+export { denoPlugins } from "jsr:@luca/esbuild-deno-loader@0.11.0";
 export { assertEquals } from "https://deno.land/std@0.216.0/assert/mod.ts";
