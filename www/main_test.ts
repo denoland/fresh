946d5aac3835c8ce465075dcab041a31fc8cdc04
Brian H. Buchholz <4773480+bhb603@users.noreply.github.com>
2024-11-21 20:30:13 -0500

    add tests

--- www/main_test.ts
+++ www/main_test.ts
@@ -15,6 +15,78 @@ Deno.test("CORS should not set on GET /fresh-badge.svg", async () => {
   expect(resp.headers.get("cross-origin-resource-policy")).toEqual(null);
 });
 
+Deno.test("/docs route properly", async (t: Deno.TestContext) => {
+  const validPaths = [
+    "/introduction",
+    "/getting-started",
+    "/getting-started/create-a-project",
+    "/concepts",
+    "/concepts/routing",
+    "/examples/writing-tests",
+  ];
+
+  await t.step("redirects work", async () => {
+    const redirects: [string, string][] = [
+      // root redirect configured in  docs/index.tsx
+      ["/docs", "/docs/introduction"],
+      // redirects configured in in docs/_middleware.ts
+      [
+        "/docs/getting-started/fetching-data",
+        "http://localhost/docs/getting-started/custom-handlers",
+      ],
+      // version roots redirect to getFirstPageUrl(version)
+      ["/docs/latest", "/docs/introduction"],
+      ["/docs/canary", "/docs/canary/the-canary-version"],
+    ];
+
+    for (const [from, to] of redirects) {
+      const req = new Request(`http://localhost${from}`);
+      const resp = await handler(req);
+      expect(resp.status).toEqual(307);
+      expect(resp.headers.get("location")).toEqual(to);
+    }
+  });
+
+  await t.step("valid /docs/:slug requests should return 200", async () => {
+    for (const path of validPaths) {
+      const req = new Request(`http://localhost/docs${path}`);
+      const resp = await handler(req);
+      expect(resp.status).toEqual(200);
+    }
+  });
+
+  await t.step("valid /docs/:version/:slug should return 200", async () => {
+    for (const version of ["latest", "canary"]) {
+      for (const path of validPaths) {
+        const req = new Request(`http://localhost/docs/${version}${path}`);
+        const resp = await handler(req);
+        expect(resp.status).toEqual(200);
+      }
+    }
+  });
+
+  await t.step("invalid paths should 404", async () => {
+    const invalidPaths = [
+      "/foo",
+      "/foo/bar",
+      "/concepts/foo/bar",
+      "/latest/foo",
+      "/latest/foo/bar",
+      "/latest/concepts/foo/bar",
+      "/canary/foo",
+      "/canary/foo/bar",
+      "/canary/concepts/foo/bar",
+      "/foo/concepts",
+      "/foo/concepts/routing",
+    ];
+    for (const path of invalidPaths) {
+      const req = new Request(`http://localhost/docs/${path}`);
+      const resp = await handler(req);
+      expect(resp.status).toEqual(404);
+    }
+  });
+});
+
 Deno.test({
   name: "shows version selector",
   fn: async () => {
