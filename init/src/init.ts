65c05b21272cbfa33183f8ab27e3c593c9f1ef81
Marvin Hagemeister <marvin@deno.com>
2025-01-16 22:43:40 +0100

    2.0.0-alpha.29 (#2812)

--- init/src/init.ts
+++ init/src/init.ts
@@ -2,7 +2,7 @@ import * as colors from "@std/fmt/colors";
 import * as path from "@std/path";
 
 // Keep these as is, as we replace these version in our release script
-const FRESH_VERSION = "2.0.0-alpha.28";
+const FRESH_VERSION = "2.0.0-alpha.29";
 const FRESH_TAILWIND_VERSION = "0.0.1-alpha.7";
 const PREACT_VERSION = "10.25.4";
 const PREACT_SIGNALS_VERSION = "2.0.1";
