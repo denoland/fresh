82aabd54d210cd5e83d580f350d27256b9744870
Asher Gomez <ashersaupingomez@gmail.com>
2025-04-16 11:09:01 +1000

    x

--- init/src/init_test.ts
+++ init/src/init_test.ts
@@ -136,10 +136,6 @@ Deno.test.only("init - fmt, lint, and type check project", async () => {
       stderr: "inherit",
       stdout: "inherit",
     }).output();
-    // deno-lint-ignore no-console
-    console.log(new TextDecoder().decode(check.stdout));
-    // deno-lint-ignore no-console
-    console.log(new TextDecoder().decode(check.stderr));
     expect(check.code).toEqual(0);
   });
 });
