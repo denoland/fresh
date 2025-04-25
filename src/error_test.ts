07d9d27e07a1058814fe3001ff00c9a341443afe
Asher Gomez <ashersaupingomez@gmail.com>
2025-04-25 14:14:19 +1000

    work

--- src/error_test.ts
+++ src/error_test.ts
@@ -1,12 +1,14 @@
 import { expect } from "@std/expect";
 import { HttpError } from "./error.ts";
 
-Deno.test("HttpError - contains stack", () => {
+Deno.test("HttpError", () => {
   const err = new HttpError(404);
   expect(err.status).toEqual(404);
+  expect(err.message).toEqual("Not Found");
   expect(typeof err.stack).toEqual("string");
 
   const err2 = new HttpError(500);
   expect(err2.status).toEqual(500);
+  expect(err2.message).toEqual("Internal Server Error");
   expect(typeof err2.stack).toEqual("string");
 });
