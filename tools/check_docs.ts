93b0ac78c52b7dc7b14d9a86cf8f55bf9cf120d0
Asher Gomez <ashersaupingomez@gmail.com>
2025-04-30 16:10:00 +1000

    x

--- tools/check_docs.ts
+++ tools/check_docs.ts
@@ -1,4 +1,4 @@
-import { checkDocs } from "https://raw.githubusercontent.com/denoland/std/59d3d87463e33d60f03bc93b2856549d3b17c7cc/_tools/check_docs.ts";
+import { checkDocs } from "https://github.com/denoland/std/raw/refs/heads/main/_tools/check_docs.ts";
 
 await checkDocs([
   import.meta.resolve("../src/error.ts"),
