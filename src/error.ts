a7e4c864b9b5760162ce17920b8f32d4eb5f8c59
Asher Gomez <ashersaupingomez@gmail.com>
2025-04-30 12:22:33 +1000

    work

--- src/error.ts
+++ src/error.ts
@@ -1,8 +1,37 @@
 import { STATUS_TEXT } from "@std/http/status";
 
+/**
+ * Error that's thrown when a request fails. Correlates to a
+ * {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status | HTTP status}.
+ *
+ * @property status The HTTP status code.
+ *
+ * @example Basic usage
+ * ```ts
+ * import { App, HttpError } from "fresh";
+ * import { expect } from "@std/expect";
+ *
+ * const app = new App()
+ *   .get("/", () => new Response("ok"))
+ *   .get("/not-found", () => {
+ *      throw new HttpError(404, "Nothing here");
+ *    });
+ * ```
+ */
 export class HttpError extends Error {
+  /**
+   * The HTTP status code.
+   */
   status: number;
 
+  /**
+   * Constructs a new instance.
+   *
+   * @param status The HTTP status code.
+   * @param message The error message. Defaults to the status text of the given
+   * status code.
+   * @param options Optional error options.
+   */
   constructor(
     status: keyof typeof STATUS_TEXT,
     message: string = STATUS_TEXT[status],
--- tools/check_docs.ts
+++ tools/check_docs.ts
@@ -1,4 +1,4 @@
-import { checkDocs } from "../../std/_tools/check_docs.ts";
+import { checkDocs } from "https://raw.githubusercontent.com/denoland/std/abb5a28306999b182f20532e7b2646e00d652b77/_tools/check_docs.ts";
 
 await checkDocs([
   "./src/error.ts",
