5a0ae41036ebbbeb545002abdd2725f01f8de792
Asher Gomez <ashersaupingomez@gmail.com>
2025-05-14 18:39:35 +1000

    fix

--- src/dev/dev_build_cache_test.ts
+++ src/dev/dev_build_cache_test.ts
@@ -2,7 +2,6 @@ import { expect } from "@std/expect";
 import * as path from "@std/path";
 import { MemoryBuildCache } from "./dev_build_cache.ts";
 import { FreshFileTransformer } from "./file_transformer.ts";
-import { createFakeFs } from "../test_utils.ts";
 import type { ResolvedFreshConfig } from "../mod.ts";
 
 Deno.test({
@@ -18,7 +17,7 @@ Deno.test({
         outDir: path.join(tmp, "dist"),
       },
     };
-    const fileTransformer = new FreshFileTransformer(createFakeFs({}));
+    const fileTransformer = new FreshFileTransformer();
     const buildCache = new MemoryBuildCache(
       config,
       "testing",
