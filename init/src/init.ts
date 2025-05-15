7da83daed3a5f9ee70286dc0f7ec8e2e02d67d11
Matt Nield <64328730+matthewjnield@users.noreply.github.com>
2025-05-15 11:48:19 -0400

    Update links (#3)
    Update links
    
    This commit updates several links from their current URLs to the ones that they now redirect to. Any instances of `deno.land/x` and `deno.land/std` were left alone to avoid messing with dependencies, although these links may be able to be changed to their JSR equivalents.

--- init/src/init.ts
+++ init/src/init.ts
@@ -603,7 +603,7 @@ Started" guide here: https://fresh.deno.dev/docs/getting-started
 
 ### Usage
 
-Make sure to install Deno: https://deno.land/manual/getting_started/installation
+Make sure to install Deno: https://docs.deno.com/runtime/getting_started/installation
 
 Then start the project in development mode:
 
