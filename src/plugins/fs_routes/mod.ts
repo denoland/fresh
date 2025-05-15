598e58c1623b002180b83971f833d018f2dfb156
Marvin Hagemeister <marvin@deno.com>
2025-05-15 14:06:16 +0200

    fix(core): support method handler on _error, _404 and _500 routes

--- src/plugins/fs_routes/mod.ts
+++ src/plugins/fs_routes/mod.ts
@@ -258,7 +258,14 @@ export async function fsRoutes<State>(
           ? undefined
           : typeof handlers === "function"
           ? handlers
-          : undefined; // FIXME: Method handler
+          : ((ctx) => {
+            const { method } = ctx.req;
+            const maybeFn = handlers[method as Method];
+            if (maybeFn !== undefined) {
+              return maybeFn(ctx);
+            }
+            return ctx.next();
+          }) as HandlerFn<unknown, State>;
         const errorComponents = components.slice();
         if (mod.component !== null) {
           errorComponents.push(mod.component);
@@ -285,7 +292,14 @@ export async function fsRoutes<State>(
           ? undefined
           : typeof handlers === "function"
           ? handlers
-          : undefined; // FIXME: Method handler
+          : ((ctx) => {
+            const { method } = ctx.req;
+            const maybeFn = handlers[method as Method];
+            if (maybeFn !== undefined) {
+              return maybeFn(ctx);
+            }
+            return ctx.next();
+          }) as HandlerFn<unknown, State>;
         const notFoundComponents = components.slice();
         if (mod.component !== null) {
           notFoundComponents.push(mod.component);
