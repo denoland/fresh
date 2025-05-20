f049d57f2d90366fa1f117d5f14b6da64051bd54
Christian Svensson <csvn.dev@gmail.com>
2025-05-20 21:02:22 +0200

    fix: convert island filePath to valid variable name

--- src/utils_test.ts
+++ src/utils_test.ts
@@ -1,5 +1,5 @@
 import { expect } from "@std/expect";
-import { mergePaths } from "./utils.ts";
+import { mergePaths, pathToExportName } from "./utils.ts";
 
 Deno.test("mergePaths", () => {
   expect(mergePaths("", "")).toEqual("");
@@ -8,4 +8,12 @@ Deno.test("mergePaths", () => {
   expect(mergePaths("/foo/bar", "/baz")).toEqual("/foo/bar/baz");
   expect(mergePaths("/foo/bar/", "/baz")).toEqual("/foo/bar/baz");
   expect(mergePaths("/foo/bar", "baz")).toEqual("/foo/bar/baz");
+});
+
+Deno.test("filenameToExportName", () => {
+  expect(pathToExportName("/islands/foo.tsx")).toBe("foo");
+  expect(pathToExportName("/islands/foo.v2.tsx")).toBe("foo_v2");
+  expect(pathToExportName("/islands/nav-bar.tsx")).toBe("nav_bar");
+  expect(pathToExportName("/islands/_.$bar.tsx")).toBe("_$bar");
+  expect(pathToExportName("/islands/1.hello.tsx")).toBe("_hello");
 });
