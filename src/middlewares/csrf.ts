db94614fedf21f25a06cb8f0f566af0d442b3bc7
Octo8080 <okutannderuta0529@gmail.com>
2025-06-11 02:00:11 +0900

    Refactor CSRF middleware tests for clarity and coverage

---
+++ src/middlewares/csrf.ts
@@ -0,0 +1,94 @@
+import { FreshContext } from "fresh";
+
+type IsAllowedOriginHandler = (
+  origin: string,
+  context: FreshContext,
+) => boolean;
+export interface CSRFOptions {
+  origin?: string | string[] | IsAllowedOriginHandler;
+}
+
+const isSafeMethodRe = /^(GET|HEAD)$/;
+const isRequestedByFormElementRe =
+  /^\b(application\/x-www-form-urlencoded|multipart\/form-data|text\/plain)\b/i;
+
+/**
+ * CSRF Protection Middleware for Fresh.
+ * Adapted from Hono's CORS middleware.
+ * Would like to express my gratitude to all the developers of Hono.
+ *
+ * @param {CSRFOptions} [options] - The options for the CSRF protection middleware.
+ * @param {string|string[]|(origin: string, context: Context) => boolean} [options.origin] - Specify origins.
+ * @returns {MiddlewareHandler} The middleware handler function.
+ *
+ * @example
+ * ```ts
+ * const app = new App<State>()
+ *
+ * app.use(csrf())
+ *
+ * // Specifying origins with using `origin` option
+ * // string
+ * app.use(csrf({ origin: 'https://myapp.example.com' }))
+ *
+ * // string[]
+ * app.use(
+ *   csrf({
+ *     origin: ['https://myapp.example.com', 'http://development.myapp.example.com'],
+ *   })
+ * )
+ *
+ * // Function
+ * // It is strongly recommended that the protocol be verified to ensure a match to `$`.
+ * // You should *never* do a forward match.
+ * app.use(
+ *   '*',
+ *   csrf({
+ *     origin: (origin) => /https:\/\/(\w+\.)?myapp\.example\.com$/.test(origin),
+ *   })
+ * )
+ * ```
+ */
+export function csrf(
+  options?: CSRFOptions,
+): (ctx: FreshContext) => Promise<Response> {
+  const handler: IsAllowedOriginHandler = ((optsOrigin) => {
+    if (!optsOrigin) {
+      return (origin, ctx) => origin === new URL(ctx.req.url).origin;
+    } else if (typeof optsOrigin === "string") {
+      return (origin) => origin === optsOrigin;
+    } else if (typeof optsOrigin === "function") {
+      return optsOrigin;
+    } else {
+      return (origin) => optsOrigin.includes(origin);
+    }
+  })(options?.origin);
+  const isAllowedOrigin = (
+    origin: string | undefined | null,
+    ctx: FreshContext,
+  ) => {
+    if (origin === undefined || origin === null) {
+      return false;
+    }
+    return handler(origin, ctx);
+  };
+
+  return async (ctx: FreshContext): Promise<Response> => {
+    if (
+      !isSafeMethodRe.test(ctx.req.method) &&
+      isRequestedByFormElementRe.test(
+        ctx.req.headers.get("content-type") || "text/plain",
+      ) &&
+      !isAllowedOrigin(ctx.req.headers.get("origin"), ctx)
+    ) {
+      return new Response(
+        "Forbidden: CSRF protection failed. The request is not allowed from this origin.",
+        {
+          status: 403,
+        },
+      );
+    }
+
+    return await ctx.next();
+  };
+}
---
+++ src/middlewares/csrf_test.ts
@@ -0,0 +1,421 @@
+import { App, type FreshContext } from "fresh";
+import { csrf } from "./csrf.ts";
+import { beforeEach, describe, it } from "@std/testing/bdd";
+import { expect } from "@std/expect";
+import { spy } from "@std/testing/mock";
+
+const rawSimplePostHandler = async (ctx: FreshContext) => {
+  if (ctx.req.headers.get("content-type") === "application/json") {
+    return new Response((await ctx.req.json())["name"]);
+  } else {
+    return new Response((await ctx.req.formData()).get("name"));
+  }
+};
+
+describe("CSRF by Middleware", () => {
+  let simplePostHandler = spy(rawSimplePostHandler);
+
+  beforeEach(() => {
+    simplePostHandler = spy(rawSimplePostHandler);
+  });
+
+  //  describe('simple usage', () => {
+  const app = new App();
+
+  //
+  app.all("*", csrf());
+  app.get("/form", (_ctx: FreshContext) => new Response("<form></form>"));
+  app.post("/form", simplePostHandler);
+  app.put("/form", (_ctx: FreshContext) => new Response("OK"));
+  app.delete("/form", (_ctx: FreshContext) => new Response("OK"));
+  app.patch("/form", (_ctx: FreshContext) => new Response("OK"));
+  app.head("/form", (_ctx: FreshContext) => new Response("OK"));
+
+  const handler = app.handler();
+
+  describe("GET /form", () => {
+    it("should be 200 for any request", async () => {
+      const res = await handler(new Request("http://localhost/form"));
+
+      expect(res.status).toBe(200);
+      expect(await res.text()).toBe("<form></form>");
+    });
+  });
+
+  describe("HEAD /form", () => {
+    it("should be 200 for any request", async () => {
+      const res = await handler(
+        new Request("http://localhost/form", {
+          method: "HEAD",
+        }),
+      );
+
+      expect(res.status).toBe(200);
+    });
+  });
+
+  describe("POST /form", () => {
+    it("should be 200 for local request", async () => {
+      /*
+       * <form action="/form" method="POST"><input name="name" value="hono" /></form>
+       * or
+       * <script>
+       * fetch('/form', {
+       *   method: 'POST',
+       *   headers: {
+       *     'content-type': 'application/x-www-form-urlencoded',
+       *   },
+       *   body: 'name=hono',
+       * });
+       * </script>
+       */
+      const res = await handler(
+        new Request("http://localhost/form", {
+          method: "POST",
+          headers: {
+            "content-type": "application/x-www-form-urlencoded",
+            "origin": "http://localhost",
+          },
+          body: "name=fresh",
+        }),
+      );
+
+      expect(res.status).toBe(200);
+      expect(await res.text()).toBe("fresh");
+    });
+
+    it('should be 403 for "application/x-www-form-urlencoded" cross origin', async () => {
+      /*
+       * via http://example.com
+       *
+       * <form action="http://localhost/form" method="POST">
+       *   <input name="name" value="hono" />
+       * </form>
+       * or
+       * <script>
+       * fetch('http://localhost/form', {
+       *   method: 'POST',
+       *   headers: {
+       *     'content-type': 'application/x-www-form-urlencoded',
+       *   },
+       *   body: 'name=hono',
+       * });
+       * </script>
+       */
+      const res = await handler(
+        new Request("http://localhost/form", {
+          method: "POST",
+          headers: {
+            "content-type": "application/x-www-form-urlencoded",
+            "origin": "http://example.com",
+          },
+          body: "name=fresh",
+        }),
+      );
+
+      expect(res.status).toBe(403);
+      expect(simplePostHandler.calls.length).toBe(0);
+    });
+    it('should be 403 for "multipart/form-data" cross origin', async () => {
+      /*
+       * via http://example.com
+       *
+       * <form action="http://localhost/form" method="POST" enctype="multipart/form-data">
+       *   <input name="name" value="hono" />
+       * </form>
+       * or
+       * <script>
+       * fetch('http://localhost/form', {
+       *   method: 'POST',
+       *   headers: {
+       *     'content-type': 'multipart/form-data',
+       *   },
+       *   body: 'name=fresh',
+       * });
+       * </script>
+       */
+      const res = await handler(
+        new Request("http://localhost/form", {
+          method: "POST",
+          headers: {
+            "content-type": "multipart/form-data",
+            "origin": "http://example.com",
+          },
+          body: "name=fresh",
+        }),
+      );
+
+      expect(res.status).toBe(403);
+      expect(simplePostHandler.calls.length).toBe(0);
+    });
+
+    it('should be 403 for "text/plain" cross origin', async () => {
+      /*
+       * via http://example.com
+       *
+       * <form action="http://localhost/form" method="POST" enctype="text/plain">
+       *   <input name="name" value="hono" />
+       * </form>
+       * or
+       * <script>
+       * fetch('http://localhost/form', {
+       *   method: 'POST',
+       *   headers: {
+       *     'content-type': 'text/plain',
+       *   },
+       *   body: 'name=hono',
+       * });
+       * </script>
+       */
+      const res = await handler(
+        new Request("http://localhost/form", {
+          method: "POST",
+          headers: {
+            "content-type": "text/plain",
+            "origin": "http://example.com",
+          },
+          body: "name=fresh",
+        }),
+      );
+
+      expect(res.status).toBe(403);
+      expect(simplePostHandler.calls.length).toBe(0);
+    });
+
+    it("should be 403 if request has no origin header", async () => {
+      const res = await app.handler()(
+        new Request("http://localhost/form", {
+          method: "POST",
+          headers: {
+            "content-type": "application/x-www-form-urlencoded",
+          },
+          body: "name=fresh",
+        }),
+      );
+
+      expect(res.status).toBe(403);
+      expect(simplePostHandler.calls.length).toBe(0);
+    });
+
+    it("should be 200 for application/json", async () => {
+      /*
+       * via http://example.com
+       * Assume localhost allows cross origin POST
+       *
+       * <script>
+       * fetch('http://localhost/form', {
+       *   method: 'POST',
+       *   headers: {
+       *     'content-type': 'application/json',
+       *   },
+       *   body: JSON.stringify({ name: 'fresh' }),
+       * });
+       * </script>
+       */
+      const res = await handler(
+        new Request("http://localhost/form", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+            origin: "http://example.com",
+          },
+          body: JSON.stringify({ name: "fresh" }),
+        }),
+      );
+
+      expect(res.status).toBe(200);
+      expect(await res.text()).toBe("fresh");
+    });
+
+    it('should be 403 for "Application/x-www-form-urlencoded" cross origin', async () => {
+      const res = await app.handler()(
+        new Request("http://localhost/form", {
+          method: "POST",
+          headers: {
+            "content-type": "Application/x-www-form-urlencoded",
+          },
+          body: "name=fresh",
+        }),
+      );
+      expect(res.status).toBe(403);
+      expect(simplePostHandler.calls.length).toBe(0);
+    });
+
+    it("should be 403 if the content-type is not set", async () => {
+      const res = await handler(
+        new Request("http://localhost/form", {
+          method: "POST",
+          body: new Blob(["test"], {}),
+        }),
+      );
+      expect(res.status).toBe(403);
+      expect(simplePostHandler.calls.length).toBe(0);
+    });
+  });
+
+  describe("with origin option", () => {
+    describe("string", () => {
+      const app = new App();
+
+      app.all(
+        "*",
+        csrf({
+          origin: "https://example.com",
+        }),
+      );
+      app.post("/form", simplePostHandler);
+
+      const handler = app.handler();
+
+      it("should be 200 for allowed origin", async () => {
+        const res = await handler(
+          new Request("https://example.com/form", {
+            method: "POST",
+            headers: {
+              "content-type": "application/x-www-form-urlencoded",
+              "origin": "https://example.com",
+            },
+            body: "name=fresh",
+          }),
+        );
+        expect(res.status).toBe(200);
+      });
+
+      it("should be 403 for not allowed origin", async () => {
+        const res = await handler(
+          new Request("https://example.jp/form", {
+            method: "POST",
+            headers: {
+              "content-type": "application/x-www-form-urlencoded",
+              "origin": "https://example.jp",
+            },
+            body: "name=fresh",
+          }),
+        );
+        expect(res.status).toBe(403);
+        expect(simplePostHandler.calls.length).toBe(0);
+      });
+    });
+
+    describe("string[]", () => {
+      const app = new App();
+
+      app.all(
+        "*",
+        csrf({
+          origin: ["https://example.com", "https://fresh.example.com"],
+        }),
+      );
+      app.post("/form", simplePostHandler);
+
+      const handler = app.handler();
+
+      it("should be 200 for allowed origin", async () => {
+        let res = await handler(
+          new Request("https://fresh.example.com/form", {
+            method: "POST",
+            headers: {
+              "content-type": "application/x-www-form-urlencoded",
+              "origin": "https://fresh.example.com",
+            },
+            body: "name=fresh",
+          }),
+        );
+        expect(res.status).toBe(200);
+
+        res = await handler(
+          new Request("https://example.com/form", {
+            method: "POST",
+            headers: {
+              "content-type": "application/x-www-form-urlencoded",
+              "origin": "https://example.com",
+            },
+            body: "name=fresh",
+          }),
+        );
+        expect(res.status).toBe(200);
+      });
+
+      it("should be 403 for not allowed origin", async () => {
+        const res = await handler(
+          new Request("http://example.jp/form", {
+            method: "POST",
+            headers: {
+              "content-type": "application/x-www-form-urlencoded",
+              "origin": "http://example.jp",
+            },
+            body: "name=fresh",
+          }),
+        );
+        expect(res.status).toBe(403);
+        expect(simplePostHandler.calls.length).toBe(0);
+      });
+    });
+
+    describe("IsAllowedOriginHandler", () => {
+      const app = new App();
+
+      app.all(
+        "*",
+        csrf({
+          origin: (origin) => /https:\/\/(\w+\.)?example\.com$/.test(origin),
+        }),
+      );
+      app.post("/form", simplePostHandler);
+
+      const handler = app.handler();
+      it("should be 200 for allowed origin", async () => {
+        let res = await handler(
+          new Request("https://fresh.example.com/form", {
+            method: "POST",
+            headers: {
+              "content-type": "application/x-www-form-urlencoded",
+              "origin": "https://fresh.example.com",
+            },
+            body: "name=fresh",
+          }),
+        );
+        expect(res.status).toBe(200);
+        res = await handler(
+          new Request("https://example.com/form", {
+            method: "POST",
+            headers: {
+              "content-type": "application/x-www-form-urlencoded",
+              "origin": "https://example.com",
+            },
+            body: "name=fresh",
+          }),
+        );
+        expect(res.status).toBe(200);
+      });
+
+      it("should be 403 for not allowed origin", async () => {
+        let res = await handler(
+          new Request("http://deno.fresh.example.jp/form", {
+            method: "POST",
+            headers: {
+              "content-type": "application/x-www-form-urlencoded",
+              "origin": "http://deno.fresh.example.jp",
+            },
+            body: "name=fresh",
+          }),
+        );
+        expect(res.status).toBe(403);
+        expect(simplePostHandler.calls.length).toBe(0);
+
+        res = await handler(
+          new Request("http://example.jp/form", {
+            method: "POST",
+            headers: {
+              "content-type": "application/x-www-form-urlencoded",
+              "origin": "http://example.jp",
+            },
+            body: "name=fresh",
+          }),
+        );
+        expect(res.status).toBe(403);
+        expect(simplePostHandler.calls.length).toBe(0);
+      });
+    });
+  });
+});
