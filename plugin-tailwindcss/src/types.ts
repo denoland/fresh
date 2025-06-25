0e18b083891e352401fba88236cee2d8680e88bf
李嘉图·M·路 <146103794+Ricardo-M-Zheng@users.noreply.github.com>
2025-06-25 14:02:55 +0800

    Update types.ts

--- plugin-tailwindcss/src/types.ts
+++ plugin-tailwindcss/src/types.ts
@@ -1,4 +1,4 @@
-import type { OnTransformOptions } from "@fresh/core/dev";
+import type { OnTransformOptions } from "fresh/dev";
 
 export interface TailwindPluginOptions {
   /** Exclude paths or globs that should not be processed */
