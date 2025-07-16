denoland/fresh

commit c11cf7b41aefb7384cd1d844e8b5128960cf8138

feat: expose layouts and pages on `App` (#3086)
This PR extracts the functionality from the `fs_routes` plugin and
exposes it onto `App`. These are the new APIs:

- `app.layout(pattern, route)`
- `app.onError(pattern, route)`
- `app.notFound(pattern, route)`
- `app.route(pattern, route)`

This also removes the need for `.use(staticFiles())`. This line can be
safely removed.

Fixes https://github.com/denoland/fresh/issues/3040
Fixes https://github.com/denoland/fresh/issues/3033
Fixes https://github.com/denoland/fresh/issues/2591
Fixes https://github.com/denoland/fresh/issues/950
Fixes https://github.com/denoland/fresh/issues/1918

src/app.ts
@@ -5,18 +5,32 @@ import { trace } from "@opentelemetry/api";
 import { DENO_DEPLOYMENT_ID } from "./runtime/build_id.ts";
 import * as colors from "@std/fmt/colors";
 import { type MiddlewareFn, runMiddlewares } from "./middlewares/mod.ts";
-import { FreshReqContext } from "./context.ts";
-import { type Method, type Router, UrlPatternRouter } from "./router.ts";
+import { Context, type ServerIslandRegistry } from "./context.ts";
+import {
+  mergePath,
+  type Method,
+  type Router,
+  UrlPatternRouter,
+} from "./router.ts";
 import {
   type FreshConfig,
   normalizeConfig,
   type ResolvedFreshConfig,
 } from "./config.ts";
 import { type BuildCache, ProdBuildCache } from "./build_cache.ts";
-import type { ServerIslandRegistry } from "./context.ts";
 import { FinishSetup, ForgotBuild } from "./finish_setup.tsx";
 import { HttpError } from "./error.ts";
-import { mergePaths, pathToExportName } from "./utils.ts";
+import { pathToExportName } from "./utils.ts";
+import type { LayoutConfig, Route } from "./types.ts";
+import {
+  getOrCreateSegment,
+  newSegment,
+  renderRoute,
+  type RouteComponent,
+  segmentToMiddlewares,
+} from "./segments.ts";
+import { isHandlerByMethod, type PageResponse } from "./handlers.ts";
+import { serveInternalStaticFiles } from "./middlewares/static_files.ts";
 
 // TODO: Completed type clashes in older Deno versions
 // deno-lint-ignore no-explicit-any
@@ -25,11 +39,43 @@ export const DEFAULT_CONN_INFO: any = {
   remoteAddr: { transport: "tcp", hostname: "localhost", port: 1234 },
 };
 
-const DEFAULT_NOT_FOUND = () => {
+/** Used to group mounted apps. Only internal */
+let INTERNAL_ID = 0;
+
+const DEFAULT_RENDER = <State>(): Promise<PageResponse<State>> =>
+  // deno-lint-ignore no-explicit-any
+  Promise.resolve({ data: {} as any });
+
+const DEFAULT_NOT_FOUND = (): Promise<Response> => {
   throw new HttpError(404);
 };
-const DEFAULT_NOT_ALLOWED_METHOD = () => {
+const DEFAULT_NOT_ALLOWED_METHOD = (): Promise<Response> => {
   throw new HttpError(405);
+};
+const defaultOptionsHandler = (methods: string[]): () => Promise<Response> => {
+  return () =>
+    Promise.resolve(
+      new Response(null, {
+        status: 204,
+        headers: { Allow: methods.join(", ") },
+      }),
+    );
+};
+// deno-lint-ignore require-await
+const DEFAULT_ERROR_HANDLER = async <State>(ctx: Context<State>) => {
+  const { error } = ctx;
+
+  if (error instanceof HttpError) {
+    if (error.status >= 500) {
+      // deno-lint-ignore no-console
+      console.error(error);
+    }
+    return new Response(error.message, { status: error.status });
+  }
+
+  // deno-lint-ignore no-console
+  console.error(error);
+  return new Response("Internal server error", { status: 500 });
 };
 
 export type ListenOptions =
@@ -117,7 +163,6 @@ async function listenOnFreePort(
   throw firstError;
 }
 
-export let getRouter: <State>(app: App<State>) => Router<MiddlewareFn<State>>;
 // deno-lint-ignore no-explicit-any
 export let getIslandRegistry: (app: App<any>) => ServerIslandRegistry;
 // deno-lint-ignore no-explicit-any
@@ -132,9 +177,15 @@ export class App<State> {
   #islandRegistry: ServerIslandRegistry = new Map();
   #buildCache: BuildCache | null = null;
   #islandNames = new Set<string>();
+  #root = newSegment<State>("", null);
+  #routeDefs: {
+    method: Method | "ALL";
+    pattern: string;
+    fns: MiddlewareFn<State>[];
+    unshift: boolean;
+  }[] = [];
 
   static {
-    getRouter = (app) => app.#router;
     getIslandRegistry = (app) => app.#islandRegistry;
     getBuildCache = (app) => app.#buildCache;
     setBuildCache = (app, cache) => app.#buildCache = cache;
@@ -175,76 +226,170 @@ export class App<State> {
     return this;
   }
 
-  use(middleware: MiddlewareFn<State>): this {
-    this.#router.addMiddleware(middleware);
+  use(...middleware: MiddlewareFn<State>[]): this;
+  use(path: string, ...middleware: MiddlewareFn<State>[]): this;
+  use(
+    pathOrMiddleware: string | MiddlewareFn<State>,
+    ...middlewares: MiddlewareFn<State>[]
+  ): this {
+    let path: string;
+    let fns: MiddlewareFn<State>[];
+    if (typeof pathOrMiddleware === "string") {
+      path = pathOrMiddleware;
+      fns = middlewares!;
+    } else {
+      path = "*";
+      middlewares.unshift(pathOrMiddleware);
+      fns = middlewares;
+    }
+
+    const segment = getOrCreateSegment(this.#root, path, false);
+    segment.middlewares.push(...fns);
+
+    return this;
+  }
+
+  notFound(routeOrMiddleware: Route<State> | MiddlewareFn<State>): this {
+    const route = typeof routeOrMiddleware === "function"
+      ? { handler: routeOrMiddleware }
+      : routeOrMiddleware;
+    ensureHandler(route);
+    this.#root.notFound = (ctx) => renderRoute(ctx, route);
+
+    return this;
+  }
+
+  onError(
+    path: string,
+    routeOrMiddleware: Route<State> | MiddlewareFn<State>,
+  ): this {
+    const segment = getOrCreateSegment(this.#root, path, true);
+    segment.errorRoute = typeof routeOrMiddleware === "function"
+      ? { handler: routeOrMiddleware }
+      : routeOrMiddleware;
+
+    ensureHandler(segment.errorRoute);
+
+    return this;
+  }
+
+  appWrapper(component: RouteComponent<State>) {
+    const segment = getOrCreateSegment<State>(this.#root, "", false);
+    segment.app = component;
+  }
+
+  layout(
+    path: string,
+    component: RouteComponent<State>,
+    config?: LayoutConfig,
+  ): this {
+    const segment = getOrCreateSegment<State>(this.#root, path, true);
+    segment.layout = { component, config: config ?? null };
+
+    return this;
+  }
+
+  route(path: string, route: Route<State>): this {
+    const segment = getOrCreateSegment<State>(this.#root, path, false);
+    const middlewares = segmentToMiddlewares(segment);
+
+    ensureHandler(route);
+    middlewares.push((ctx) => renderRoute(ctx, route));
+
+    const routePath = route.config?.routeOverride ?? path;
+
+    if (typeof route.handler === "function") {
+      this.#addRoute("ALL", routePath, middlewares);
+    } else if (isHandlerByMethod(route.handler!)) {
+      for (const method of Object.keys(route.handler)) {
+        this.#addRoute(method as Method, routePath, middlewares);
+      }
+    }
+
     return this;
   }
 
   get(path: string, ...middlewares: MiddlewareFn<State>[]): this {
-    return this.#addRoutes("GET", path, middlewares);
+    this.#addMiddleware("GET", path, middlewares);
+    return this;
   }
   post(path: string, ...middlewares: MiddlewareFn<State>[]): this {
-    return this.#addRoutes("POST", path, middlewares);
+    this.#addMiddleware("POST", path, middlewares);
+    return this;
   }
   patch(path: string, ...middlewares: MiddlewareFn<State>[]): this {
-    return this.#addRoutes("PATCH", path, middlewares);
+    this.#addMiddleware("PATCH", path, middlewares);
+    return this;
   }
   put(path: string, ...middlewares: MiddlewareFn<State>[]): this {
-    return this.#addRoutes("PUT", path, middlewares);
+    this.#addMiddleware("PUT", path, middlewares);
+    return this;
   }
   delete(path: string, ...middlewares: MiddlewareFn<State>[]): this {
-    return this.#addRoutes("DELETE", path, middlewares);
+    this.#addMiddleware("DELETE", path, middlewares);
+    return this;
   }
   head(path: string, ...middlewares: MiddlewareFn<State>[]): this {
-    return this.#addRoutes("HEAD", path, middlewares);
+    this.#addMiddleware("HEAD", path, middlewares);
+    return this;
   }
+
   all(path: string, ...middlewares: MiddlewareFn<State>[]): this {
-    return this.#addRoutes("ALL", path, middlewares);
+    this.#addMiddleware("ALL", path, middlewares);
+    return this;
+  }
+
+  #addMiddleware(
+    method: Method | "ALL",
+    path: string,
+    fns: MiddlewareFn<State>[],
+    unshift = false,
+  ) {
+    const segment = getOrCreateSegment<State>(this.#root, path, false);
+    const result = segmentToMiddlewares(segment);
+
+    result.push(...fns);
+
+    const resPath = mergePath(this.config.basePath, path);
+    this.#addRoute(method, resPath, result, unshift);
+  }
+
+  #addRoute(
+    method: Method | "ALL",
+    path: string,
+    fns: MiddlewareFn<State>[],
+    unshift = false,
+  ) {
+    this.#routeDefs.push({ method, pattern: path, fns, unshift });
   }
 
   mountApp(path: string, app: App<State>): this {
-    const routes = app.#router._routes;
+    const segmentPath = mergePath(path, `/__${INTERNAL_ID++}/`);
+    const segment = getOrCreateSegment(this.#root, segmentPath, true);
+    const fns = segmentToMiddlewares(segment);
+
+    const routes = app.#routeDefs;
+    for (let i = 0; i < routes.length; i++) {
+      const route = routes[i];
+
+      const merged = mergePath(path, route.pattern);
+      const mergedFns = [...fns, ...route.fns];
+      this.#addRoute(route.method, merged, mergedFns, route.unshift);
+    }
+
     app.#islandRegistry.forEach((value, key) => {
       this.#islandRegistry.set(key, value);
     });
 
-    const middlewares = app.#router._middlewares;
-
-    // Special case when user calls one of these:
-    // - `app.mountApp("/", otherApp)`
-    // - `app.mountApp("/*", otherApp)`
-    const isSelf = path === "/*" || path === "/";
-    if (isSelf && middlewares.length > 0) {
-      this.#router._middlewares.push(...middlewares);
-    }
-
-    for (let i = 0; i < routes.length; i++) {
-      const route = routes[i];
-
-      const merged = typeof route.path === "string"
-        ? mergePaths(path, route.path)
-        : route.path;
-      const combined = isSelf
-        ? route.handlers
-        : middlewares.concat(route.handlers);
-      this.#router.add(route.method, merged, combined);
-    }
+    app.#root.notFound = this.#root.notFound;
 
     return this;
   }
 
-  #addRoutes(
-    method: Method | "ALL",
-    pathname: string | URLPattern,
-    middlewares: MiddlewareFn<State>[],
-  ): this {
-    const merged = typeof pathname === "string"
-      ? mergePaths(this.config.basePath, pathname)
-      : pathname;
-    this.#router.add(method, merged, middlewares);
-    return this;
-  }
-
+  /**
+   * Create handler function for `Deno.serve` or to be used in
+   * testing.
+   */
   handler(): (
     request: Request,
     info?: Deno.ServeHandlerInfo,
@@ -263,6 +408,27 @@ export class App<State> {
       return missingBuildHandler;
     }
 
+    const staticMids: MiddlewareFn<State>[] = [];
+    if (this.#root.middlewares.length > 0) {
+      staticMids.push(...this.#root.middlewares);
+    }
+
+    staticMids.push(serveInternalStaticFiles());
+
+    this.#addMiddleware(
+      "ALL",
+      "*",
+      staticMids,
+      true,
+    );
+
+    for (let i = 0; i < this.#routeDefs.length; i++) {
+      const route = this.#routeDefs[i];
+      this.#router.add(route.method, route.pattern, route.fns, route.unshift);
+    }
+
+    const rootMiddlewares = this.#root.middlewares;
+
     return async (
       req: Request,
       conn: Deno.ServeHandlerInfo = DEFAULT_CONN_INFO,
@@ -273,13 +439,32 @@ export class App<State> {
 
       const method = req.method.toUpperCase() as Method;
       const matched = this.#router.match(method, url);
+      let { params, pattern, handlers, methodMatch } = matched;
 
-      const next = matched.patternMatch && !matched.methodMatch
-        ? DEFAULT_NOT_ALLOWED_METHOD
-        : DEFAULT_NOT_FOUND;
+      const span = trace.getActiveSpan();
+      if (span && pattern) {
+        span.updateName(`${method} ${pattern}`);
+        span.setAttribute("http.route", pattern);
+      }
 
-      const { params, handlers, pattern } = matched;
-      const ctx = new FreshReqContext<State>(
+      let next: () => Promise<Response>;
+
+      if (pattern === null || !methodMatch) {
+        handlers = rootMiddlewares;
+      }
+
+      if (matched.pattern !== null && !methodMatch) {
+        if (method === "OPTIONS") {
+          const allowed = this.#router.getAllowedMethods(matched.pattern);
+          next = defaultOptionsHandler(allowed);
+        } else {
+          next = DEFAULT_NOT_ALLOWED_METHOD;
+        }
+      } else {
+        next = DEFAULT_NOT_FOUND;
+      }
+
+      const ctx = new Context<State>(
         req,
         url,
         conn,
@@ -290,19 +475,10 @@ export class App<State> {
         this.#buildCache!,
       );
 
-      const span = trace.getActiveSpan();
-      if (span && pattern) {
-        span.updateName(`${method} ${pattern}`);
-        span.setAttribute("http.route", pattern);
-      }
-
       try {
-        let result: unknown;
-        if (handlers.length === 1 && handlers[0].length === 1) {
-          result = await handlers[0][0](ctx);
-        } else {
-          result = await runMiddlewares(handlers, ctx);
-        }
+        if (handlers.length === 0) return await next();
+
+        const result = await runMiddlewares(handlers, ctx);
         if (!(result instanceof Response)) {
           throw new Error(
             `Expected a "Response" instance to be returned, but got: ${result}`,
@@ -311,17 +487,8 @@ export class App<State> {
 
         return result;
       } catch (err) {
-        if (err instanceof HttpError) {
-          if (err.status >= 500) {
-            // deno-lint-ignore no-console
-            console.error(err);
-          }
-          return new Response(err.message, { status: err.status });
-        }
-
-        // deno-lint-ignore no-console
-        console.error(err);
-        return new Response("Internal server error", { status: 500 });
+        ctx.error = err;
+        return await DEFAULT_ERROR_HANDLER(ctx);
       }
     };
   }
@@ -331,7 +498,7 @@ export class App<State> {
       options.onListen = createOnListen(this.config.basePath, options);
     }
 
-    const handler = await this.handler();
+    const handler = this.handler();
     if (options.port) {
       await Deno.serve(options, handler);
       return;
@@ -351,3 +518,15 @@ const missingBuildHandler = async (): Promise<Response> => {
     : renderToString(h(ForgotBuild, null));
   return new Response(html, { headers, status: 500 });
 };
+
+function ensureHandler<State>(route: Route<State>) {
+  if (route.handler === undefined) {
+    route.handler = route.component !== undefined
+      ? DEFAULT_RENDER
+      : DEFAULT_NOT_FOUND;
+  } else if (isHandlerByMethod(route.handler)) {
+    if (route.component !== undefined && !route.handler.GET) {
+      route.handler.GET = DEFAULT_RENDER;
+    }
+  }
+}

src/plugins/fs_routes/mod_test.tsx
@@ -13,7 +13,8 @@ import { stub } from "@std/testing/mock";
 import { type HandlerByMethod, type HandlerFn, page } from "../../handlers.ts";
 import type { Method } from "../../router.ts";
 import { parseHtml } from "../../../tests/test_utils.tsx";
-import type { FreshContext } from "fresh";
+import type { FreshContext } from "../../context.ts";
+import { HttpError } from "../../error.ts";
 
 async function createServer<T>(
   files: Record<string, string | Uint8Array | FreshFsItem<T>>,
@@ -193,13 +194,13 @@ Deno.test("fsRoutes - middleware", async () => {
 Deno.test("fsRoutes - nested middlewares", async () => {
   const server = await createServer<{ text: string }>({
     "routes/_middleware.ts": {
-      handler: (ctx) => {
+      handler: function A(ctx) {
         ctx.state.text = "A";
         return ctx.next();
       },
     },
     "routes/foo/_middleware.ts": {
-      handler: (ctx) => {
+      handler: function B(ctx) {
         ctx.state.text += "B";
         return ctx.next();
       },
@@ -332,18 +333,22 @@ Deno.test("fsRoutes - nested _layout", async () => {
       default: () => <>foo</>,
     },
     "routes/foo/_layout.tsx": {
-      default: (ctx) => (
-        <>
-          layout_foo_bar/<ctx.Component />
-        </>
-      ),
+      default: function fooBar(ctx) {
+        return (
+          <>
+            layout_foo_bar/<ctx.Component />
+          </>
+        );
+      },
     },
     "routes/_layout.tsx": {
-      default: (ctx) => (
-        <>
-          layout/<ctx.Component />
-        </>
-      ),
+      default: function rootLayout(ctx) {
+        return (
+          <>
+            layout/<ctx.Component />
+          </>
+        );
+      },
     },
     "routes/_app.tsx": {
       default: (ctx) => (
@@ -806,7 +811,7 @@ Deno.test("fsRoutes - _error render component", async () => {
     },
     "routes/foo/index.tsx": {
       handlers: () => {
-        throw new Error("ok");
+        throw new Error("failing");
       },
     },
   });
@@ -816,7 +821,8 @@ Deno.test("fsRoutes - _error render component", async () => {
   expect(doc.body.firstChild?.textContent).toEqual("ok");
 });
 
-Deno.test("fsRoutes - _error render on 404", async () => {
+// 404 pages go to the root error page if available
+Deno.test.ignore("fsRoutes - _error render on 404", async () => {
   // deno-lint-ignore no-explicit-any
   let error: any = null;
   const server = await createServer({
@@ -828,7 +834,7 @@ Deno.test("fsRoutes - _error render on 404", async () => {
       },
     },
     "routes/foo/_error.tsx": {
-      default: (ctx) => {
+      default: function foo2(ctx) {
         // deno-lint-ignore no-explicit-any
         error = ctx.error as any;
         return <p>ok foo</p>;
@@ -1110,34 +1116,23 @@ Deno.test(
   "fsRoutes - returns response code from error route",
   async () => {
     const server = await createServer<{ text: string }>({
-      "routes/_app.tsx": {
-        default: (ctx) => {
-          return (
-            <div>
-              _app/<ctx.Component />
-            </div>
-          );
-        },
-      },
       "routes/_error.tsx": {
         default: () => <div>fail</div>,
       },
-      "routes/index.tsx": {
-        default: () => <div>index</div>,
-      },
-      "routes/bar.tsx": {
-        default: () => <div>index</div>,
-      },
-      "routes/foo/index.tsx": {
-        default: () => <div>foo/index</div>,
-      },
-      "routes/foo/_error.tsx": {
+      "routes/fail.tsx": {
         default: () => {
           throw new Error("fail");
         },
       },
-      "routes/foo/bar.tsx": {
-        default: () => <div>foo/index</div>,
+      "routes/foo/_error.tsx": {
+        default: () => {
+          throw new HttpError(501);
+        },
+      },
+      "routes/foo/fail.tsx": {
+        default: () => {
+          throw new Error("fail");
+        },
       },
     });
 
@@ -1145,9 +1140,13 @@ Deno.test(
     await res.body?.cancel();
     expect(res.status).toEqual(404);
 
-    res = await server.get("/foo/asdf");
+    res = await server.get("/fail");
     await res.body?.cancel();
     expect(res.status).toEqual(500);
+
+    res = await server.get("/foo/fail");
+    await res.body?.cancel();
+    expect(res.status).toEqual(501);
   },
 );
 

init/src/init.ts
@@ -360,12 +360,10 @@ ${GRADIENT_CSS}`;
     // Skip this and be silent if there is a network issue.
   }
 
-  const MAIN_TS = `import { App, fsRoutes, staticFiles } from "fresh";
+  const MAIN_TS = `import { App, fsRoutes } from "fresh";
 import { define, type State } from "./utils.ts";
 
 export const app = new App<State>();
-
-app.use(staticFiles());
 
 // this is the same as the /api/:name route defined via a file. feel free to delete this!
 app.get("/api2/:name", (ctx) => {

src/app_test.tsx
@@ -2,6 +2,7 @@ import { expect } from "@std/expect";
 import { App, getIslandRegistry, setBuildCache } from "./app.ts";
 import { FakeServer } from "./test_utils.ts";
 import { ProdBuildCache } from "./build_cache.ts";
+import { HttpError } from "./error.ts";
 
 Deno.test("App - .use()", async () => {
   const app = new App<{ text: string }>()
@@ -35,8 +36,6 @@ Deno.test("App - .use() #2", async () => {
 
 Deno.test("App - .get()", async () => {
   const app = new App()
-    .post("/", () => new Response("ok"))
-    .post("/foo", () => new Response("ok"))
     .get("/", () => new Response("ok"))
     .get("/foo", () => new Response("ok"));
 
@@ -323,11 +322,11 @@ Deno.test(
   "App - .mountApp() self mount, different order",
   async () => {
     const innerApp = new App<{ text: string }>()
-      .get("/foo", (ctx) => new Response(ctx.state.text))
       .use(function B(ctx) {
         ctx.state.text += "B";
         return ctx.next();
       })
+      .get("/foo", (ctx) => new Response(ctx.state.text))
       .post("/foo", (ctx) => new Response(ctx.state.text));
 
     const app = new App<{ text: string }>()
@@ -389,6 +388,27 @@ Deno.test(
 
     const res = await server.get("/");
     expect(await res.text()).toEqual("Outer_Inner");
+  },
+);
+
+// https://github.com/denoland/fresh/issues/3033
+Deno.test(
+  "App - .mountApp() self mount with dynamic routes",
+  async () => {
+    const innerApp = new App<{ text: string }>()
+      .get("/", () => new Response("list authors"))
+      .get("/:name", (ctx) => new Response(`show: ${ctx.params.name}`));
+
+    const app = new App<{ text: string }>()
+      .mountApp("/api/authors", innerApp);
+
+    const server = new FakeServer(app.handler());
+
+    let res = await server.get("/api/authors");
+    expect(await res.text()).toEqual("list authors");
+
+    res = await server.get("/api/authors/foo");
+    expect(await res.text()).toEqual("show: foo");
   },
 );
 
@@ -531,4 +551,60 @@ Deno.test("App - adding Island should convert to valid export names", () => {
     exportName: "default",
     fn: component3,
   });
+});
+
+Deno.test("App - overwrite default 404 handler", async () => {
+  const app = new App()
+    .notFound(() => new Response("bar", { status: 404 }))
+    .get("/foo", () => new Response("foo"))
+    .get("/thrower", () => {
+      throw new HttpError(404);
+    });
+
+  const server = new FakeServer(app.handler());
+
+  let res = await server.get("/invalid");
+  expect(await res.text()).toEqual("bar");
+
+  res = await server.get("/thrower");
+  expect(await res.text()).toEqual("bar");
+});
+
+Deno.test("App - uses error route", async () => {
+  const app = new App()
+    .onError("*", () => new Response("error route", { status: 200 }))
+    .get("/thrower", () => {
+      throw new HttpError(404);
+    });
+
+  const server = new FakeServer(app.handler());
+
+  let res = await server.get("/invalid");
+  expect(await res.text()).toEqual("error route");
+
+  res = await server.get("/thrower");
+  expect(await res.text()).toEqual("error route");
+});
+
+Deno.test("App - uses notFound route on 404", async () => {
+  const app = new App()
+    .notFound(() => new Response("not found route", { status: 404 }))
+    .onError("*", () => new Response("error route", { status: 500 }))
+    .get("/thrower", () => {
+      throw new HttpError(404);
+    })
+    .get("/thrower_2", () => {
+      throw new HttpError(500);
+    });
+
+  const server = new FakeServer(app.handler());
+
+  let res = await server.get("/invalid");
+  expect(await res.text()).toEqual("not found route");
+
+  res = await server.get("/thrower");
+  expect(await res.text()).toEqual("not found route");
+
+  res = await server.get("/thrower_2");
+  expect(await res.text()).toEqual("error route");
 });

src/compat.ts
@@ -1,5 +1,6 @@
 import type { ComponentChildren } from "preact";
-import type { FreshContext, PageProps } from "./context.ts";
+import type { FreshContext } from "./context.ts";
+import type { PageProps } from "./render.ts";
 import type { HandlerFn, RouteHandler } from "./handlers.ts";
 
 /**

src/compat_test.tsx
@@ -1,4 +1,4 @@
-import type { PageProps } from "./context.ts";
+import type { PageProps } from "./render.ts";
 import { assertType, type IsExact } from "@std/testing/types";
 import { expect } from "@std/expect";
 import { type defineApp, type defineLayout, defineRoute } from "./compat.ts";

commit 78a5bf682bacb8035fd50ac19fc9653f45232a16

fix: incorrect middleware ordering
This is a regression introduced in https://github.com/denoland/fresh/pull/3086

src/app.ts
@@ -242,7 +242,7 @@ export class App<State> {
       fns = middlewares;
     }
 
-    const segment = getOrCreateSegment(this.#root, path, false);
+    const segment = getOrCreateSegment(this.#root, path, true);
     segment.middlewares.push(...fns);
 
     return this;

src/plugins/fs_routes/mod_test.tsx
@@ -1512,3 +1512,37 @@ Deno.test("fsRoutes - warn on _layout handler", async () => {
     expect.any(String),
   );
 });
+
+// Middleware issue in 2.0.0-alpha.40
+Deno.test("fsRoutes - call correct middleware", async () => {
+  const server = await createServer<{ text: string }>({
+    "routes/_middleware.ts": {
+      handler: async function middleware(ctx) {
+        ctx.state.text = "_middleware";
+        return await ctx.next();
+      },
+    },
+    "routes/index.ts": {
+      default: async (ctx) => await new Response(ctx.state.text),
+    },
+    "routes/admin/_middleware.ts": {
+      handler: async function adminMiddleware(ctx) {
+        ctx.state.text += "admin/_middleware";
+        return await ctx.next();
+      },
+    },
+    "routes/foo/index.ts": {
+      handler: (ctx) => {
+        return new Response(ctx.state.text);
+      },
+    },
+  });
+
+  let res = await server.get("/");
+  let text = await res.text();
+  expect(text).toEqual("_middleware");
+
+  res = await server.get("/foo");
+  text = await res.text();
+  expect(text).toEqual("_middleware");
+});