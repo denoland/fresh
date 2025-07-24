0841940b746f277a2fb5690eb8f9c337f7ab2021
Marvin Hagemeister <marvin@deno.com>
2025-07-24 16:12:00 +0200

    chore: release tailwind plugin 0.0.1-alpha.9 (#3127)

--- init/src/init.ts
+++ init/src/init.ts
@@ -4,7 +4,7 @@ import * as path from "@std/path";
 
 // Keep these as is, as we replace these version in our release script
 const FRESH_VERSION = "2.0.0-alpha.44";
-const FRESH_TAILWIND_VERSION = "0.0.1-alpha.8";
+const FRESH_TAILWIND_VERSION = "0.0.1-alpha.9";
 const PREACT_VERSION = "10.26.9";
 const PREACT_SIGNALS_VERSION = "2.2.1";
 const TAILWINDCSS_VERSION = "4.1.10";
