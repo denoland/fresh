5b061edf48715f25cc25add691e3fad131c1954b
Marvin Hagemeister <marvin@deno.com>
2025-08-08 12:24:24 +0200

    fix: don't externalize packages on ssr

--- packages/plugin-vite/src/plugins/patches.ts
+++ packages/plugin-vite/src/plugins/patches.ts
@@ -1,6 +1,5 @@
 import type { Plugin } from "vite";
 import * as babel from "@babel/core";
-import { cjsPlugin } from "./commonjs.ts";
 import { npmWorkaround } from "./npm_workaround.ts";
 
 export function patches(): Plugin {
@@ -13,7 +12,7 @@ export function patches(): Plugin {
       const res = babel.transformSync(code, {
         filename: id,
         babelrc: false,
-        plugins: [npmWorkaround, cjsPlugin],
+        plugins: [npmWorkaround],
       });
 
       if (res?.code) {
