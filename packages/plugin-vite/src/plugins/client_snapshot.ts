3b63b2217e8752a8da24e21675fda0ddb8fa6ed8
Marvin Hagemeister <marvin@deno.com>
2025-09-15 17:26:42 +0200

    fix(vite): nested islands not working

--- packages/plugin-vite/src/plugins/client_snapshot.ts
+++ packages/plugin-vite/src/plugins/client_snapshot.ts
@@ -1,94 +1,143 @@
 import type { Plugin, ViteDevServer } from "vite";
-import type { ResolvedFreshViteConfig } from "../utils.ts";
-import { crawlFsItem } from "fresh/internal-dev";
+import { pathWithRoot, type ResolvedFreshViteConfig } from "../utils.ts";
+import { crawlFsItem, specToName } from "fresh/internal-dev";
 
-export function clientSnapshot(options: ResolvedFreshViteConfig): Plugin {
+export function clientSnapshot(options: ResolvedFreshViteConfig): Plugin[] {
   const modName = "fresh:client-snapshot";
 
   const islands = new Set<string>();
   let server: ViteDevServer | undefined;
   let isDev = false;
 
-  return {
-    name: "fresh:client-snapshot",
-    applyToEnvironment(env) {
-      return env.name === "client";
-    },
-    config(_, env) {
-      isDev = env.command === "serve";
+  const entryToIsland = new Map<string, string>();
 
-      return {
-        environments: {
-          client: {
-            build: {
-              rollupOptions: {
-                input: {
-                  "client-snapshot": "fresh:client-snapshot",
+  return [
+    {
+      name: "fresh:client-snapshot",
+      applyToEnvironment(env) {
+        return env.name === "client";
+      },
+
+      async config(cfg, env) {
+        isDev = env.command === "serve";
+
+        const cwd = Deno.cwd();
+
+        const result = await crawlFsItem({
+          islandDir: pathWithRoot(options.islandsDir, cfg.root ?? cwd),
+          routeDir: pathWithRoot(options.routeDir, cfg.root ?? cwd),
+          ignore: options.ignore,
+        });
+
+        const input: Record<string, string> = {};
+
+        if (isDev) {
+          input["client-snapshot"] = "fresh:client-snapshot";
+        }
+
+        for (let i = 0; i < result.islands.length; i++) {
+          const filePath = result.islands[i];
+          islands.add(filePath);
+
+          if (!isDev) {
+            const specName = specToName(filePath);
+            const name = options.namer.getUniqueName(specName);
+
+            entryToIsland.set(name, filePath);
+            input[`fresh-island::${name}`] = `fresh-client-island::${name}`;
+          }
+        }
+
+        return {
+          environments: {
+            client: {
+              build: {
+                rollupOptions: {
+                  input,
                 },
               },
             },
           },
+        };
+      },
+      configResolved(cfg) {
+        for (const [name, spec] of entryToIsland.entries()) {
+          const full = pathWithRoot(spec, cfg.root);
+          entryToIsland.set(name, full);
+        }
+      },
+      options(opts) {
+        if (!isDev) {
+          options.islandSpecifiers.forEach((_name, spec) => {
+            islands.add(spec);
+
+            if (!isDev) {
+              const specName = specToName(spec);
+              const name = options.namer.getUniqueName(specName);
+              entryToIsland.set(name, spec);
+
+              // deno-lint-ignore no-explicit-any
+              (opts.input as any)[`fresh-island::${name}`] =
+                `fresh-client-island::${name}`;
+            }
+          });
+        }
+      },
+      configureServer(devServer) {
+        server = devServer;
+      },
+      resolveId: {
+        filter: {
+          id: /fresh:client-snapshot/,
         },
-      };
-    },
-    configResolved() {
-      options.islandSpecifiers.forEach((_name, spec) => {
-        islands.add(spec);
-      });
-    },
-    async buildStart() {
-      const result = await crawlFsItem({
-        islandDir: options.islandsDir,
-        routeDir: options.routeDir,
-        ignore: options.ignore,
-      });
-
-      for (let i = 0; i < result.islands.length; i++) {
-        const filePath = result.islands[i];
-        islands.add(filePath);
-      }
-    },
-    configureServer(devServer) {
-      server = devServer;
-    },
-    resolveId: {
-      filter: {
-        id: /fresh:client-snapshot/,
-      },
-      handler(id) {
-        if (id === modName) {
-          return `\0${modName}`;
-        }
-      },
-    },
-    load: {
-      filter: {
-        id: /\0fresh:client-snapshot/,
-      },
-      handler() {
-        const imports = Array.from(islands.keys()).map((file, i) => {
-          return `export const mod_${i} = await import(${
-            JSON.stringify(file)
-          });`;
-        }).join("\n");
-
-        if (isDev && server !== undefined) {
-          const mod = server.environments.ssr.moduleGraph.getModuleById(
-            "\0fresh:server-snapshot",
-          );
-          if (mod) {
-            server.environments.ssr.moduleGraph.invalidateModule(mod);
+        handler(id) {
+          if (id === modName) {
+            return `\0${modName}`;
           }
-        }
+        },
+      },
+      load: {
+        filter: {
+          id: /\0fresh:client-snapshot/,
+        },
+        handler() {
+          const imports = Array.from(islands.keys()).map((file, i) => {
+            return `export const mod_${i} = await import(${
+              JSON.stringify(file)
+            });`;
+          }).join("\n");
 
-        return `${imports}
+          if (isDev && server !== undefined) {
+            const mod = server.environments.ssr.moduleGraph.getModuleById(
+              "\0fresh:server-snapshot",
+            );
+            if (mod) {
+              server.environments.ssr.moduleGraph.invalidateModule(mod);
+            }
+          }
+
+          return `${imports}
 if (import.meta.hot) {
   import.meta.hot.accept(() => {
     console.log("accepting client-snapshot")
   });
 }
 `;
+        },
       },
     },
-  };
+    {
+      name: "fresh:client-island",
+      resolveId: {
+        filter: {
+          id: /^fresh-client-island::/,
+        },
+        handler(id) {
+          const name = id.slice("fresh-client-island::".length);
+          const full = entryToIsland.get(name);
+          return full;
+        },
+      },
+    },
+  ];
 }
