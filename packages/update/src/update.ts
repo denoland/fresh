a46bd8c19c18f909cdef16dd5cba7bb67b304792
Marvin Hagemeister <marvin@deno.com>
2025-09-15 19:22:10 +0200

    chore: release 2.1.0
    - @fresh/core@2.1.0
    - @fresh/plugin-vite@1.0.3
    - @fresh/update@2.1.0
    - @fresh/init@2.0.9

--- packages/update/src/update.ts
+++ packages/update/src/update.ts
@@ -6,8 +6,8 @@ import { ProgressBar } from "@std/cli/unstable-progress-bar";
 
 export const SyntaxKind = tsmorph.ts.SyntaxKind;
 
-export const FRESH_VERSION = "2.0.2";
-export const PREACT_VERSION = "10.27.1";
+export const FRESH_VERSION = "2.1.0";
+export const PREACT_VERSION = "10.27.2";
 export const PREACT_SIGNALS_VERSION = "2.3.1";
 
 // Function to filter out node_modules and vendor directories from logs
