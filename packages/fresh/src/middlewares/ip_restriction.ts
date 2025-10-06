caae8537f5b7d62033fe62e5af3835bbcf3ec1c8
Octo8080 <okutannderuta0529@gmail.com>
2025-10-07 00:16:06 +0900

    use @std/net

--- packages/fresh/src/middlewares/ip_restriction.ts
+++ packages/fresh/src/middlewares/ip_restriction.ts
@@ -1,299 +1,17 @@
-import type { FreshContext } from "../context.ts";
-import type { MiddlewareFn } from "./mod.ts";
+import type { Context } from "../context.ts";
+import type { Middleware } from "./mod.ts";
+import { isIPv4, isIPv6, matchSubnets } from "@std/net/unstable-ip";
 
-export type AddressType = "IPv4" | "IPv6";
-
-export type NetAddrInfo = {
-  /**
-   * Transport protocol type
-   */
-  transport?: "tcp" | "udp";
-  /**
-   * Transport port number
-   */
-  port: number;
-  address: string;
-  addressType?: AddressType;
-};
-
-export interface ConnInfo {
-  /**
-   * Remote information
-   */
-  remote: NetAddrInfo;
+export type AddressType = "IPv6" | "IPv4" | "none";
+export interface IPRestrictionRules {
+  denyList?: string[];
+  allowList?: string[];
 }
 
-/**
- * Helper type
- */
-export type GetConnInfo = (c: FreshContext) => ConnInfo;
-
-/**
- * Expand IPv6 Address
- * @param ipV6 Shorten IPv6 Address
- * @return expanded IPv6 Address
- */
-
-export const expandIPv6 = (ipV6: string): string => {
-  const sections = ipV6.split(":");
-  if (IPV4_REGEX.test(sections.at(-1) as string)) {
-    sections.splice(
-      -1,
-      1,
-      ...convertIPv6BinaryToString(
-        convertIPv4ToBinary(sections.at(-1) as string),
-      ) // => ::7f00:0001
-        .substring(2) // => 7f00:0001
-        .split(":"), // => ['7f00', '0001']
-    );
-  }
-  for (let i = 0; i < sections.length; i++) {
-    const node = sections[i];
-    if (node !== "") {
-      sections[i] = node.padStart(4, "0");
-    } else {
-      sections[i + 1] === "" && sections.splice(i + 1, 1);
-      sections[i] = new Array(8 - sections.length + 1).fill("0000").join(":");
-    }
-  }
-  return sections.join(":");
-};
-
-const IPV4_REGEX = /^[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}$/;
-
-/**
- * Returns the address type ("IPv4" or "IPv6") for a given remote address string.
- *
- * @param remoteAddr - The remote address as a string.
- * @returns The address type ("IPv4" | "IPv6") or undefined if not recognized.
- */
-export const distinctRemoteAddr = (
-  remoteAddr: string,
-): AddressType | undefined => {
-  if (IPV4_REGEX.test(remoteAddr)) {
-    return "IPv4";
-  }
-  if (remoteAddr.includes(":")) {
-    return "IPv6";
-  }
-};
-
-/**
- * Converts an IPv4 address string to a bigint binary representation.
- *
- * @param ipv4 - The IPv4 address as a string (e.g., "192.168.0.1").
- * @returns The IPv4 address as a bigint.
- */
-export const convertIPv4ToBinary = (ipv4: string): bigint => {
-  const parts = ipv4.split(".");
-  let result = 0n;
-  for (let i = 0; i < 4; i++) {
-    result <<= 8n;
-    result += BigInt(parts[i]);
-  }
-  return result;
-};
-
-/**
- * Converts an IPv6 address string to a bigint binary representation.
- *
- * @param ipv6 - The IPv6 address as a string (e.g., "2001:db8::1").
- * @returns The IPv6 address as a bigint.
- */
-export const convertIPv6ToBinary = (ipv6: string): bigint => {
-  const sections = expandIPv6(ipv6).split(":");
-  let result = 0n;
-  for (let i = 0; i < 8; i++) {
-    result <<= 16n;
-    result += BigInt(parseInt(sections[i], 16));
-  }
-  return result;
-};
-
-/**
- * Converts a binary representation of an IPv4 address to its string form.
- *
- * @param ipV4 - The IPv4 address as a bigint.
- * @returns The IPv4 address as a string (e.g., "192.168.0.1").
- */
-export const convertIPv4BinaryToString = (ipV4: bigint): string => {
-  const sections = [];
-  for (let i = 0; i < 4; i++) {
-    sections.push((ipV4 >> BigInt(8 * (3 - i))) & 0xffn);
-  }
-  return sections.join(".");
-};
-
-/**
- * Converts a binary representation of an IPv6 address to its normalized string form.
- *
- * @param ipV6 - The IPv6 address as a bigint.
- * @returns The normalized IPv6 address as a string (e.g., "2001:db8::1").
- */
-export const convertIPv6BinaryToString = (ipV6: bigint): string => {
-  // IPv6-mapped IPv4 address
-  if (ipV6 >> 32n === 0xffffn) {
-    return `::ffff:${convertIPv4BinaryToString(ipV6 & 0xffffffffn)}`;
-  }
-
-  const sections = [];
-  for (let i = 0; i < 8; i++) {
-    sections.push(((ipV6 >> BigInt(16 * (7 - i))) & 0xffffn).toString(16));
-  }
-
-  let currentZeroStart = -1;
-  let maxZeroStart = -1;
-  let maxZeroEnd = -1;
-  for (let i = 0; i < 8; i++) {
-    if (sections[i] === "0") {
-      if (currentZeroStart === -1) {
-        currentZeroStart = i;
-      }
-    } else {
-      if (currentZeroStart > -1) {
-        if (i - currentZeroStart > maxZeroEnd - maxZeroStart) {
-          maxZeroStart = currentZeroStart;
-          maxZeroEnd = i;
-        }
-        currentZeroStart = -1;
-      }
-    }
-  }
-  if (currentZeroStart > -1) {
-    if (8 - currentZeroStart > maxZeroEnd - maxZeroStart) {
-      maxZeroStart = currentZeroStart;
-      maxZeroEnd = 8;
-    }
-  }
-  if (maxZeroStart !== -1) {
-    sections.splice(maxZeroStart, maxZeroEnd - maxZeroStart, ":");
-  }
-
-  return sections.join(":").replace(/:{2,}/g, "::");
-};
-
-/**
- * ### IPv4 and IPv6
- * - `*` match all
- *
- * ### IPv4
- * - `192.168.2.0` static
- * - `192.168.2.0/24` CIDR Notation
- *
- * ### IPv6
- * - `::1` static
- * - `::1/10` CIDR Notation
- */
-/**
- * Type for a function that matches an IP restriction rule.
- *
- * @param addr - The address and its type to check.
- * @returns True if the rule matches, false otherwise.
- */
-type IPRestrictionRuleFunction = (
-  addr: { addr: string; type: AddressType },
-) => boolean;
-
-/**
- * IP restriction rule, which can be a string or a function.
- */
-export type IPRestrictionRule =
-  | string
-  | ((addr: { addr: string; type: AddressType }) => boolean);
-
-const IS_CIDR_NOTATION_REGEX = /\/[0-9]{0,3}$/;
-const buildMatcher = (
-  rules: IPRestrictionRule[],
-): (addr: { addr: string; type: AddressType; isIPv4: boolean }) => boolean => {
-  const functionRules: IPRestrictionRuleFunction[] = [];
-  const staticRules: Set<string> = new Set();
-  const cidrRules: [boolean, bigint, bigint][] = [];
-
-  for (let rule of rules) {
-    if (rule === "*") {
-      return () => true;
-    } else if (typeof rule === "function") {
-      functionRules.push(rule);
-    } else {
-      if (IS_CIDR_NOTATION_REGEX.test(rule)) {
-        const separatedRule = rule.split("/");
-
-        const addrStr = separatedRule[0];
-        const type = distinctRemoteAddr(addrStr);
-        if (type === undefined) {
-          throw new TypeError(`Invalid rule: ${rule}`);
-        }
-
-        const isIPv4 = type === "IPv4";
-        const prefix = parseInt(separatedRule[1]);
-
-        if (isIPv4 ? prefix === 32 : prefix === 128) {
-          // this rule is a static rule
-          rule = addrStr;
-        } else {
-          const addr = (isIPv4 ? convertIPv4ToBinary : convertIPv6ToBinary)(
-            addrStr,
-          );
-          const mask = ((1n << BigInt(prefix)) - 1n) <<
-            BigInt((isIPv4 ? 32 : 128) - prefix);
-
-          cidrRules.push(
-            [isIPv4, addr & mask, mask] as [boolean, bigint, bigint],
-          );
-          continue;
-        }
-      }
-
-      const type = distinctRemoteAddr(rule);
-      if (type === undefined) {
-        throw new TypeError(`Invalid rule: ${rule}`);
-      }
-      staticRules.add(
-        type === "IPv4"
-          ? rule // IPv4 address is already normalized, so it is registered as is.
-          : convertIPv6BinaryToString(convertIPv6ToBinary(rule)), // normalize IPv6 address (e.g. 0000:0000:0000:0000:0000:0000:0000:0001 => ::1)
-      );
-    }
-  }
-
-  return (remote: {
-    addr: string;
-    type: AddressType;
-    isIPv4: boolean;
-    binaryAddr?: bigint;
-  }): boolean => {
-    if (staticRules.has(remote.addr)) {
-      return true;
-    }
-    for (const [isIPv4, addr, mask] of cidrRules) {
-      if (isIPv4 !== remote.isIPv4) {
-        continue;
-      }
-      const remoteAddr = (remote.binaryAddr ||= (
-        isIPv4 ? convertIPv4ToBinary : convertIPv6ToBinary
-      )(remote.addr));
-      if ((remoteAddr & mask) === addr) {
-        return true;
-      }
-    }
-    for (const rule of functionRules) {
-      if (rule({ addr: remote.addr, type: remote.type })) {
-        return true;
-      }
-    }
-    return false;
+function buildMatcher(ipList: string[]) {
+  return (addr: string) => {
+    return matchSubnets(addr, ipList);
   };
-};
-
-/**
- * Interface for configuring IP restriction middleware rules.
- *
- * @property denyList - List of rules to explicitly deny.
- * @property allowList - List of rules to explicitly allow.
- */
-export interface IPRestrictionRules {
-  denyList?: IPRestrictionRule[];
-  allowList?: IPRestrictionRule[];
 }
 
 function blockError(): Response {
@@ -305,90 +23,59 @@ function blockError(): Response {
   });
 }
 
-/**
- * Retrieves connection information from the Fresh context.
- *
- * @param ctx - The Fresh context object.
- * @param distinctRemoteAddr - Function to determine the address type.
- * @returns The connection information.
- * @throws {TypeError} If the transport protocol is not TCP.
- */
-function getIP(
-  ctx: FreshContext,
-  distinctRemoteAddr: (addr: string) => AddressType | undefined,
-): ConnInfo | never {
-  const remoteAddr = ctx.info.remoteAddr;
-
-  if (remoteAddr.transport !== "tcp") {
-    throw new TypeError(
-      "Unsupported transport protocol. Only TCP is supported.",
-    );
+function findType(addr: string): AddressType {
+  if (isIPv4(addr)) {
+    return "IPv4";
+  } else if (isIPv6(addr)) {
+    return "IPv6";
   }
-
-  return {
-    remote: {
-      transport: remoteAddr.transport,
-      port: remoteAddr.port,
-      address: remoteAddr.hostname,
-      addressType: distinctRemoteAddr(remoteAddr.hostname),
-    },
-  };
+  return "none";
 }
 
-/**
- * IP restriction middleware for Fresh.
- *
- * Allows or denies requests based on allowList and denyList rules.
- *
- * @template T Middleware type parameter
- * @param rules Object containing IP restriction rules:
- *   - denyList: List of rules to explicitly deny (default: [])
- *   - allowList: List of rules to explicitly allow (default: [])
- * @param options Optional settings:
- *   - onError: Function to handle denied requests
- *     (remote, ctx) => Response | Promise<Response>
- *   - getIP: Function to extract connection info from context
- *     (ctx, distinctRemoteAddr) => ConnInfo | never
- * @returns Middleware function for Fresh
- */
-export function ipRestriction<T>(
+export interface IpRestrictionOptions {
+  onError?: <State>(
+    remote: { addr: string; type: AddressType },
+    ctx: Context<State>,
+  ) => Response | Promise<Response>;
+}
+
+export function ipRestriction<State>(
   { denyList = [], allowList = [] }: IPRestrictionRules,
-  options?: {
-    onError?: (
-      remote: { addr: string; type: AddressType },
-      ctx: FreshContext,
-    ) => Response | Promise<Response>;
-    getIP?: (
-      ctx: FreshContext,
-      distinctRemoteAddr: (addr: string) => AddressType | undefined,
-    ) => ConnInfo | never;
-  },
-): MiddlewareFn<T> {
+  options?: IpRestrictionOptions,
+): Middleware<State> {
   const allowLength = allowList.length;
 
   const denyMatcher = buildMatcher(denyList);
   const allowMatcher = buildMatcher(allowList);
 
-  return async function ipRestriction(ctx: FreshContext) {
-    const connInfo = (options?.getIP || getIP)(ctx, distinctRemoteAddr);
-    const addr = connInfo?.remote.address;
-    if (!addr) {
-      return blockError();
+  return async function ipRestriction<State>(ctx: Context<State>) {
+    if (ctx.info.remoteAddr.transport !== "tcp") {
+      throw new TypeError(
+        "Unsupported transport protocol. Only TCP is supported.",
+      );
     }
-    const type = connInfo?.remote.addressType;
-    if (!type) {
+
+    if (!ctx.info.remoteAddr.hostname) {
       return blockError();
     }
 
-    const remoteData = { addr, type, isIPv4: type === "IPv4" };
+    const addr = ctx.info.remoteAddr.hostname == "localhost"
+      ? "172.0.0.1"
+      : ctx.info.remoteAddr.hostname;
 
-    if (denyMatcher(remoteData)) {
+    const type = findType(addr);
+    if (type == "none") {
+      return blockError();
+    }
+
+    if (denyMatcher(addr)) {
       if (options?.onError) {
         return options.onError({ addr, type }, ctx);
       }
       return blockError();
     }
-    if (allowMatcher(remoteData)) {
+
+    if (allowMatcher(addr)) {
       const res = await ctx.next();
       return res;
     }
--- packages/fresh/src/middlewares/ip_restriction_test.ts
+++ packages/fresh/src/middlewares/ip_restriction_test.ts
@@ -1,324 +1,105 @@
 import { App } from "../app.ts";
-import type { FreshContext } from "../context.ts";
+import type { Context } from "../context.ts";
 import {
-  type AddressType,
-  type ConnInfo,
-  convertIPv4BinaryToString,
-  convertIPv4ToBinary,
-  convertIPv6BinaryToString,
-  convertIPv6ToBinary,
-  distinctRemoteAddr,
-  expandIPv6,
   ipRestriction,
-  type IPRestrictionRule,
+  type IpRestrictionOptions,
+  type IPRestrictionRules,
 } from "./ip_restriction.ts";
-
-import { beforeEach, describe, it } from "@std/testing/bdd";
 import { expect } from "@std/expect";
 
-describe("expandIPv6", () => {
-  it("Should result be valid", () => {
-    expect(expandIPv6("1::1")).toBe("0001:0000:0000:0000:0000:0000:0000:0001");
-    expect(expandIPv6("::1")).toBe("0000:0000:0000:0000:0000:0000:0000:0001");
-    expect(expandIPv6("2001:2::")).toBe(
-      "2001:0002:0000:0000:0000:0000:0000:0000",
-    );
-    expect(expandIPv6("2001:2::")).toBe(
-      "2001:0002:0000:0000:0000:0000:0000:0000",
-    );
-    expect(expandIPv6("2001:0:0:db8::1")).toBe(
-      "2001:0000:0000:0db8:0000:0000:0000:0001",
-    );
-    expect(expandIPv6("::ffff:127.0.0.1")).toBe(
-      "0000:0000:0000:0000:0000:ffff:7f00:0001",
-    );
-  });
-});
-describe("distinctRemoteAddr", () => {
-  it("Should result be valid", () => {
-    expect(distinctRemoteAddr("1::1")).toBe("IPv6");
-    expect(distinctRemoteAddr("::1")).toBe("IPv6");
-    expect(distinctRemoteAddr("::ffff:127.0.0.1")).toBe("IPv6");
-
-    expect(distinctRemoteAddr("192.168.2.0")).toBe("IPv4");
-    expect(distinctRemoteAddr("192.168.2.0")).toBe("IPv4");
-
-    expect(distinctRemoteAddr("example.com")).toBeUndefined();
-  });
-});
-
-describe("convertIPv4ToBinary", () => {
-  it("Should result is valid", () => {
-    expect(convertIPv4ToBinary("0.0.0.0")).toBe(0n);
-    expect(convertIPv4ToBinary("0.0.0.1")).toBe(1n);
-
-    expect(convertIPv4ToBinary("0.0.1.0")).toBe(1n << 8n);
-  });
-});
-
-describe("convertIPv4ToString", () => {
-  expect(convertIPv4BinaryToString(convertIPv4ToBinary("0.0.0.0"))).toBe(
-    "0.0.0.0",
-  );
-  expect(convertIPv4BinaryToString(convertIPv4ToBinary("0.0.0.1"))).toBe(
-    "0.0.0.1",
-  );
-  expect(convertIPv4BinaryToString(convertIPv4ToBinary("0.0.1.0"))).toBe(
-    "0.0.1.0",
-  );
-});
-
-describe("convertIPv6ToBinary", () => {
-  it("Should result is valid", () => {
-    expect(convertIPv6ToBinary("::0")).toBe(0n);
-    expect(convertIPv6ToBinary("::1")).toBe(1n);
-
-    expect(convertIPv6ToBinary("::f")).toBe(15n);
-    expect(convertIPv6ToBinary("1234:::5678")).toBe(
-      24196103360772296748952112894165669496n,
-    );
-    expect(convertIPv6ToBinary("::ffff:127.0.0.1")).toBe(281472812449793n);
-  });
-});
-
-describe("convertIPv6ToString", () => {
-  expect(convertIPv6BinaryToString(convertIPv6ToBinary("::1"))).toBe("::1");
-  expect(convertIPv6BinaryToString(convertIPv6ToBinary("1::"))).toBe("1::");
-  expect(convertIPv6BinaryToString(convertIPv6ToBinary("1234:::5678"))).toBe(
-    "1234::5678",
-  );
-  expect(convertIPv6BinaryToString(convertIPv6ToBinary("2001:2::"))).toBe(
-    "2001:2::",
-  );
-  expect(convertIPv6BinaryToString(convertIPv6ToBinary("2001::db8:0:0:0:0:1")))
-    .toBe("2001:0:db8::1");
-  expect(
-    convertIPv6BinaryToString(
-      convertIPv6ToBinary("1234:5678:9abc:def0:1234:5678:9abc:def0"),
-    ),
-  ).toBe("1234:5678:9abc:def0:1234:5678:9abc:def0");
-  expect(convertIPv6BinaryToString(convertIPv6ToBinary("::ffff:127.0.0.1")))
-    .toBe("::ffff:127.0.0.1");
-});
-
-describe("ipRestriction middleware", () => {
-  const app = new App();
-
-  let hostname = "hogehoge";
-
-  function setDummyHostName(source: string): void {
-    hostname = source;
+function testHandler<T>(
+  remortAddr: string,
+  ipRestrictionRules: IPRestrictionRules,
+  options?: IpRestrictionOptions,
+): (request: Request) => Promise<Response> {
+  function remoteHostOverRide(ctx: Context<T>) {
+    (ctx.info.remoteAddr as { hostname: string }).hostname = remortAddr;
+    return ctx.next();
   }
 
-  function dummyGetIp(
-    _ctx: FreshContext,
-    distinctRemoteAddr: (addr: string) => AddressType | undefined,
-  ): ConnInfo | never {
-    return {
-      remote: {
-        transport: "tcp",
-        port: 12345,
-        address: hostname,
-        addressType: distinctRemoteAddr(hostname),
-      },
-    };
+  if (!options) {
+    return new App<T>()
+      .use(remoteHostOverRide)
+      .use(ipRestriction(ipRestrictionRules))
+      .all("/", () => new Response("hello"))
+      .handler();
   }
 
-  app.all(
-    "/basic/*",
-    ipRestriction(
-      {
-        allowList: ["192.168.1.0", "192.168.2.0/24"],
-        denyList: ["192.168.2.10"],
-      },
-      {
-        getIP: dummyGetIp,
-      },
-    ),
-  );
-  app.get("/basic/", () => {
-    return new Response("Hello World!");
-  });
+  return new App<T>()
+    .use(remoteHostOverRide)
+    .use(ipRestriction(ipRestrictionRules, options))
+    .all("/", () => new Response("hello"))
+    .handler();
+}
 
-  app.all(
-    "/allow-empty/*",
-    ipRestriction({
-      denyList: ["192.168.1.0"],
-    }, {
-      getIP: dummyGetIp,
-    }),
-  );
-  app.get("/allow-empty/", () => new Response("Hello World!"));
+async function createTest(
+  addr: string,
+  ipRestrictionRules: IPRestrictionRules,
+  options?: IpRestrictionOptions,
+): Promise<number> {
+  const handler = testHandler(addr, ipRestrictionRules, options);
 
-  const handler = app.handler();
+  const res = await handler(new Request("https://localhost/"));
 
-  beforeEach(() => {
-    setDummyHostName("hoge");
-  });
-  it("GET /basic with! 0.0.0.0", async () => {
-    setDummyHostName("0.0.0.0");
-    const res = await handler(new Request("http://localhost/basic/"));
-    expect(res.status).toBe(403);
-  });
-  it("GET /basic with! 192.168.1.0", async () => {
-    setDummyHostName("192.168.1.0");
-    const res = await handler(new Request("http://localhost/basic/"));
-    expect(res.status).toBe(200);
-  });
+  return res.status;
+}
 
-  it("GET /basic with! 192.168.2.5", async () => {
-    setDummyHostName("192.168.2.5");
-    const res = await handler(new Request("http://localhost/basic/"));
-    expect(res.status).toBe(200);
-  });
-  it("GET /basic with! 192.168.2.10", async () => {
-    setDummyHostName("192.168.2.10");
-    const res = await handler(new Request("http://localhost/basic/"));
-    expect(res.status).toBe(403);
-  });
-  it("GET /basic with! 0.0.0.0", async () => {
-    setDummyHostName("0.0.0.0");
-    const res = await handler(new Request("http://localhost/allow-empty/"));
-    expect(res.status).toBe(200);
-  });
-  it("GET /basic with! 192.168.1.0", async () => {
-    setDummyHostName("192.168.1.0");
-    const res = await handler(new Request("http://localhost/allow-empty/"));
-    expect(res.status).toBe(403);
-  });
-  it("GET /basic with! 192.168.2.5", async () => {
-    setDummyHostName("192.168.2.5");
-    const res = await handler(new Request("http://localhost/allow-empty/"));
-    expect(res.status).toBe(200);
-  });
-  it("GET /basic with! 192.168.2.10", async () => {
-    setDummyHostName("192.168.2.10");
-    const res = await handler(new Request("http://localhost/allow-empty/"));
-    expect(res.status).toBe(200);
-  });
+Deno.test("ipRestriction - no option", async () => {
+  expect(await createTest("192.168.1.10", {})).toBe(200);
 });
-
-describe("isMatchForRule", () => {
-  const app = new App();
-
-  const hostname = "0.0.0.0";
-
-  function dummyGetIp(
-    _ctx: FreshContext,
-    distinctRemoteAddr: (addr: string) => AddressType | undefined,
-  ): ConnInfo | never {
-    return {
-      remote: {
-        transport: "tcp",
-        port: 12345,
-        address: hostname,
-        addressType: distinctRemoteAddr(hostname),
-      },
-    };
-  }
-
-  app.all(
-    "*",
-    ipRestriction(
-      {
-        denyList: ["0.0.0.0"],
-      },
-      {
-        getIP: dummyGetIp,
-        onError: () => new Response("error"),
-      },
-    ),
-  );
-  app.get("*", () => new Response("ok"));
-
-  const handler = app.handler();
-
-  it("Custom onerror", async () => {
-    const res = await handler(new Request("http://localhost/"));
-    expect(res).toBeTruthy();
-    expect(await res.text()).toBe("error");
-  });
-});
-
-describe("isMatchForRule", () => {
-  const isMatch = async (
-    info: { addr: string; type: AddressType },
-    rule: IPRestrictionRule,
-  ) => {
-    const middleware = ipRestriction(
-      {
-        allowList: [rule],
-      },
-      {
-        getIP: () => ({
-          remote: {
-            transport: "tcp",
-            port: 12345,
-            address: info.addr,
-            addressType: info.type,
-          },
-        }),
-      },
-    );
-    const mockContext = {
-      next: () => Promise.resolve(new Response("hoge")),
-    } as unknown as FreshContext;
-    const res = await middleware(mockContext);
-
-    return res.status === 200;
+Deno.test("ipRestriction - set ipRestrictionRules deny only", async () => {
+  const ipRestrictionRules = {
+    denyList: ["192.168.1.10", "2001:db8::1"],
   };
 
-  it("star", async () => {
-    expect(await isMatch({ addr: "192.168.2.0", type: "IPv4" }, "*"))
-      .toBeTruthy();
-    expect(await isMatch({ addr: "192.168.2.1", type: "IPv4" }, "*"))
-      .toBeTruthy();
-    expect(await isMatch({ addr: "::0", type: "IPv6" }, "*")).toBeTruthy();
-  });
-  it("CIDR Notation", async () => {
-    expect(
-      await isMatch({ addr: "192.168.2.0", type: "IPv4" }, "192.168.2.0/24"),
-    ).toBeTruthy();
-    expect(
-      await isMatch({ addr: "192.168.2.1", type: "IPv4" }, "192.168.2.0/24"),
-    ).toBeTruthy();
-    expect(
-      await isMatch({ addr: "192.168.2.1", type: "IPv4" }, "192.168.2.1/32"),
-    ).toBeTruthy();
-    expect(
-      await isMatch({ addr: "192.168.2.1", type: "IPv4" }, "192.168.2.2/32"),
-    ).toBeFalsy();
+  expect(await createTest("192.168.1.10", ipRestrictionRules)).toBe(403);
+  expect(await createTest("192.168.1.11", ipRestrictionRules)).toBe(200);
+  expect(await createTest("2001:db8::1", ipRestrictionRules)).toBe(403);
+  expect(await createTest("2001:db8::2", ipRestrictionRules)).toBe(200);
+});
 
-    expect(await isMatch({ addr: "::0", type: "IPv6" }, "::0/1")).toBeTruthy();
-  });
-  it("Static Rules", async () => {
-    expect(await isMatch({ addr: "192.168.2.1", type: "IPv4" }, "192.168.2.1"))
-      .toBeTruthy();
-    expect(await isMatch({ addr: "1234::5678", type: "IPv6" }, "1234::5678"))
-      .toBeTruthy();
-    expect(
-      await isMatch(
-        { addr: "::ffff:127.0.0.1", type: "IPv6" },
-        "::ffff:127.0.0.1",
-      ),
-    ).toBeTruthy();
-    expect(
-      await isMatch(
-        { addr: "::ffff:127.0.0.1", type: "IPv6" },
-        "::ffff:7f00:1",
-      ),
-    ).toBeTruthy();
-  });
-  it("Function Rules", async () => {
-    expect(await isMatch({ addr: "0.0.0.0", type: "IPv4" }, () => true))
-      .toBeTruthy();
-    expect(await isMatch({ addr: "0.0.0.0", type: "IPv4" }, () => false))
-      .toBeFalsy();
+Deno.test("ipRestriction - set ipRestrictionRules arrow only", async () => {
+  const ipRestrictionRules = {
+    allowList: ["192.168.1.10", "2001:db8::1"],
+  };
+  expect(await createTest("192.168.1.10", ipRestrictionRules)).toBe(200);
+  expect(await createTest("192.168.1.11", ipRestrictionRules)).toBe(403);
+  expect(await createTest("2001:db8::1", ipRestrictionRules)).toBe(200);
+  expect(await createTest("2001:db8::2", ipRestrictionRules)).toBe(403);
+});
 
-    const ipaddr = "93.184.216.34";
-    await isMatch({ addr: ipaddr, type: "IPv4" }, (ip) => {
-      expect(ipaddr).toBe(ip.addr);
-      return false;
-    });
-  });
+// arrow and deny
+// deny の方が優先されるを英語で
+// When both allow and deny are set, deny takes precedence
+Deno.test("ipRestriction - set ipRestrictionRules arrow and deny", async () => {
+  const ipRestrictionRules = {
+    denyList: ["192.168.1.10", "2001:db8::1"],
+    allowList: ["192.168.1.10", "2001:db8::1"],
+  };
+  expect(await createTest("192.168.1.10", ipRestrictionRules)).toBe(403);
+  expect(await createTest("2001:db8::1", ipRestrictionRules)).toBe(403);
+});
+
+// adapt subnet mask
+Deno.test("ipRestriction - adapt subnet mask", async () => {
+  const ipRestrictionRules = {
+    denyList: ["192.168.1.0/24"],
+  };
+  expect(await createTest("192.168.1.10", ipRestrictionRules)).toBe(403);
+});
+
+// onError
+Deno.test("ipRestriction - custom onError", async () => {
+  const ipRestrictionRules = {
+    denyList: ["192.168.1.0/24"],
+  };
+
+  const customOnError: IpRestrictionOptions = {
+    onError: () => {
+      return new Response("custom onError", { status: 401 });
+    },
+  };
+
+  expect(await createTest("192.168.1.10", ipRestrictionRules, customOnError))
+    .toBe(401);
 });
