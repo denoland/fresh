32c80d849b54b4615cba1816a0b3822a4ba58f48
Marvin Hagemeister <marvin@deno.com>
2025-10-24 17:34:30 +0200

    fix(vite): mark transformed cjs with __esModule

--- packages/plugin-vite/tests/dev_server_test.ts
+++ packages/plugin-vite/tests/dev_server_test.ts
@@ -465,17 +465,6 @@ Deno.test({
 });
 
 Deno.test({
-  name: "vite dev - stripe",
-  fn: async () => {
-    const res = await fetch(`${demoServer.address()}/tests/stripe`);
-    const text = await res.text();
-    expect(text).toContain("<h1>stripe</h1>");
-  },
-  sanitizeOps: false,
-  sanitizeResources: false,
-});
-
-Deno.test({
   name: "vite dev - static index.html",
   fn: async () => {
     const res = await fetch(`${demoServer.address()}/test_static/foo`);
