ad7f776df3b5242509f94cd547c9b188963ac171
Bartek Iwańczuk <biwanczuk@gmail.com>
2026-03-29 21:36:54 +0200

    fmt

--- packages/plugin-vite/tests/fixtures/basepath/main.ts
+++ packages/plugin-vite/tests/fixtures/basepath/main.ts
@@ -2,6 +2,7 @@ import { App, staticFiles } from "@fresh/core";
 
 export const app = new App({ basePath: "/ui" })
   .use(staticFiles())
-  .get("/", () => new Response("<h1>ok</h1>", {
-    headers: { "Content-Type": "text/html" },
-  }));
+  .get("/", () =>
+    new Response("<h1>ok</h1>", {
+      headers: { "Content-Type": "text/html" },
+    }));
